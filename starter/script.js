008b78657e7bdbee066ada9e46a3702fd14339b7
Edmund <edmundcjj@gmail.com>
2018-03-02 18:07:03 +0800

    Completed Part 3

--- starter/script.js
+++ starter/script.js
@@ -1,66 +1,113 @@
 // put code here
 
+// Initialize variables
+var player_score = 0;
+
 // Ask user for name
 var name = prompt("Enter your name: ");
 console.log("Name is = " + name);
 
+// Notify user what game will be played
+alert("We are going to play a maze game. In this game, you test your luck at choosing paths whether left or right at every turn you make. Sometimes, a zombie will appear and you will have the chance to kill it or be attacked");
+alert("Points will be gained along the way. Your total score will be displayed at the end of the game");
+
 // Ask user which country he wish to visit
-var country = prompt("Enter the country you wish to visit from the list: Korea/America");
-console.log("Country chosen = " + country);
+var choice = prompt("Nice to meet you, " + name + ". Would you like to enter the zombie maze or the normal maze? [zombie/normal]");
 
 // Switch case based on the country user input
 console.log("Entering switch block");
-switch (country)
+switch (choice.toLowerCase())
 {
-  // Country 1
-  case "Korea":
-    console.log("Entered Korea case");
-    var area_of_visit = prompt("Which part of Korea do you intend to visit? NORTH/SOUTH");
-    if (area_of_visit === "NORTH"){
-      var stay = prompt("How long is your trip? ");
-      if (stay <= 10) {
-        alert(name + ", I think tt is best to go on a group tour as you can get the most out of it and view the awesome places in North Korea");
+  // Zombie Maze
+  case "zombie":
+    console.log("Entered zombie maze");
+    var left_right = prompt("You encountered a crosspath, there are only 2 paths ahead of you. Would you like to turn left or right?");
+    if (left_right.toLowerCase() === "left") {
+      alert("Good, you are still alive. You earn 50 points.");
+      player_score += 50;
+
+      var path = prompt("You have turned left but now there are 2 doors in front of you, DEATH/SURVIVE. What is your choice? ");
+      if (path.toLowerCase() === "death") {
+        player_score -= 50;
+        alert("GAME OVER!~~~ You have been killed by the zombie. You have lost all the points. ");
+        alert("Your total score is: " + player_score);
       }
-      else{
-        alert(name + ", I think you will require a VISA becuase you are staying in a foreign country for too long");
+      else if (path.toLowerCase() === "survive") {
+        alert("HOORAY! You made it out of the maze in one single piece alive! You earn extra 100 points.");
+        player_score += 100;
+        alert("Your total score is: " + player_score);
       }
     }
-    else if (area_of_visit === "SOUTH"){
-      var purpose = prompt("Type of trip?  LEISURE/SERIOUS");
-      if (purpose === "LEISURE") {
-        alert(name + ", I think you should only plan to visit 3 tourist destinations daily");
+
+    else if (left_right.toLowerCase() === "right") {
+      alert("Good, you are still alive. You earn 50 points.");
+      player_score += 50;
+
+      var path = prompt("You have turned right but now there are 2 doors in front of you, CLIFF/SURVIVE. What is your choice? ");
+      if (path.toLowerCase() === "cliff") {
+        alert("You have fallen off the cliff and you are now paralyzed from head to toe, however you still SURVIVED! You lose 40 points.");
+        player_score -= 40;
+        alert("Your total score is: " + player_score);
       }
-      else if (purpose === "SERIOUS") {
-        alert(name + ", I think you should plan to visit all possible tourist destinations from the east to the west in a circular manner");
+      else if (path.toLowerCase() === "survive") {
+        alert("HOORAY! You made it out of the maze in one single piece alive! You earn extra 100 points.");
+        player_score += 100;
+        alert("Your total score is: " + player_score);
       }
     }
   break;
 
-  // Country 2
-  case "America":
-    console.log("Entered America case");
-    var area_of_visit = prompt("Which part of America do you intend to visit? NORTH/SOUTH");
-    if (area_of_visit === "NORTH"){
-      var have_friends = prompt("Do you have friends over there? YES/NO");
-      if (have_friends === "YES"){
-        alert(name + ", I think you should ask your friend to give you a tour around North America");
+  // Normal Maze
+  case "normal":
+    console.log("Entered normal maze");
+    var left_right = prompt("You encountered a crosspath, there are only 2 paths ahead of you. Would you like to turn left or right?");
+    if (left_right.toLowerCase() === "left") {
+      alert("Wonderful! You found an poison antitode. You earn 50 points.");
+      player_score += 50;
+
+      var path = prompt("You have turned left but now there are 2 doors in front of you, DEATH/SURVIVE. What is your choice? ");
+      if (path.toLowerCase() === "death") {
+        alert("You enter a room full of poisonous snakes. Suddenly, a snake out from nowehere came and bit you. The poison has entered your bloodstream, you collasped on the ground, minus 20 points");
+        player_score -= 20;
+
+        var antitode_choice = prompt("Do you wish to use the antitode you found earlier on? [YES/NO]");
+        if (antitode_choice.toLowerCase() === "yes") {
+          alert("Phew!, you somehow managed to save yourself, you are back to full health. You have pass the maze. You earned yourself an extra 50 points.");
+          player_score += 50;
+          alert("Your total score is: " + player_score);
+        }
+        else if (antitode_choice.toLowerCase() === "no") {
+          alert("What a stupid choice to make!!!~~~ You are now dead! You lost all the points.");
+          player_score = 0;
+          alert("Your total score is: " + player_score);
+        }
       }
-      else if (have_friends === "NO") {
-        alert(name + ", I think you should hire a personal tour guide to bring you around North America");
+      else if (path.toLowerCase() === "survive") {
+        alert("HOORAY! You made it out of the maze in one single piece alive! You earn extra 100 points.");
+        player_score += 100;
+        alert("Your total score is: " + player_score);
       }
     }
-    else if (area_of_visit === "SOUTH"){
-      var feeling_lucky = prompt("Are you feeling very lucky now? YES/NO");
-      if (feeling_lucky === "YES"){
-        alert(name + ", I think you should head down to a casino and gamble to test how good your luck is");
+
+    else if (left_right.toLowerCase() === "right") {
+      alert("Good, you are still alive. You earn 50 points.");
+      player_score += 50;
+
+      var path = prompt("You have turned right but now there are 2 doors in front of you, CLIFF/SURVIVE. What is your choice? ");
+      if (path.toLowerCase() === "cliff") {
+        alert("You have fallen off the cliff but PHEW! you somehow managed to survive because you fell into a river and it washed you ashore. You earn extra 200 points for being lucky.");
+        player_score += 200;
+        alert("Your total score is: " + player_score);
       }
-      else if (feeling_lucky === "NO") {
-        alert(name + ", I think you should just stick to whatever you have planned for this trip");
+      else if (path.toLowerCase() === "survive") {
+        alert("HOORAY! You made it out of the maze in one single piece alive! You earn extra 100 points.");
+        player_score += 100;
+        alert("Your total score is: " + player_score);
       }
     }
     break;
 
   default:
-    alert("Sorry, country does not exist");
+    alert("Sorry!, maze does not exist");
 }
 console.log("Exiting switch block");
