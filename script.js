1441e88934819d13d298b0d026b626ef428679ca
ElaineChoo <elainechoo85@gmail.com>
2018-03-02 17:17:24 +0800

    still trying to debug

--- script.js
+++ script.js
@@ -1,146 +1,169 @@
+//INITIALING........
 //prompt player for name
 var playerName = prompt("Hey there, what is your name?");
 
 //Greet player and ask for the year
-var yyyy = "YYYY";
-var boldYYYY = document.write(yyyy.bold());
-var yearStr = prompt(`Hi ${playerName}, nice to meet you. What year would you like to go to, from 1958? (format of year: ${boldYYYY})`);
+var yearStr = prompt(`Hi ${playerName}, nice to meet you. What year would you like to go to, from 1958? (format of year: YYYY)`);
 
-// //switch string to integer
+//switch string to integer
 var year = parseInt(yearStr);
+console.log(year);
+//variable for no. of retry
+var i = 0;
 
-
-//variable to keep track of score
+// //variable to keep track of score
 var score = 0;
 
+var ready = false;
+var q1AnsInput = "";
+var q2AnsInput = "";
+var q3AnsInput = "";
+
+//START OF GAME!!
+
 //set the conditions for the different year (total of 7 path)
-function selectYear(){
-	switch (year) {
-
-		case (year >= 2018):
-		var entry = confirm(`${playerName} Welcome to The Tower of God. This is the entry test. Are you READY?`);
-		if (entry === true){
-			yearQ1();
+//function selectYear(year){
+	if (year >= 2018){
+		ready = confirm(`${playerName} Welcome to The Tower of God. This is the entry test. Are you READY?`);
+		if (ready === true){
+			console.log(ready);
+			yearQ1(q1AnsInput);
 		}
 		else {
+			console.log(ready);
 			exit();
 		}
-
-		break;
-
-		case (year < 2018 && year >= 2008):
-		var entry = confirm(`${playerName} Welcome to The Abssy. This is the entry test. Are you READY?`);
-		if (entry === true){
-			yearQ1();
-		}
-		else {
-			exit();
-		}
-		break;
-
-		case (year < 2008 && year >= 1998):
-		var entry = confirm(`${playerName} Welcome to The Zombie Land. This is the entry test. Are you READY?`);
-		if (entry === true){
-			yearQ1();
-		}
-		else {
-			exit();
-		}
-		break;
-
-		case (year < 1998 && year >= 1988):
-		var entry = confirm(`${playerName} Welcome to The Great Labyrinth. This is the entry test. Are you READY?`);
-		if (entry === true){
-			yearQ1();
-		}
-		else {
-			exit();
-		}
-		break;
-
-		case (year < 1988 && year >= 1978):
-		var entry = confirm(`${playerName} Welcome to The Space Dungeon. This is the entry test. Are you READY?`);
-		if (entry === true){
-			yearQ1();
-		}
-		else {
-			exit();
-		}
-
-
-		case (year < 1978 && year >= 1968):
-		var entry = confirm(`${playerName} Welcome to The Cave Dungeon. This is the entry test. Are you READY?`);
-		if (entry === true){
-			yearQ1();
-		}
-		else {
-			exit();
-		}
-
-		case (year < 1968 && year >= 1958):
-		var entry = confirm(`${playerName} Welcome to The Lavar Dungeon. This is the entry test. Are you READY?`);
-		if (entry === true){
-			yearQ1();
-		}
-		else {
-			exit();
-		}
-
-		default:
-		for (var try = 3; try <= 0; try--) {
-			prompt(`${playerName}, the year you have chosen is out of range. Kindly select a year from 1958 onwards. You have ${try} try left. (format of year: ${boldYYYY})`);
-			selectYear();
-		}
-		alert(`You have keyed the wrong input too many times. You have to restart the game.`);
 	}
-}
+	else if (year < 2018 && year >= 2008){
+		ready = confirm(`${playerName} Welcome to The Abssy. This is the entry test. Are you READY?`);
+		if (ready === true){
+			console.log(ready);
+			yearQ1(q1AnsInput);
+		}
+		else {
+			console.log(ready);
+			exit();
+		}
+	}
+
+	else if (year < 2008 && year >= 1998){
+		ready = confirm(`${playerName} Welcome to The Zombie Land. This is the entry test. Are you READY?`);
+		if (ready === true){
+			console.log(ready);
+			yearQ1(q1AnsInput);
+		}
+		else {
+			console.log(ready);
+			exit();
+		}
+	}
+
+	else if (year < 1998 && year >= 1988){
+		ready = confirm(`${playerName} Welcome to The Great Labyrinth. This is the entry test. Are you READY?`);
+		if (ready === true){
+			console.log(ready);
+			yearQ1(q1AnsInput);
+		}
+		else {
+			console.log(ready);
+			exit();
+		}
+	}
+
+	else if (year < 1988 && year >= 1978){
+		ready = confirm(`${playerName} Welcome to The Space Dungeon. This is the entry test. Are you READY?`);
+		if (ready === true){
+			console.log(ready);
+			yearQ1(q1AnsInput);
+		}
+		else {
+			console.log(ready);
+			exit();
+		}
+	}
+
+	else if (year < 1978 && year >= 1968){
+		ready = confirm(`${playerName} Welcome to The Cave Dungeon. This is the entry test. Are you READY?`);
+		if (ready === true){
+			console.log(ready);
+			yearQ1(q1AnsInput);
+		}
+		else {
+			console.log(ready);
+			exit();
+		}
+	}
+
+	else if (year < 1968 && year >= 1958){
+		ready = confirm(`${playerName} Welcome to The Lavar Dungeon. This is the entry test. Are you READY?`);
+		if (ready === true){
+			console.log(ready);
+			yearQ1(q1AnsInput);
+		}
+		else {
+			console.log(ready);
+			exit();
+		}
+	}
+	else{
+		for (var i = 3; i <= 0; i--) {
+			prompt(`${playerName}, the year you have chosen is out of range. Kindly select a year from 1958 onwards. You have ${retry} try left. (format of year: ${boldYYYY})`);
+			selectYear(year);
+		}
+		//alert(`You have keyed the wrong input too many times. You have to restart the game.`);
+	}
+//}
 
 function exit(){
 	alert(`Awww... It's so sad to see you go. Do come back soon! Your Score is ${score}`);
 }
 
-function yearQ1(){
-	var q1AnsInput = prompt(`There is a huge hungry monster in an enclosed cage. Beside the huge hungry monster, there is a black ball. You are to go into the cage and pop the ball without being eaten up by the monster. Are you afraid? (Y / N)`);
+function yearQ1(q1AnsInput){
+	q1AnsInput = prompt(`There is a huge hungry monster in an enclosed cage. Beside the huge hungry monster, there is a black ball. You are to go into the cage and pop the ball without being eaten up by the monster. Are you afraid? (Y / N)`);
 	var q1Ans = q1AnsInput.toLowerCase();
-	function q1(){
-		if (q1Ans === "Y"){
+	console.log(q1Ans);
+
+	function q1(q1Ans){
+		if (q1Ans === "y"){
 			score = score - 10;
 			console.log(score);
-			yearQ2();
+			yearQ2(q2AnsInput);
 		}
-		else if (q1Ans === "N") {
+		else if (q1Ans === "n") {
 			score = score + 10;
 			console.log(score);
-			yearQ2();
+			yearQ2(q2AnsInput);
 		}
 		else {
-			for (var try = 3; try <= 0; try--) {
-				prompt(`Input is invalid. Kindly input Y / N. You have ${try} try left.`);
-				q1();
+			for (var retry = 3; retry <= 0; retry--) {
+				prompt(`Input is invalid. Kindly input Y / N. You have ${retry} try left.`);
+				q1(q1Ans);
 			}
 			alert(`You have keyed the wrong input too many times. You have to restart the game.`);
 		}
 	}
 }
 
-function yearQ2(){
-	var q2AnsInput = prompt(`${playerName}, are you going to fight the monster? (Y / N)`);
+function yearQ2(q2AnsInput){
+	q2AnsInput = prompt(`${playerName}, are you going to fight the monster? (Y / N)`);
 	var q2Ans = q2AnsInput.toLowerCase();
-	function q2(){
-		if (q2Ans === "Y"){
+	console.log(q2Ans);
+
+	function q2(q2Ans){
+		if (q2Ans === "y"){
 			score = score + 10;
 			console.log(score);
-			yearQ3();
+			yearQ3(q3AnsInput);
 		}
-		else if (q2Ans === "N") {
+		else if (q2Ans === "n") {
 			score = score - 10;
 			console.log(score);
-			yearQ3();
+			yearQ3(q3AnsInput);
 		}
 		else {
-			for (var try = 3; try <= 0; try--) {
-				prompt(`Input is invalid. Kindly input Y / N. You have ${try} try left.`);
-				q2();
+			for (var retry = 3; retry <= 0; retry--) {
+				prompt(`Input is invalid. Kindly input Y / N. You have ${retry} try left.`);
+				q2(q2Ans);
 			}
 			alert(`You have keyed the wrong input too many times. You have to restart the game.`);
 		}
@@ -148,22 +171,24 @@ function yearQ2(){
 
 }
 
-function yearQ3(){
-	var q3AnsInput = prompt(`${playerName},you are now in the enclosed cage. Will you kill the huge hungry monster? (Y / N)`);
+function yearQ3(q3AnsInput){
+	q3AnsInput = prompt(`${playerName},you are now in the enclosed cage. Will you kill the huge hungry monster? (Y / N)`);
 	var q3Ans = q3AnsInput.toLowerCase();
-	function q3(){
-		if (q3Ans === "Y"){
+	console.log(q3Ans);
+
+	function q3(q3Ans){
+		if (q3Ans === "y"){
 			score = score + 10;
 			alert(`CONGRATULATIONS! You have killed the monster! Your total score is ${score}`);
 		}
-		else if (q3Ans === "N") {
+		else if (q3Ans === "n") {
 			score = score - 10;
 			alert(`ON NO!! ${playerName}, you have been eaten by the huge hungry monster. Your total score is ${score}`);
 		}
 		else {
-			for (var try = 3; try <= 0; try--) {
-				prompt(`Input is invalid. Kindly input Y / N. You have ${try} try left.`);
-				q3();
+			for (var retry = 3; retry <= 0; retry--) {
+				prompt(`Input is invalid. Kindly input Y / N. You have ${retry} try left.`);
+				q3(q3Ans);
 			}
 			alert(`You have keyed the wrong input too many times. You have to restart the game.`);
 		}
