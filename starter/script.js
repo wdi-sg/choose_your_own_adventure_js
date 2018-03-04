99874f5ec82f84153966a9ede02b4d36c5535785
1ung <leewhe@hotmail.com>
2018-03-05 00:33:41 +0800

    incom

--- starter/script.js
+++ starter/script.js
@@ -9,80 +9,72 @@ function gameStart() {
 	while (isNaN(age) == true) {
 		playerAge = prompt("Please enter your age here");
 		age = parseInt(playerAge);
-		if (isNaN(age) == false) {
-			 startingQn = prompt(playerName + " is in a room with 2 doors, Black and White. Which will " + playerName + " choose? (B/W)").toLowerCase();
-			while (startingQn !== "b" && startingQn !== "w") {
-				startPrompt();
-				if (startingQn === "b") {
-					darkPath();
-				} else if (startingQn === "w") {
-					whitePath();
-				}
-			}
+	} if (isNaN(age) == false) {
+		startingQn = prompt(playerName + " is in a room with 2 doors, Black and White. Which will " + playerName + " choose? (B/W)").toLowerCase();
+		while (startingQn !== "b" && startingQn !== "w") {
+			startPrompt();
+		} if (startingQn === "b") {
+			darkPath();
+		} else if (startingQn === "w") {
+			whitePath();
 		}
 	}
 };
 
 
 function darkPath() {
-	var darkRoom = prompt(playerName + " entered a dark room and felt a breeze coming from the other end of the room but " + playerName + " sensed another presence as well. Search for the Light or run towards the Breeze? (L/B)").toLowerCase();
+	darkRoom = prompt(playerName + " entered a dark room and felt a breeze coming from the other end of the room but " + playerName + " sensed another presence as well. Search for the Light or run towards the Breeze? (L/B)").toLowerCase();
 	while (darkRoom !== "l" && darkRoom !== "b") {
 		darkPrompt();
-		if (darkRoom === "l") {
-			var lightSwitch = prompt("There was another deaf creature at the corner of the room, upon catching sight of " + playerName + " it aggressively lunged towards " + playerName + "." + " Fight back, try to Escape or Go back to previous options? (F/E/G)").toLowerCase();
-			while (lightSwitch !== "f" && lightSwitch !== "e" && lightSwitch !== "g") {
-				lightPrompt();
-				if (lightSwitch === "f") {
-					totalScore += 3
-					var battleCreature = alert("Good choice, the creature was weak and starving, " + playerName + " managed to grab hold of the nearest object and defeated it! " + playerName + " felt a strange surge of sensation through his body. Player Score = " + totalScore);
-				} else if (lightSwitch === "e") {
-					var attemptEscape = alert("Coward, There is nowhere to run! " + playerName + " got devoured by the starving creature. Game over! Player Score = " + totalScore);
-				} else if (lightSwitch === "g") {
-					var repeatDarkPath = prompt("Do you want to repeat the Dark room or back to the Beginning? (D/B)").toLowerCase();
-					while (repeatDarkPath !== "d" && repeatDarkPath !== "b") {
-						darkPathPrompt();
-						if (repeatDarkPath === "d") {
-							darkPath();
-						} else if (repeatDarkPath === "b") {
-							gameStart();
-						}
-					}
-				}
+	} if (darkRoom === "l") {
+		lightSwitch = prompt("There was another deaf creature at the corner of the room, upon catching sight of " + playerName + " it aggressively lunged towards " + playerName + "." + " Fight back, try to Escape or Go back to previous options? (F/E/G)").toLowerCase();
+		while (lightSwitch !== "f" && lightSwitch !== "e" && lightSwitch !== "g") {
+			lightPrompt();
+		} if (lightSwitch === "f") {
+			totalScore += 3
+			var battleCreature = alert("Good choice, the creature was weak and starving, " + playerName + " managed to grab hold of the nearest object and defeated it! " + playerName + " felt a strange surge of sensation through his body. Player Score = " + totalScore);
+		} else if (lightSwitch === "e") {
+			var attemptEscape = alert("Coward, There is nowhere to run! " + playerName + " got devoured by the starving creature. Game over! Player Score = " + totalScore);
+		} else if (lightSwitch === "g") {
+			repeatDarkPath = prompt("Do you want to repeat the Dark room or back to the Beginning? (D/B)").toLowerCase();
+			while (repeatDarkPath !== "d" && repeatDarkPath !== "b") {
+				darkPathPrompt();
+			} if (repeatDarkPath === "d") {
+				darkPath();
+			} else if (repeatDarkPath === "b") {
+				gameStart();
 			}
-		} else if (darkRoom === "b") {
-			var abyss = alert(playerName + " ran towards the direction of the breeze and ended up falling into an abyss. Game over! Player Score = " + totalScore);
 		}
+	} else if (darkRoom === "b") {
+		var abyss = alert(playerName + " ran towards the direction of the breeze and ended up falling into an abyss. Game over! Player Score = " + totalScore);
 	}
 };
 
 function whitePath() {
-	var whiteDoor = prompt(playerName + " entered a white empty room with a staff on an empty throne and another white door at the end of the room. Grab Staff or head towards the Door? (S/D)").toLowerCase();
+	whiteDoor = prompt(playerName + " entered a white empty room with a staff on an empty throne and another white door at the end of the room. Grab Staff or head towards the Door? (S/D)").toLowerCase();
 	while (whiteDoor !== "s" && whiteDoor !== "d") {
 		whitePrompt();
-		if (whiteDoor === "s") {
-			var sleepingMage = prompt("As " + playerName + " touches the staff, a Mage appears on the throne. The mage appears to be asleep. Wake him up, Steal his staff or Go back to previous options? (W/S/G)").toLowerCase();
-			while (sleepingMage !== "w" && sleepingMage !== "s" && sleepingMage !== "g") {
-				magePrompt();
-				if (sleepingMage === "w") {
-					totalScore += 5
-					var giftReward = alert("Oh you must be " + playerName + ", I have been waiting for you! Mage hands staff to " + playerName + " and vanishes. " + playerName + " acquired magical powers. Player Score = " + totalScore);
-				} else if (sleepingMage === "s") {
-					var angryMage = alert("The Mage woke up furious and spouted a few incantations, " + playerName + " has been burnt to crisp. Game over! Player Score = " + totalScore);
-				} else if (sleepingMage === "g") {
-					var repeatWhitePath = prompt("Do you want to repeat the White room or go back to the Beginning? (W/B)").toLowerCase();
-					while (repeatWhitePath !== "w" && repeatWhitePath !== "b") {
-						whitePathPrompt();
-						if (repeatWhitePath === "w") {
-							whitePath();
-						} else if (repeatWhitePath === "b") {
-							gameStart();
-						}
-					}
-				}
+	} if (whiteDoor === "s") {
+		sleepingMage = prompt("As " + playerName + " touches the staff, a Mage appears on the throne. The mage appears to be asleep. Wake him up, Steal his staff or Go back to previous options? (W/S/G)").toLowerCase();
+		while (sleepingMage !== "w" && sleepingMage !== "s" && sleepingMage !== "g") {
+			magePrompt();
+		} if (sleepingMage === "w") {
+			totalScore += 5
+			var giftReward = alert("Oh you must be " + playerName + ", I have been waiting for you! Mage hands staff to " + playerName + " and vanishes. " + playerName + " acquired magical powers. Player Score = " + totalScore);
+		} else if (sleepingMage === "s") {
+			var angryMage = alert("The Mage woke up furious and spouted a few incantations, " + playerName + " has been burnt to crisp. Game over! Player Score = " + totalScore);
+		} else if (sleepingMage === "g") {
+			repeatWhitePath = prompt("Do you want to repeat the White room or go back to the Beginning? (W/B)").toLowerCase();
+			while (repeatWhitePath !== "w" && repeatWhitePath !== "b") {
+				whitePathPrompt();
+			} if (repeatWhitePath === "w") {
+				whitePath();
+			} else if (repeatWhitePath === "b") {
+				gameStart();
 			}
-		} else if (whiteDoor === "d") {
-			var snow = alert(playerName + " enters the door into another dimension covered in never-ending snow. The door behind shuts and vanishes, " + playerName + " eventually froze to death. Game over! Player Score = " + totalScore);
 		}
+	} else if (whiteDoor === "d") {
+		var snow = alert(playerName + " enters the door into another dimension covered in never-ending snow. The door behind shuts and vanishes, " + playerName + " eventually froze to death. Game over! Player Score = " + totalScore);
 	}
 };
 
