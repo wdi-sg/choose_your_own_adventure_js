093e6bee94e74f221c16c4d630f8494708b23f9b
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-06-03 19:54:12 +0800

    tested completely. completed till part 3

--- main.js
+++ main.js
@@ -10,24 +10,31 @@ character = {
 
 
 var beer = function() {
-	var pints =  Math.floor((Math.random() * 5) + 1) 
+	var pints =  Math.floor((Math.random() * 4) + 1) 
 	return character.drunkedness += pints; 
 }
 
 var shots = function() {
-	var shots =  Math.floor((Math.random() * 7) + 1) 
+	var shots =  Math.floor((Math.random() * 5) + 1) 
 	return character.drunkedness += shots; 
+}
+
+var water = function() {
+	var shots =  Math.floor((Math.random() * 1) + 1) 
+	return character.drunkedness -= shots; 
 }
 
 var pickYourPoison = function(poison) {
 			switch (poison) {
 			 	case 'shots' : shots();
-			 	console.log(character.drunkedness);
+			 	console.log(character.drunkedness + ' Drinks');
 			 	break;
 			 	case 'beer' : beer();
-			 	console.log(character.drunkedness);
-			 	break;
-}
+			 	console.log(character.drunkedness + ' Drinks');
+			 	break
+			 	case 'water' : water();
+			 	console.log(character.drunkedness + ' Drinks');
+			 	break;}
 }
 
 
@@ -35,9 +42,7 @@ var pickYourPoison = function(poison) {
 //Level 0: where user inputs name name and the name is added to the character object.
 	character.name = prompt('What is your name tired soul?')
 
-//simple rule alerting the end of the game and showing the drunkedness score
- /*if (character.drunkedness > 10) {
- 	alert("Game over! Enjoy the hangover! You got " + character.drunkedness + "/10 drunk");	*/
+
 
 //Level 1:
 	var level1 = prompt('You have had a long week ' + character.name + '.' + ' Go home or Go hard?', 'home/hard' )
@@ -47,20 +52,20 @@ var pickYourPoison = function(poison) {
 	if (level1 == 'hard') 
 		//Level 2:
 		var level2 = prompt('Where are you going to meet your friends?', 'cq/hv')
-		if (level2 == 'cq' || level3 == 'cq') {
+		if (level2 == 'cq') {
 			character.location = 'Clarke Quay';
-			//Level 3: Run pickYourPoison fucntion. It runs till drunkedness >= 5.
+			//Level 3: Run pickYourPoison fucntion.
 			 var poison = prompt('You reach Clarke Quay. Your friends urge you to to pick your poison!', 'beer/shots')
 			 pickYourPoison(poison)
-			 while (character.drunkedness < 5) { poison = prompt('Still sober! Drink somemore!', 'beer/shots')
+			 while (character.drunkedness < 6) { poison = prompt('Still sober! Drink somemore!', 'beer/shots')
 			 pickYourPoison(poison)
 				}
 				//Level 4: if drunkedess from previous while loop > 8 game over.
 				// else character is given option to proceed home or head to hv.
-				if (character.drunkedness > 10) {
+				if (character.drunkedness >= 10) {
  				alert("Enjoy the hangover! You got " + character.drunkedness + "/10 drunk")}
  				else {
- 				var level3 = prompt('Everything is fuzzzxssyy now. Head home or not.' )
+ 				var level3 = prompt('Everything is fuzzzxssyy now. Head home or not.', 'home/hv' )
  				if (level3 == 'home'){
 				alert('You know when to quit! You got ' + character.drunkedness + "/10 drunk")
 				}
@@ -71,22 +76,42 @@ var pickYourPoison = function(poison) {
 
 		if (level2 == 'hv' || level3 == 'hv')	{
 		character.location = 'Holland Village';
-			//Level 3: Run pickYourPoison fucntion. It runs till drunkedness >= 5.
+			//Level 3: Run pickYourPoison fucntion.
 			 var poison = prompt('You reach Holland Village. Your friends urge you to to pick your poison!', 'beer/shots')
 			 pickYourPoison(poison)
-			 while (character.drunkedness < 5) { poison = prompt('Still sober! Drink somemore!', 'beer/shots')
+			 while (character.drunkedness < 6) { poison = prompt('Still sober! Drink somemore!', 'beer/shots')
 			 pickYourPoison(poison)
 				}
-				//Level 4: if drunkedess from previous while loop > 8 game over.
+				//Level 4: if drunkedess from previous while loop > 10 game over.
 				// else character is given option to proceed home or head to hv.
-				if (character.drunkedness > 10) {
+				if (character.drunkedness >= 10) {
  				alert("Enjoy the hangover! You got " + character.drunkedness + "/10 drunk")}
  				else {
- 				var level3 = prompt('Everything is fuzzzxssyy now. Head home or not.' )
+ 				var level3 = prompt('Everything is fuzzzxssyy now. Head home or not.', 'home/cq')
  				if (level3 == 'home'){
 				alert('You know when to quit! You got ' + character.drunkedness + "/10 drunk")
 				}
  				}
 		}
+		//repititive code block. Need to improve, so that this block is not required and that the previous 2 can be looped.
+		if (level3 == 'cq') {
+			character.location = 'Clarke Quay';
+			//Level 3: Run pickYourPoison fucntion. It runs till drunkedness >= 6.
+			 var poison = prompt('You reach Clarke Quay. Your friends urge you to to pick your poison!', 'beer/shots')
+			 pickYourPoison(poison)
+			 while (character.drunkedness < 6) { poison = prompt('Still sober! Drink somemore!', 'beer/shots')
+			 pickYourPoison(poison)
+				}
+				//Level 4: if drunkedess from previous while loop >= 10 game over.
+				// else character is given option to proceed home or head to hv.
+				if (character.drunkedness >= 10) {
+ 				alert("Enjoy the hangover! You got " + character.drunkedness + "/10 drunk")}
+ 				else {
+ 				var level3 = prompt('Everything is fuzzzxssyy now. Head home or not.', 'home/hv' )
+ 				if (level3 == 'home'){
+				alert('You know when to quit! You got ' + character.drunkedness + "/10 drunk")
+				}
+ 				}
+		}		
 
 	
