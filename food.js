507d6dc05cef7bf79bd2ba9f73b3d76828b1681c
mervyns <mervyns@gmail.com>
2018-09-07 21:21:15 +0800

    refactored the code to use the forEach function to loop through the food items and match the choices

---
+++ food.js
@@ -0,0 +1,87 @@
+var food = {
+    "items": [{
+            "foodtype": "chicken rice",
+            "shop": "hainanese delight",
+            "location": "Level 5, Far East Plaza",
+            "type": "rice",
+            "subtype": "dry"
+        },
+        {
+            "foodtype": "duck rice",
+            "shop": "kam's delight",
+            "location": "Level 2, Pacific Plaza",
+            "type": "rice",
+            "subtype": "dry"
+        },
+        {
+            "foodtype": "nasi padang",
+            "shop": "Malay Stall",
+            "location": "Basement 1, Orchard Towers",
+            "type": "rice",
+            "subtype": "dry"
+        },
+        {
+            "foodtype": "Plain Salad",
+            "shop": "Plain Salad Shop",
+            "location": "Basement 1, Orchard Towers",
+            "type": "salad",
+            "subtype": "dry"
+        },
+        {
+            "foodtype": "Salad with Steak",
+            "shop": "Salad Stop",
+            "location": "Basement 1, Tang Plaza",
+            "type": "salad",
+            "subtype": "dry"
+        },
+        {
+            "foodtype": "Salad with Salmon Fillet",
+            "shop": "Salad Stop 1",
+            "location": "Basement 1, Tang Plaza",
+            "type": "salad",
+            "subtype": "dry"
+        },
+        {
+            "foodtype": "Ramen",
+            "shop": "Tsuta Ramen",
+            "location": "Level 1, Pacific Plaza",
+            "type": "noodles",
+            "subtype": "soup"
+        },
+        {
+            "foodtype": "Fishball Noodle Soup",
+            "shop": "Eat.",
+            "location": "Basement 1, Far East Plaza",
+            "type": "noodles",
+            "subtype": "soup"
+        },
+        {
+            "foodtype": "Wanton Noodle Soup",
+            "shop": "Grafitti Cafe",
+            "location": "Basement 1, Far East Plaza",
+            "type": "noodles",
+            "subtype": "soup"
+        },
+        {
+            "foodtype": "Tenzaru Soba",
+            "shop": "Posh Jap Restaurant",
+            "location": "Shaw Towers",
+            "type": "noodles",
+            "subtype": "dry"
+        },
+        {
+            "foodtype": "Fried Hokkien Noodles",
+            "shop": "Great Hokkien Noodles",
+            "location": "Newton Circus",
+            "type": "noodles",
+            "subtype": "dry"
+        },
+        {
+            "foodtype": "Dry Wanton Noodles",
+            "shop": "Grafitti Cafe",
+            "location": "Basement 1, Far East Plaza",
+            "type": "noodles",
+            "subtype": "dry"
+        },
+    ]
+}
--- script.js
+++ script.js
@@ -1,16 +1,49 @@
+// Prompt the player for their name
 var nameOfPlayer = prompt("Hello, what is your name?")
-
+// Prompt the player on their choice of food
 var foodChoice = prompt("Nice to meet you, " + nameOfPlayer + ". Would you like to have noodles, rice, salad, or something else?")
-
+console.log(food)
+// Logic for food choices 
 if (foodChoice.toLowerCase() === "noodles") {
     var noodleChoice = prompt("Would you like soup or dry noodles?")
-    console.log(noodleChoice)
+    // logic if choose soup noodles
+    if (noodleChoice.toLowerCase() === "soup") {
+        var soupNoodleChoice = prompt("Would you like to have fishball noodles, ramen or wanton soup noodles?")
+        // Set forEach function to loop through the entire object showing the foods and returns something when it finds a match
+        var thingThatHappensForEachValue = function(food) {
+            if (food.foodtype.toLowerCase().includes(soupNoodleChoice)) {
+                console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location)
+                // Logic if choose dry noodles
+            }
+        };
+        food.items.forEach(thingThatHappensForEachValue)
+    } else if (noodleChoice.toLowerCase() === "dry") {
+        var dryNoodleChoice = prompt("Would you like to have dry wanton noodle, fried hokkien noodles or tenzaru soba")
+        // Set forEach function to loop through the entire object showing the foods and returns something when it finds a match
+        var thingThatHappensForEachValue = function(food) {
+            if (food.foodtype.toLowerCase().includes(dryNoodleChoice)) {
+                console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location)
+            }
+        };
+        food.items.forEach(thingThatHappensForEachValue);
+    }
 } else if (foodChoice.toLowerCase() === "rice") {
-    var riceChoice = prompt("What kinda rice do you like?")
-    console.log(riceChoice)
+    var riceChoice = prompt("What rice do you want? Fried rice, Chicken Rice, Duck Rice, Nasi Padang?")
+    var thingThatHappensForEachValue = function(food) {
+        if (food.foodtype.toLowerCase().includes(riceChoice)) {
+            console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location)
+        }
+    };
+    food.items.forEach(thingThatHappensForEachValue);
 } else if (foodChoice.toLowerCase() === "salad") {
-    var saladchoice = prompt("What would you like your salad with?")
-    console.log(saladchoice)
+    var saladChoice = prompt("What would you like your salad with? Steak, Fish or nothing?")
+    var thingThatHappensForEachValue = function(food) {
+        if (food.foodtype.toLowerCase().includes(saladChoice)) {
+            console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location)
+        }
+    };
+    food.items.forEach(thingThatHappensForEachValue);
+
 } else {
     console.log("Would you like something else?")
 }
