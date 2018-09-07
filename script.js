c0cba3c86cd15d543ef46d2fb1d113af51e00f72
mervyns <mervyns@gmail.com>
2018-09-07 21:40:32 +0800

    added calories points for food intake

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 var nameOfPlayer = prompt("Hello, what is your name?")
 // Prompt the player on their choice of food
 var foodChoice = prompt("Nice to meet you, " + nameOfPlayer + ". Would you like to have noodles, rice, salad, or something else?")
+// Introduced Calories as the points systems
+var caloriePoints = 0;
+
 console.log(food)
 // Logic for food choices 
 if (foodChoice.toLowerCase() === "noodles") {
     var noodleChoice = prompt("Would you like soup or dry noodles?")
     // logic if choose soup noodles
     if (noodleChoice.toLowerCase() === "soup") {
-        var soupNoodleChoice = prompt("Would you like to have fishball noodles, ramen or wanton soup noodles?")
+        var soupNoodleChoice = prompt("Would you like to have fishball noodles, ramen or Seafood soup noodles?")
         // Set forEach function to loop through the entire object showing the foods and returns something when it finds a match
         var thingThatHappensForEachValue = function(food) {
             if (food.foodtype.toLowerCase().includes(soupNoodleChoice)) {
-                console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location)
+                caloriePoints = caloriePoints + parseInt(food.calories)
+                console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location + ". You will gain " + caloriePoints + " calories after eating this.")
                 // Logic if choose dry noodles
             }
         };
@@ -22,7 +26,8 @@ if (foodChoice.toLowerCase() === "noodles") {
         // Set forEach function to loop through the entire object showing the foods and returns something when it finds a match
         var thingThatHappensForEachValue = function(food) {
             if (food.foodtype.toLowerCase().includes(dryNoodleChoice)) {
-                console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location)
+                caloriePoints = caloriePoints + parseInt(food.calories)
+                console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location + ". You will gain " + caloriePoints + " calories after eating this.")
             }
         };
         food.items.forEach(thingThatHappensForEachValue);
@@ -31,7 +36,8 @@ if (foodChoice.toLowerCase() === "noodles") {
     var riceChoice = prompt("What rice do you want? Fried rice, Chicken Rice, Duck Rice, Nasi Padang?")
     var thingThatHappensForEachValue = function(food) {
         if (food.foodtype.toLowerCase().includes(riceChoice)) {
-            console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location)
+            caloriePoints = caloriePoints + parseInt(food.calories)
+            console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location + ". You will gain " + caloriePoints + " calories after eating this.")
         }
     };
     food.items.forEach(thingThatHappensForEachValue);
@@ -39,7 +45,8 @@ if (foodChoice.toLowerCase() === "noodles") {
     var saladChoice = prompt("What would you like your salad with? Steak, Fish or nothing?")
     var thingThatHappensForEachValue = function(food) {
         if (food.foodtype.toLowerCase().includes(saladChoice)) {
-            console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location)
+            caloriePoints = caloriePoints + parseInt(food.calories)
+            console.log("You can go eat " + food.foodtype + " at " + food.shop + " located at " + food.location + ". You will gain " + caloriePoints + " calories after eating this.")
         }
     };
     food.items.forEach(thingThatHappensForEachValue);
