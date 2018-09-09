c2da7b6e780f63efc048c81bed5a71a668702f4f
ah5ive <ah5ive@gmail.com>
2018-09-09 21:54:35 +0800

    part 1 officer test  give up on return prompt if name is not fiiled

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,14 @@
 // put code here!
-//creat a function for user name inputs
-// var myname = function(){
+
+
+
+function myname() {
     var nameEnter = prompt("What is you name?");
-// }
-
-var cellbreak = prompt((nameEnter)+ ", You got kidnapped and you found yourself\nin this room, the door is open! You walk out of the door,\ninfront of you are three ways to go?\nWhich way to go? (Press L to turn left, R to right and C to walk straight)");
-
+    return nameEnter;
+};
+//console.log(myname());
+var cellbreak = prompt(myname() + ", You got kidnapped and you found yourself\nin this room, the door is open! You walk out of the door,\ninfront of you are three ways to go?\nWhich way to go? (Press L to turn left, R to right and C to walk straight)");
+//
 if (cellbreak === ("L" && "l")){//saw kidnapper
     var cellbreakkidnapper = prompt("You saw the kidnapper running towards you!\nWhat would you do? Press F to fight him?\n Press R to run away.");
     } else if (cellbreak === ("C" && "c")){//saw 2 doors
@@ -14,20 +17,22 @@ if (cellbreak === ("L" && "l")){//saw kidnapper
         var cellbreakpromptmate = prompt("You saw someone running towards you");
     } else {
         alert("You decide to quit the game");
+        // myname();
     }
+
 
 //1st left turn saw officer
 
 if (cellbreakkidnapper === ("F" && "f")){
     var searchkidnapper = prompt("You caught the kidnapper unprepared, you knock him down, you found a bag. You search the bag you found 2 small boxes.\nChoose boxes Press A for box 1 and Press B for box 2");
     } else {cellbreakkidnapper === ("R" && "r");
-    var runaway = prompt("The kidnapper saw you running away, he took out the gun and shoot you. You are died. Press A to restart or Press Q to quit.");
-        }
+    var runaway = prompt("The kidnapper saw you running away, he took out the gun and shoot you. You are died. Press S to restart or Press Q to quit.");
+        };
 
-// if (runaway === ("a" && "A")){
-//    console.log("WTF");
+// if (runaway === ("s" && "S")){
+//    alert("WTF");
 // }
-// else {runaway === ("q" && "Q");
+// else {runaway === "q" && "Q"){
 // alert("You have quit the game.");
 // }
 
