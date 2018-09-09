45bf0c360817f6ef92c3eb80a014ade7e18a87c3
shroomcrumpet <kencheng3@gmail.com>
2018-09-09 16:52:30 +0800

    Added first 'room', working inventories

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,19 @@
 console.log ("functions.js running");
 
+function checkGold (inv) {
+
+    if (inv.gold > 0) {
+        return alert (`You currently have ${inv.gold} Gold in your coin pouch.`)
+    } else if (inv.gold === 0) {
+        return alert (`Your coin pouch is empty!`)
+    }
+};
+
+function checkBackpack (inv) {
+
+    if (inv.bag.length > 0) {
+        return alert (`You currently have ${inv.bag.toString()} in your backpack.`)
+    } else if (inv.bag.length === 0) {
+        return alert (`Your backpack is empty!`)
+    }
+};
--- js/script.js
+++ js/script.js
@@ -1,33 +1,55 @@
 console.log ("script.js running");
 
-var playerName = prompt("Greetings, adventurer. What is your name?");
+var playerName = prompt("Greetings, adventurer. What is your name?").toUpperCase();
+
+var inventory = {
+    gold: 0,
+    bag:[]
+};
 
 
-Awaken with no memory, choose path 1 or 2, or check coin pouch, or check backpack
+while (true) {
 
-while choice = checks, loop prompt
+    var choice1 = parseInt(prompt(playerName + ", you awaken in a forest clearing with no memory of past events. You notice that you are carrying a backpack, and have a coin pouch strapped to your waist. Looking around, you can make out two dirt paths leading away from the clearing: one to the left, and another to the right. What would you like to do?\n\n[1] Take the path on the left.\n[2] Take the path on the right.\n[3] Look inside your backpack.\n[4] Look inside your coin pouch.\n\n(Please enter corresponding number for choice)"));
 
-    if check coins
+    if (choice1 === 1 || choice1 === 2) {
+        break;
+    } if (choice1 === 3) {
+        checkBackpack (inventory);
+    } if (choice1 === 4) {
+        checkGold (inventory);
+    }
+};
 
-        run coincheck function, repeat prompt
+if (choice1 === 1) {
+    alert("xxx");
+}
 
-    if check backpack
-
-        run checkbackpack function, repeat prompt
-
-    if path 1
-
-        found some coins
-
-            pick up coins?
-
-            path 1A, 1B, check coins, check backpack
-
-
-    if path 2
-
-        found a rusty dagger
-
-            pick up dagger?
-
-            path 2A, 2B, check coins, check backpack
+// Awaken with no memory, choose path 1 or 2, or check coin pouch, or check backpack
+//
+// while choice = checks, loop prompt
+//
+//     if check coins
+//
+//         run coincheck function, repeat prompt
+//
+//     if check backpack
+//
+//         run checkbackpack function, repeat prompt
+//
+//     if path 1
+//
+//         found some coins
+//
+//             pick up coins?
+//
+//             path 1A, 1B, check coins, check backpack
+//
+//
+//     if path 2
+//
+//         found a rusty dagger
+//
+//             pick up dagger?
+//
+//             path 2A, 2B, check coins, check backpack
