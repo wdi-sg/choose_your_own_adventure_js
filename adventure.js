f100eb073f6880b3f7c9917b14de26641eecbc99
audthenerd <aud.kow88@gmail.com>
2018-09-09 22:01:31 +0800

    Updated complete game structure. But loop is not working

--- adventure.js
+++ adventure.js
@@ -1,188 +1,338 @@
+//********************************************************************
+//WELCOME & SORTING
 var welcome = prompt("Welcome to Hogwarts, School of Magic! Please enter your name to start.");
 var userName = welcome.charAt(0).toUpperCase() + welcome.slice(1);
 
-// var sortMsg1 = alert(`Pleasure to meet you, ${userName}! \n
-// Now it's time to sort you into your house! \n
-// Click "OK" to let The Sorting Hat do his job!`);
+var sortMsg1 = alert(`Pleasure to meet you, ${userName}! \n
+Now it's time to sort you into your house! \n
+Click "OK" to let The Sorting Hat do his job!`);
+
+//The Sorting Hat: for user to get randomly assigned house.
+var house = ["Gryffindor", "Slytherin", "HufflePuff", "Ravenclaw"];
+   var randomHouse = Math.floor(Math.random()*house.length);
+   var userHouse = house[randomHouse];
+   console.log(userHouse);
+//********************************************************************
+
+//MESSAGES ARENA
+//MAIN: For user to choose where to go.
+var message = `Congratulations, you belong to ${userHouse}!
+
+Now, where would you like to go? (D/H/L/M/W)
+D - Diagon Alley. Haven’t seen much of it. I wanna explore!
+L - Lessons please. I take my studies very seriously.`;
+//M - Ministry of Magic. I'm with Dumbledore's Army, man!
+//W - Just wander around Hogwarts.
+//H - Hagrid’s Hut. Miss the dude man!`;
 
 
-
-// // //The Sorting Hat: for user to get randomly assigned house.
-// // var house = ["Gryffindor", "Slytherin", "HufflePuff", "Ravenclaw"];
-// //    var randomHouse = Math.floor(Math.random()*house.length);
-// //    var userHouse = house[randomHouse];
-// //    console.log(userHouse);
-
-
-
-// //For user to choose where to go.
-// var mainMenu = prompt(`Now, where would you like to go? (D/H/L/M/W)
-// D - Diagon Alley. Haven’t seen much of it. I wanna explore!
-// H - Hagrid’s Hut. Miss the dude man!
-// L - Lessons please. I take my studies very seriously.
-// M - Ministry of Magic. I'm with Dumbledore's Army, man!
-// W - Just wander around Hogwarts.
-// `)
-
-// var userGo = mainMenu.toLowerCase();
-
-
-
-//When user choses Diagon Alley.
-//if (userGo === "d"){
-    // var userDa = prompt(`*Zzzaaap!* Not bad, your apparition skill has improved! You are now in Diagon Alley.\n
-    // Choose a shop to go into (G/L/O/R)
-    // G - Gringotts Wizarding Bank
-    // L - Leaky Cauldron Pub
-    // O - Ollivander’s Wands
-    // R - Return to Hogwarts
-    // `);
-
-
-var counter = function(){
-    count ++;
-    if (count === 1){
-        break;
-    }
-};
-
-//------------------------------
-//Arrays area: All the arrays for all the variables of the switch cases go here.
-var passwordArr =[];
-var leakyCalArr = [];
-var wandArr = [];
-//------------------------------
-
-//This function consists of everything to do at Diagon Alley.
-var returnDiagon= function(){
- var count = 0;
-//For user to choose where to go:
-var userDa = prompt(`*Zzzaaap!* Not bad, your apparition skill has improved! You are now in Diagon Alley.\n
+var message2 = `*Zzzaaap!* Not bad, your apparition skill has improved! You are now in Diagon Alley.\n
     Choose a shop to go into (G/L/O/R)
     G - Gringotts Wizarding Bank
     L - Leaky Cauldron Pub
     O - Ollivander’s Wands
-    R - Return to Hogwarts
-    `);
+    R - Return to Hogwarts`;
 
-//Switch statements for the choices made above.
+var message3 = `Hermione runs up to you. "Hey, ${userName}! Which class shall we head to first?"\n
+    Choose a class to go to (P/D/R)
+    P - Potions
+    D - Defense against the Dark Arts
+    R - Return to Hogwarts`;
+
+
+var mGringotts = `As you push through the bronze doors of Gringotts, a goblin hustles over to you.\n
+    “What is the password?” he asks you with a smirk.\n
+
+    You say: (A/B)
+    A: The password is “buckbeak82790”.
+    B: I don’t know!`;
+
+var mPassA = `The goblin’s smile fades. He bows down hastily,
+“Forgive me sir, your vault has been prepared for your visit. Please follow me.”\n
+You are a VIP at Gringotts, it seems!`;
+
+var mPassB = `“THIEEEFF!”\n\nLittle did you know, you were carried out of the bank by a hoard of goblins. Better luck next time! `;
+
+
+
+var mLeaky = `Tom the innkeeper (who looks like an old gummy walnut) greets you:\n
+
+“Welcome, ${userName}! It’s been a while. Would you like some Butterbeer or a Room?”
+(B/R)`;
+
+
+var mLeakA = `A butterbeer appears in your hand. You drink it and feel your adrenaline rush!\n\nTime to go, many other places to explore!`;
+
+var mLeakB = `An elf appears out of nowhere and ushers you to an old, dingy room. There’s the smell of warm vanilla in the air. Time to sleep!`;
+
+
+
+var mOllie = `Ollivander is huddled over a chest of worn out wands when you arrive. \n
+“These wands may be cursed. Sorting them out will take time!” he mutters under his breath.\n
+Do you: Come back later or Pick up a wand? (C/P) `;
+
+ var mComeBack = `Ollivander seems busy. You'll come back later.`;
+
+var mPickUp = `Fearless as you are, you picked up one of the wands.\nYou feel a pull on your belly button and.....\n\n “PPOOOFFFF!”\n\nYou find yourself in a vault full of gold! Nice!`;
+
+
+
+var mReturnHog = `Nothing for me to do here. I want to return to Hogwarts.`;
+
+var mPotions = `The class has already started when Ron and you arrived. Professor Snape narrows his eyes at the both you.
+            "Ahhh, my test subjects have arrived,"he sneers. He points to two bottles on the table, "Pick one each and drink!"
+
+            Bottle A: Liquid is golden in colour and water-like.
+            Bottle B: Liquid is clear and viscous.
+
+            You choose: (A/B)`;
+
+var mDefense = `In DADA, you are assigned Draco Malfoy as a partner fot a duel assignment.
+        As you stand facing each other, Draco immediately raises his wand and shouts, "Stupefy!"
+
+        You shout:
+
+        C - Crucio!
+        E - Experlliarmus!
+        P - Protego!`;
+
+
+//********************************************************************
+
+var secondBranch = {
+        daObj: {gringArr: [], leakyArr: [], ollieArr: []},
+        lessonsObj: {potionsArr: [], defenseArr: []},
+        thirdObj: {passwordArr:[], leakChoiceArr:[], potionsArr2:[], daddArr:[], ollieChoiceArr:[]}
+};
+
+
+var combArr = secondBranch.daObj;
+var combArr2 = secondBranch.lessonsObj;
+var combArr3 = secondBranch.thirdObj;
+
+//********************************************************************
+
+
+//FIRST BRANCH: DIAGON ALLEY, LESSONS TIME OR MINISTRY OF MAGIC
+var userGoArr =[];
+var userGo = userGoArr.toString();
+
+var mainMenu = prompt(message);
+var userGo = mainMenu.toLowerCase();
+// userGoArr.push(userGoIn);
+
+
+var hogwardsArr = {
+    diagon: {
+        diagonPlaces: {
+                    gringotts: {
+                        gringOBj:mGringotts,
+                        passA: mPassA,
+                        passB: mPassB
+                    },
+                    leaky: {leakObj: mLeaky,
+                            leakA: mLeakA,
+                            leakB: mLeakB
+
+                    },
+                    ollie: {ollieObj: mOllie,
+                            ollieC: ollieCome,
+                            ollieP: olliePick
+                    },
+
+                    returnHog: mReturnHog
+                    }
+            },
+
+    lesson: {
+        lessonPlaces:{
+                    potions: mPotions,
+                    defense:mDefense
+                    }
+            }
+};
+
+
+while (true){
+
+// // Choosing places to go from Hogwarts: Switch statements.
+var userDaArr =[];
+var userLessonsArr = [];
+
+switch (userGo){
+    case "d":
+    var userDaIn = prompt(message2);
+    userDaArr.push(userDaIn);
+    break;
+
+    case "l":
+    var userLessonsIn = prompt(message3);
+    userLessonsArr.push(userLessonsIn);
+    break;
+};
+
+
+
+var userDa = userDaArr.toString();
+var userLessons = userLessonsArr.toString();
+
+
+if(userDa === false){
+    break;
+}
+// //********************************************************************
+// //SECOND BRANCH:
+// //CHOOSING PLACES AT DIAGON ALLEY: GRINGOTTS, LEAKY CAULDRON, OLLIVANDER'S OR GO BACK
+
 switch (userDa.toLowerCase()){
     case "g":
-        var gringotts = prompt(`As you push through the bronze doors of Gringotts, a goblin hustles over to you.\n
-    “What is the password?” he asks you with a smirk.\n
-    You say: (A/B)
-        A: The password is “buckbeak82790”.
-        B: I don’t know!
-        `);
-        passwordArr.push(gringotts);
-
+        var gringotts1 = prompt(mGringotts);
+        combArr.gringArr.push(gringotts1);
       break;
 
     case "l":
-        var leaky = prompt(`Tom the innkeeper (who looks like an old gummy walnut) greets you:\n\n“Welcome, ${userName}! It’s been a while."\nWould you like some Butterbeer or a Room?” (B/R)`);
-        leakyCalArr.push(leaky);
+        var leaky1 = prompt(mLeaky);
+        combArr.leakyArr.push(leaky1)
         break;
 
     case "o":
-        var ollie = prompt(`Ollivander is huddled over a chest of worn out wands when you arrive. \n\n“These wands may be cursed. Sorting them out will take time!” he mutters under his breath. \n\nDo you: Come back later or Pick up a wand? (C/P) `);
-        wandArr.push(ollie);
+        var ollie1 = prompt(mOllie);
+       combArr.ollieArr.push(ollie1);
         break;
+
+    case "r":
+        var gobackHogwarts = alert(mReturnHog);
+        console.log(gobackHogwarts);
+        break;
+
     }
-}
 
-returnDiagon();
 
-var password = passwordArr.toString();
-var leakyCal = leakyCalArr.toString();
-var wandAns = wandArr.toString();
+var password = combArr.gringArr.toString();
+var leakyCal = combArr.leakyArr.toString();
+var wandAns = combArr.ollieArr.toString();
 
-//From Gringgots Wizarding Bank: Password switch case
-    var password;
+//----------------------------
+
+
+switch(userLessons.toLowerCase()){
+
+    case "p":
+        var potions1 = prompt(mPotions);
+        combArr2.potionsArr.push(potions1);
+        break;
+
+    case "d":
+        var defense1 = prompt(mDefense);
+        combArr2.defenseArr.push(defense1);
+        break;
+
+    case "r":
+        var gobackHogwarts = alert(mReturnHog);
+        console.log(gobackHogwarts);
+        break;
+
+};
+
+var potionsChoice = combArr2.potionsArr.toString();
+var defenseChoice = combArr2.defenseArr.toString();
+
+// //********************************************************************
+// //FINAL BRANCH OPTIONS(1)
+
+//From Gringgots Wizarding Bank option
     switch (password.toLowerCase()) {
         case "a":
-            alert(`The goblin’s smile fades. He bows down hastily, “Forgive me sir, your vault has been prepared for your visit. Please follow me.”\n
-    You are a VIP at Gringotts, it seems!`);
+            var passA1 = alert(mGringotts);
+            combArr3.passwordArr.push(passA1);
+            console.log("passA:",passA1);
+            if(passA1 === undefined){
+                break;
+            }
         break;
 
         case "b":
-            alert(`“THIEEEFF!”\n\nLittle did you know, you were carried out of the bank by a hoard of goblins. Better luck next time! `);
-            while (true){
-            returnDiagon();
+            var passB1 = confirm(mPassB);
+            combArr3.passwordArr.push(passB1);
+             console.log("passB:",passB1);
+             if (passB1 !== true){
+                break;
             }
             break;
+
     };
 
-
-    var leakyCal;
+//From Leaky Cauldron option
     switch (leakyCal.toLowerCase()){
         case "b":
-        // var count = 0;
-            alert(`A butterbeer appears in your hand. You drink it and feel your adrenaline rush! Time to
-            go, many other places to explore!`);
-            while(true){
-            returnDiagon();
-            }
+            var leakA1 = confirm(mLeakA);
+            combArr3.leakChoiceArr.push(leakA1);
+
                 break;
 
         case "r":
-            alert(`An elf appears out of nowhere and ushers you to an old, dingy room. There’s the smell of warm vanilla in the air. Time to sleep!`);
+            var leakB1 = alert(mLeakB);
+            combArr3.leakChoiceArr.push(leakB1);
                 break;
     };
 
-
-    var wandAns;
+//From Ollivander option
     switch (wandAns.toLowerCase()){
         case "c":
-            alert(`Ollivander seems busy. You'll come back later.`);
+            var ollieCome = alert(mComeBack);
+            combArr3.ollieChoiceArr.push(ollieCome);
             break;
 
         case "p":
-            alert(`Fearless as you are, you picked up one of the wands.\nYou feel a pull on your belly button and “ppooofff!” you find yourself back at home! Weird!`);
+            var olliePick = confirm(mPickUp);
+           combArr3.ollieChoiceArr.push(olliePick);
+            break;
+    };
+
+var passwordChoice = combArr3.passwordArr.toString();
+var leakFinalChoice = combArr3.leakChoiceArr.toString();
+var ollieFinalChoice = combArr3.ollieChoiceArr.toString();
+
+// //********************************************************************
+
+
+    switch(potionsChoice.toLowerCase()){
+        case "a":
+            var potionA = alert(`Potion A`);
+            i++;
+            break;
+
+        case "b":
+            var potionB = alert(`Potion B`);
             break;
     };
 
 
+    switch (defenseChoice.toLowerCase()){
+
+        case "c":
+            var choiceFail = alert(`Fail`);
+            break;
+
+        case "e":
+            var choiceFail = alert(`Fail`);
+            break;
+
+        case "p":
+            var choicePass = alert(`Pass`);
+            i++;
+            break;
+    }
+
+};
 
 
-//----------------------------------------------
-//Diagon Alley Nav
-//----------------------------------------------
-
-// var leakyCauldron = prompt(`Tom the landlord, who looks like an old gummy walnut greets you, “Welcome, userName! It’s been a while.
-// Would you like some Butterbeer or a Room?” (B/R)`);
-
-// var ollivander = prompt(`Ollivander is huddled over a chest of worn out wands when you arrive.
-// “These wands may be cursed. Sorting them out will take time!” he mutters under his breath.
-
-//  Do you: (C/S)
-//  C: Come back later
-//  S: Stay?
-// `);
 
 
 
-    // if(userDa === "g"){
-    //     var gringotts = prompt(`As you push through the bronze doors of Gringotts, a goblin hustles over to you.\n
-    // “What is the password?” he asks you with a smirk.\n
-    // You say: (A/B)
-    //     A: The password is “buckbeak82790”,
-    //     B: I don’t know,
-    // `);
-    //     var password = gringotts.toLowerCase();
-
-    //             if (password === "a"){
-    //                 alert(`The goblin’s smile fades. He bows down hastily, “Forgive me sir,
-    //                     your vault has been prepared for your visit. Please follow me.”\n
-    //                     You are a VIP at Gringotts, it seems!`);
-
-    //             } else if (password === "b"){
-    //                 alert( `                       “THIEEEFF!”\n
-    //                     Little did you know, you were carried out of the bank by a hoard of goblins. Better luck next time! `);
-    //                             returnDiagon();
 
 
-    //                             }
-    //                         }
+
 
 
 
---
+++ spare.js
@@ -0,0 +1,41 @@
+// var userGo1 = [].toString().toLowerCase();
+// var mainMenu =function () {
+//     for (i=0;i<userGo.length;i++){
+//     var mainMenuIn = prompt(message);
+//     userGo.push(mainMenuIn[i]);
+//     }
+// }
+
+// var counter = function(){
+//     if (count = 1){
+//         break;
+//     }
+// };
+
+
+// var returnDiagon= function(){
+
+// //For user to choose where to go:
+// var userDa1 = prompt(`*Zzzaaap!* Not bad, your apparition skill has improved! You are now in Diagon Alley.\n
+//     Choose a shop to go into (G/L/O/R)
+//     G - Gringotts Wizarding Bank
+//     L - Leaky Cauldron Pub
+//     O - Ollivander’s Wands
+//     R - Return to Hogwarts
+//     `);
+// }
+
+
+// var message2 = `*Zzzaaap!* Not bad, your apparition skill has improved! You are now in Diagon Alley.\n
+//     Choose a shop to go into (G/L/O/R)
+//     G - Gringotts Wizarding Bank
+//     L - Leaky Cauldron Pub
+//     O - Ollivander’s Wands
+//     R - Return to Hogwarts
+//     `;
+
+// for (count=0; count<2; count++){
+// var userDa = prompt(message2);
+
+// This function consists of everything to do at Diagon Alley.
+// for (count = 0; count<2; count++){
