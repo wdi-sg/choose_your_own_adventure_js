3194ade5facf7f24ac4df9f1420a32949a5a1994
shroomcrumpet <kencheng3@gmail.com>
2018-09-09 15:16:37 +0800

    Added some pseudocode for basic structure

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log ("Works");
+console.log ("script.js running");
+
+var playerName = prompt("Greetings, adventurer. What is your name?");
+
+
+Awaken with no memory, choose path 1 or 2, or check coin pouch, or check backpack
+
+while choice = checks, loop prompt
+
+    if check coins
+
+        run coincheck function, repeat prompt
+
+    if check backpack
+
+        run checkbackpack function, repeat prompt
+
+    if path 1
+
+        found some coins
+
+            pick up coins?
+
+            path 1A, 1B, check coins, check backpack
+
+
+    if path 2
+
+        found a rusty dagger
+
+            pick up dagger?
+
+            path 2A, 2B, check coins, check backpack
