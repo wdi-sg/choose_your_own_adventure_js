554aba23e2c3743a98fd5e409b8c047c08bdb206
herda95 <herda58@gmail.com>
2019-03-16 19:33:59 +0800

    done with number choice

--- starter/script.js
+++ starter/script.js
@@ -1,71 +1,27 @@
 var name = prompt("Enter your name!")
 var gameChoice = prompt(`Hello, ${name}! Do you want to guess the order of numbers or letters? (N / L)`).toLowerCase();
 
-var numArray = [];
-var ansNumArray = [];
 var score;
 
 if (gameChoice === "n") {
-    var test = test(numArray, "number");
-    console.log(test);
-    //var firstNumber = askNumber("first", "number");
+    // ask user for number that wants to be shuffled
+    var numArray = promptNStore("number");   // this is an array
+    console.log("Ori array: " + numArray);
 
-   /* if (Number.isInteger(firstNumber) === true) {
-        var secondNumber = askNumber("second", "number");
+    // randomise the order of the current array
+    numArray = shuffle(numArray);
+    console.log("Shuffled array: " + numArray);
 
-        if (Number.isInteger(secondNumber) === true) {
-            var thirdNumber = askNumber("third", "number");
+    alert("TIME TO GUESS THE ORDERS OF THE NUMBER!");
 
-            if (Number.isInteger(thirdNumber) === true) {
-                eleToArray(numArray, firstNumber, secondNumber, thirdNumber);
+    // ask user to guess order and make an array
+    var ansNumArray = promptNStore("order");
+    console.log("Ans array: " + ansNumArray);
 
-                // randomise the order of the current array
-                numArray = shuffle(numArray);
-                console.log(numArray);
-
-                alert("TIME TO GUESS THE ORDERS OF THE NUMBER!")
-
-                var firstOrder = askNumber("first", "order");
-
-                if (Number.isInteger(firstOrder) === true) {
-                    var secondOrder = askNumber("second", "order");
-
-                    if (Number.isInteger(secondOrder) === true) {
-                        var thirdOrder = askNumber("third", "order")
-
-                        if (Number.isInteger(thirdOrder) === true) {
-                            eleToArray(ansNumArray, firstOrder, secondOrder, thirdOrder);
-
-                            console.log(ansNumArray);
-
-                            score = checkMatch(numArray, ansNumArray);
-                            if (score === 0) {
-                                alert(`TOO BAD! \nSCORE: ${score}`);
-                            } else {
-                               alert(`CONGRATS! \nSCORE: ${score}`);
-                            }
-
-
-
-
-
-                        } else {  // error for third order
-                            alert(numError());
-                        }
-                    } else {  // error for second order
-                        alert(numError());
-                    }
-                } else {  // error for first order
-                    alert(numError());
-                }
-
-            } else {   // error for third number
-                alert(numError());
-            }
-        } else { // error for second number
-            alert(numError());
-        }
-    } else {  // error for first number
-        alert(numError());
-    }*/
+    score = checkMatch(numArray, ansNumArray);
+    if (score === 0) {
+        alert(`TOO BAD! \nSCORE: ${score}`);
+    } else {
+     alert(`CONGRATS! \nSCORE: ${score}`);
+    }
 } // end of game choice
