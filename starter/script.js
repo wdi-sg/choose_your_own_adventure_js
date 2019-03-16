007f31671d7e31cb2ecd2f6077b01f44f29c8171
herda95 <herda58@gmail.com>
2019-03-16 20:32:56 +0800

    done letter choice

--- starter/script.js
+++ starter/script.js
@@ -12,7 +12,7 @@ if (gameChoice === "n") {
     numArray = shuffle(numArray);
     console.log("Shuffled array: " + numArray);
 
-    alert("TIME TO GUESS THE ORDERS OF THE NUMBER!");
+    alert("TIME TO GUESS THE ORDERS OF THE NUMBERS!");
 
     // ask user to guess order and make an array
     var ansNumArray = promptNStore("order");
@@ -23,5 +23,29 @@ if (gameChoice === "n") {
         alert(`TOO BAD! \nSCORE: ${score}`);
     } else {
      alert(`CONGRATS! \nSCORE: ${score}`);
-    }
-} // end of game choice
+    } // end of number game
+
+} else if (gameChoice === "l") {
+    // ask user for letters that wants to be shuffled
+    var letterArray = promptLStore("letter");
+    console.log("Ori array: " + letterArray);
+
+    // shuffle letter array
+    letterArray = shuffle(letterArray);
+    console.log("Shuffled array: " + letterArray);
+
+    alert("TIME TO GUESS THE ORDERS OF THE LETTERS!");
+
+    // ask user to guess order and make an array
+    var ansLetterArray = promptLStore("order");
+    console.log("Ans array: " + ansLetterArray);
+
+    score = checkMatch(letterArray, ansLetterArray);
+    if (score === 0) {
+        alert(`TOO BAD! \nSCORE: ${score}`);
+    } else {
+     alert(`CONGRATS! \nSCORE: ${score}`);
+    } // end of letter game
+
+
+}// end of game choice
