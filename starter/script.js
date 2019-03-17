15fc85116ddde2a80bc91ff5a94dc7f58661fc51
seeo <aragonlotr5@gmail.com>
2019-03-17 20:20:09 +0800

    created a conditional loop for checking of user name

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,24 @@
 // put code here!
 console.log("So it begins...");
-var userName = prompt('What is your name?');
+var userName = 'randon player';
+var askUserForName = function(){
+  userName = prompt('What is your name?','random player');
+  userName = userName.toString();
+  console.log(userName);
+  return userName;
+}
+//NOTE: if userName is left empty, console will say that userName.toString() cannot accept null property...
+
+console.log('end of ask for user name');
+
+askUserForName();
 //insert a confirm prompt here:
-alert('You are '+userName+".");
-var chooseFigther = prompt('WhAt hAvE yOu beCoMe??!\n gecko pecko\n black menace');
+if (isUserNameTrue = confirm('You are '+userName+".")){
+  console.log(userName);
+}else{
+  askUserForName();
+}
+
+console.log('Log the value of stored userName')
+console.log(userName);
+var chooseFigther = prompt('WhAt hAvE yOu beCoMe??!\n 1. gecko pecko\n 2. black menace\n 3. pointy boi\n 4. stabo the crabo\n 5. monky bizness\n 6. rhino threat\n 7. cock with a glock\n 8. siege\n 9. hopper with a chopper\n 10. cat with a gat');
