b57c3daac0dd233904fb57dad3721c1ecaa764ff
seeo <aragonlotr5@gmail.com>
2019-03-17 19:42:36 +0800

    prompt outputs can break sentences down line by line

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,6 @@
 // put code here!
 console.log("So it begins...");
 var userName = prompt('What is your name?');
+//insert a confirm prompt here:
 alert('You are '+userName+".");
-var chooseFigther = prompt('WhAt hAvE yOu beCoMe??!');
+var chooseFigther = prompt('WhAt hAvE yOu beCoMe??!\n gecko pecko\n black menace');
