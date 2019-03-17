ef4002316db50f994f5a5815a237024144840a70
seeo <aragonlotr5@gmail.com>
2019-03-17 18:42:07 +0800

    updated home page for choose your fighter, and also the name of each fighter if the image does not load. Created two prompts: one for user name, and one for user choice fighter

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,5 @@
 // put code here!
+console.log("So it begins...");
+var userName = prompt('What is your name?');
+alert('You are '+userName+".");
+var chooseFigther = prompt('WhAt hAvE yOu beCoMe??!');
