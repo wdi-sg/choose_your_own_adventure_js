9ccb1be5e82e64e7e696a4236f7a3f3b86da1547
kach92 <kenny.ang9206@gmail.com>
2019-06-29 19:22:03 +0800

    evertime user finished with a planet, they will come back to present time to choose distination again. completed morag and morag. morag has 2 levels of choices while vismor only 1 for now.

--- script.js
+++ script.js
@@ -2,75 +2,186 @@ console.log("hello script js");
 
 
 var goToVormir = false;
-var foundTimeStone = {found:false,name:"Time Stone"};
+var goToMorag = false;
+var timeStone = {found:false,name:"Time Stone"};
+var powerStone = {found:false,name:"Power Stone"};
+var moragChoiceCount = 0;
+var life = true;
 
 //just a function to return destinations choices
-var choicesDestinations = function(){
-    return "A : New York City\nB : Vormir\nC : Morag\nD : Asgard"
+var choicesDestinationsDisplay = function(){
+    return "A : New York City (Time/Mind/Space stones)\nB : Vormir (Soul Stone)\nC : Morag (Power Stone)\nD : Asgard (Reality Stone)"
 }
 
 //display the choices at the begining of the game
-display(choicesDestinations());
+display(`Welcome to Avengers:End Game!\n\nYou will be joining the Avengers as a new recruit to help them find the Infinity Stones by travelling back in time.\n\n Lets choose a destination:\n\n${choicesDestinationsDisplay()}`);
+
 
 //MAIN FUNCTION TO TAKE USER INPUT
 var inputHappened = function(currentInput){
 
-    if (goToVormir && !foundTimeStone.found){
-        choicesVormir(currentInput);
+    if(life){
+        if (goToVormir && !timeStone.found){
+            choicesVormir(currentInput);
 
+        }else if(goToMorag && !powerStone.found){
+            if(moragChoiceCount === 0)
+                choicesMorag(currentInput);
+            else if (moragChoiceCount === 1)
+                choicesMorag2(currentInput);
+        }else{
+            chooseDestinations(currentInput);
+        }
     }else{
-        chooseDestinations(currentInput);
+        display(gameOverMessage());
     }
 
 }
 
 
-//return invalid input message
-var invalidInputMessage = function(){
-    return "Please enter something that makes sense!"
-}
 
 //display choice of destinations
 var chooseDestinations = function(currentInput){
-    display(choicesDestinations());
+    display(choicesDestinationsDisplay());
     switch (currentInput){
         case "A":
             console.log("Test");
             break;
         case "B":
-            if(foundTimeStone.found){
-                display(`${stoneFoundMessage(foundTimeStone)}\n\n${choicesDestinations()}`);
+            if(timeStone.found){
+                display(`${stoneFoundMessage(timeStone)}\n\n${choicesDestinationsDisplay()}`);
                 break;
             }else{
                 goToVormir = true;
-                display("At Vormir, you found out that the Soul Stone requires one soul in exchange. Who will you sacrifice?\n\nA : Sacrifice yourself\nB : Sacrifice your partner\nC : Sacrifice Red Skull");
+                display(choicesVormirDescription()+"\n\n"+choicesVormirDisplay());
                 break;
             }
         case "C":
-            console.log("Test");
-            break;
+            if(powerStone.found){
+                display(`${stoneFoundMessage(powerStone)}\n\n${choicesDestinationsDisplay()}`);
+                break;
+            }else{
+                goToMorag = true;
+                display(choicesMoragDescription()+"\n\nWhat would you do?\n"+choicesMoragDisplay());
+                break;
+            }
 
         case "D":
             console.log("Test");
             break;
 
         default:
-            display(`${invalidInputMessage()}\n\n${choicesDestinations()}`);
+            display(`${invalidInputMessage()}\n\n${choicesDestinationsDisplay()}`);
             break;
 
     }
 }
 
+//take input for choices at Vormir
 var choicesVormir = function(currentInput){
     switch(currentInput){
         case "A":
             goToVormir = false;
-            foundTimeStone.found = true;
-            display("You get the Time Stone!\n\nYou head back to present time.\n\nChoose your next destination:\n"+choicesDestinations());
+            life = false;
+            display("You jumped off the cliff suddenly without discussing with the others.. thats heroic and all but, you died and never get to see the ending.\n\n"+gameOverMessage());
+            break;
+        case "B":
+            goToVormir = false;
+            life = false;
+            display("Your suggestion made Hawkeye pissed and he pushed you down the cliff instead\n\n"+gameOverMessage());
+            break;
+        case "C":
+            goToVormir = false;
+            timeStone.found = true
+            display("The Three of you pushed Red Skull down the cliff and gotten the Soul Stone. \n\nYes!!\n\n Your team head back to the present time.\n\nChoose your next destination\n\n"+choicesDestinationsDisplay());
+            break;
+        default:
+            display(`${invalidInputMessage()}\n\n${choicesVormirDisplay()}`);
             break;
     }
 }
 
+var choicesMorag = function(currentInput){
+    switch(currentInput){
+        case "A":
+            moragChoiceCount += 1;
+            display("Your distraction helped WarMachine to knock out StarLord. Great Job!\n\n"+choicesMoragDescription2()+"\n"+choicesMoragDisplay2());
+            break;
+        case "B":
+            moragChoiceCount += 1;
+            display("You win StarLord in the dance battle and he give you the key to the Ancient Temple voluntarily.\n\nGreat Job!\n\n"+choicesMoragDescription2()+"\n"+choicesMoragDisplay2());
+            break;
+        case "C":
+            life = false;
+            display("StarLord noticed the ambush and escaped. You staying quiet is actually useless zz..\n\n"+gameOverMessage())
+            break;
+        default:
+            display(`${invalidInputMessage()}\n\n${choicesMoragDisplay()}`);
+
+    }
+}
+
+var choicesMorag2 = function(currentInput){
+    switch(currentInput){
+        case "A":
+            life = false;
+            goToMorag = false;
+            display("Nebula doesn't like the idea and throat punched you. \n\n"+gameOverMessage());
+            break;
+        case "B":
+            goToMorag = false;
+            powerStone.found = true;
+            display("You managed to take the stone but it cost your hand. The team thank you for your effort but they think they had better ideas than this. Well too bad..\n\nYour team head back to the present time.\n\nChoose your next destination\n\n"+choicesDestinationsDisplay());
+            break;
+        case "C":
+            goToMorag = false;
+            powerStone.found = true;
+            display("The stone was successfully retrieved without much sacrifices.\n\nGreat Job!\n\nYour team head back to the present time.\n\nChoose your next destination\n\n"+choicesDestinationsDisplay());
+            break;
+        default:
+            display(`${invalidInputMessage()}\n\n${choicesMoragDisplay2()}`);
+    }
+}
+
+
+
+
+
+
+//display message if stone found
 var stoneFoundMessage = function(stoneObject){
     return `You already found the ${stoneObject.name}!\n\n Please choose another destination`;
+}
+
+var choicesVormirDescription = function(){
+    return "You went to Vormir with Hawkeye and BlackWidow, you found out that the Soul Stone requires one soul in exchange. Who will you sacrifice?";
+}
+
+var choicesVormirDisplay = function(){
+    return "A : Sacrifice yourself\nB : Sacrifice BlackWidow\nC : Sacrifice Red Skull"
+}
+
+var choicesMoragDescription = function(){
+    return "You went to Morag with WarMachine and Nebula.\n\nStarLord is dancing and on his way to the Ancient Temple. WarMachine is ready to knock him out."
+}
+
+var choicesMoragDisplay = function(){
+    return "A : Help him to distract StarLord\nB : Go for a dance battle with StarLord \nC : Stay quiet\n"
+}
+
+var choicesMoragDescription2 = function(){
+    return "All of you then enter the ancient temple and found out the power stone is surrounded by a magic force field\n\nWhat should you do?"
+}
+
+var choicesMoragDisplay2 = function(){
+    return "A : Let Nebula take the stone with her hand\nB : Take the stone with your hand\n C : Suggest WarMachine to control his suit to take the stone without his hand in it"
+}
+
+//return invalid input message
+var invalidInputMessage = function(){
+    return "Please enter something that makes sense!"
+}
+
+var gameOverMessage = function(){
+    return "Game Over\n\nRefresh to restart the game"
 }
