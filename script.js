f95f9159dcc060959db8a4edc8c19dd21614d552
Jonathan <jonathanbek93@gmail.com>
2019-06-30 02:03:36 +0800

    moved buttons to script.js!!!

--- script.js
+++ script.js
@@ -16,43 +16,67 @@ var choice2 = null;
 var choice3 = null;
 
 
-//NOTE: btn 3 can be hid but btns 1 and 2 cannot!
-// function to take text in choice and display it in button
+//function to create buttons, set id/class, insert text and push to buttonContainer
 var pushBtn1 = function(choice1){
-    if (choice1 !== null) {
-        document.getElementById('btn1').innerHTML = choice1;
-    } else {
-        document.getElementById('btn1').style.display = "none";
+    if (choice1 !== null){
+        var createBtn = document.createElement('button');
+    createBtn.setAttribute("class", "btn");
+    createBtn.setAttribute("id", "btn1");
+    createBtn.innerHTML = choice1;
+    document.getElementById('btnContainer').appendChild(createBtn);
     }
 }
+
 var pushBtn2 = function(choice2){
-    if (choice2 !== null) {
-        document.getElementById('btn2').innerHTML = choice2;
-    } else {
-        document.getElementById('btn2').style.display = "none";
+    if (choice2 !== null){
+        var createBtn = document.createElement('button');
+    createBtn.setAttribute("class", "btn");
+    createBtn.setAttribute("id", "btn2");
+    createBtn.innerHTML = choice2;
+    document.getElementById('btnContainer').appendChild(createBtn);
     }
 }
-//pushBtn3 has and if/else to hide button 3 if there is no choice 3
+
 var pushBtn3 = function(choice3){
-    if (choice3 !== null) {
-        document.getElementById('btn3').innerHTML = choice3;
-    } else {
-        document.getElementById('btn3').style.display = "none";
+    if (choice3 !== null){
+        var createBtn = document.createElement('button');
+    createBtn.setAttribute("class", "btn");
+    createBtn.setAttribute("id", "btn3");
+    createBtn.innerHTML = choice3;
+    document.getElementById('btnContainer').appendChild(createBtn);
     }
 }
 
-//each call, 3 btns
-// pushBtn1(choice1);
-// pushBtn2(choice2);
-// pushBtn3(choice3);
+// //each call, 3 btns
+// // pushBtn1(choice1);
+// // pushBtn2(choice2);
+// // pushBtn3(choice3);
 
-// all-in-one pushBtn function
+// // all-in-one pushBtn function
 var pushBtns = function(){
     pushBtn1(choice1);
     pushBtn2(choice2);
     pushBtn3(choice3);
 }
 //call: pushBtns();
+
+//test
+
+var test = function(time){
+    console.log(time);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 var resetPage = function(){
     storyText = null;
@@ -66,16 +90,6 @@ var resetPage = function(){
 }
 
 
-//test
-
-var test = function(time){
-    console.log(time);
-}
-
-
-
-
-
 
 var beforeSchool = function(){
     storyText = `You stand in front of 79 Anson Road with a tough decision to make. You've had a rough night and ${properTime} is way too early for anything. You haven't had breakfast. You're terrible at coding and you know it. After a minute of indecision, you...`;
@@ -86,4 +100,4 @@ var beforeSchool = function(){
 //call: schoolEvent();
 beforeSchool();
 pushBtns();
-pushMain(storyText);
+// pushMain(storyText);
