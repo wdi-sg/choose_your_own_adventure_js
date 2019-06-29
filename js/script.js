c5381c7d21e549352daf14d55df34545b1192d19
MARCYKAY <mahchinkok@gmail.com>
2019-06-29 17:07:39 +0800

    layout completed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("hello script js");
+
+var inputHappened = function(currentInput){
+  console.log( currentInput );
+  display( "WOW SOMETHING HAPPENED" );
+};
+
+
+function initGame() {
+  // init game upon loading of JS script
+}
