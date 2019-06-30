1e6c2e2113bc3ca00035b203da0839ce1e67d743
gitwenlei <ngwenlei@gmail.com>
2019-06-30 11:10:56 +0800

    Updated prompt msg

--- script.js
+++ script.js
@@ -8,40 +8,59 @@
 console.log("hello script js");
 
 var tempInput;
+var currentStage = 1;
 
 var yearChoice = ['2015', '1985', '1955'];
-var yearMsg = ["I see you're a fan of Back to the Future 2. Would you rather deal with Biff, or Griff? (B or G)", "Doc has already destroyed the Time Machine at this point. I guess you'll have to wait around until 2015. What name would you like to go by until then?", "I see you're a fan of Back to the Future 1. Your future Mom has just asked you to the Enchantment Under the Sea dance. What do you do?"];
+var yearMsg = ["I see you're a fan of Back to the Future 2. Would you rather deal with Biff, or Griff? **(B/G)**", "Doc has already destroyed the Time Machine at this point. I guess you'll have to wait around until 2015. What name would you like to go by until then?", "I see you're a fan of Back to the Future 1. Your future Mom has just asked you to the Enchantment Under the Sea dance. What do you do?**(Y/N/S)**"];
 
 var charChoice = ['B', 'G'];
-var charMsg = ["Hmm, interesting. Biff is angry and has a cane. Do you stand and fight, or run away like a coward?", "Griff is asking you if you are in, or out. What do you say?"];
+var charMsg = ["Hmm, interesting. Biff is angry and has a cane. Do you Stand and fight, or Run away like a coward? **(S/R)**", "Griff is asking you if you are in, or out. What do you say?**(I/O)**"];
+
+var fightFlightChoice = ['S', 'R'];
+var fightFlightMsg = ["Good choice. Biff is old and feeble at this point. You push him over and he falls in a pile of manure.", "You get away, but your future son Marty Jr. is heckled for the rest of his days for his dad's cowardice."];
 
 
 var inputHappened = function(currentInput){
     tempInput = currentInput.toUpperCase();
     timeTravel(tempInput);
-
 };
 
 var timeTravel = function (currentInput) {
     for (var i = 0; i < yearChoice.length; i++ ) {
-        // PLAYER CHOOSE YEAR
-        if (tempInput == yearChoice[i]) {
-            // DISPLAY CHOSEN YEAR MSG
-            display(yearMsg[i]);
-        }
+            // PLAYER CHOOSE YEAR
+            if (tempInput == yearChoice[i]) {
+                // DISPLAY CHOSEN YEAR MSG
+                display(yearMsg[i]);
+            }
         // CLEAR INPUT BOX
-    document.querySelector('#input').value="";
-    chooseChar(currentInput);
+        document.querySelector('#input').value="";
+        chooseChar(currentInput);
     }
 };
 
 var chooseChar = function (currentInput) {
-    for (var i = 0; i < charChoice.length; i++ ) {
-        // PLAYER CHOOSE CHARACTER
-        if (tempInput == charChoice[i]) {
-            // DISPLAY CHOSEN YEAR MSG
-            display(charMsg[i]);
-        }
+        for (var i = 0; i < charChoice.length; i++ ) {
+            // PLAYER CHOOSE CHARACTER
+            if (tempInput == charChoice[i]) {
+                // DISPLAY CHOSEN YEAR MSG
+                display(charMsg[i]);
+            }
+        // CLEAR INPUT BOX
+        document.querySelector('#input').value="";
+        stayRun(currentInput);
+    }
+};
+
+
+var stayRun = function (currentInput) {
+        for (var i = 0; i < fightFlightChoice.length; i++ ) {
+            // PLAYER CHOOSE CHARACTER
+            if (tempInput == fightFlightChoice[i]) {
+                // DISPLAY CHOSEN YEAR MSG
+                display(fightFlightMsg[i]);
+            }
+        // CLEAR INPUT BOX
+        document.querySelector('#input').value="";
     }
 };
 
