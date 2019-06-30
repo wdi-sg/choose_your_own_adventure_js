99337d9c2b4a5e77a0b650c4d8a2592e63092d8b
caspianzx <caspianzx@gmail.com>
2019-06-30 22:01:53 +0800

    updated logic error and added new functions to test for year <1955

--- script2.js
+++ script2.js
@@ -26,11 +26,17 @@ var response = {
     year1985to2014: {
         reply1: "Doc has already destroyed the Time Machine at this point. I guess you'll have to wait around until 2015. What name would you like to go by until then?"
     },
-    year1995to1984: {
+    year1955to1984: {
         reply1: "I see you're a fan of Back to the Future 1. Your future Mom has just asked you to the Enchantment Under the Sea dance. What do you do? (Y/N/S)",
         yes: "Creepy. I hope you have some backup plan in place to get out of this. Until then, you're stuck in 1955.",
         no: "Honorable. But this also means that your future Dad will never meet your Mom, and therefore you cannot exist.",
         setHer: "Interesting. You set up an elaborate plan for your future Dad to surprise your Mom by beating you up. Despite going horribly awry, the plan ultimately works. You may go back to your own time."
+    },
+    year1955: {
+        reply1: "I see you're a fan of Back to the Future 3. You've run out of gas and can't get back to your own time! How do you power the Time Machine? \n (H/M/T)",
+        horses: "Good idea, but no. The time machine needs to get to 88mph. 12 horsepower ain't gonna cut it.",
+        moonshine: "You'd be better off drinking the moonshine. Do not pass Go, do not collect $200. Stuck in 1855.",
+        train: "Good call! This plan seems to be working. But wait! Clara wants to go Back to the Future with you at the last moment. What do you do? \n (T/L)"
     }
 }
 
@@ -50,10 +56,16 @@ var inputHappened = function(currentInput){
         }else if (questionAnswered.length === 2 &&(yearAnswered == "2015")) {
             biffOrGriff (currentInput);
             questionAnswered.push("3");
-        }else if (questionAnswered.length === 2 &&(yearAnswered >=1985 || yearAnswered <= 2014)) {
+        }else if (questionAnswered.length === 2 &&(yearAnswered >=1985 && yearAnswered <= 2014)) {
             name1985to2014(currentInput);
             questionAnswered.push("3");
-            //edit this check counter and character name
+        }else if (questionAnswered.length === 2 &&(yearAnswered >1955 && yearAnswered <= 1984)) {
+            yesNoSet(currentInput);
+            questionAnswered.push("3");
+        }else if(questionAnswered.length === 2 && yearAnswered <=1955 ) {
+            //check counter with character
+            horseMoonTrain(currentInput);
+            questionAnswered.push("3");
         }else if (questionAnswered.length === 3 && (character == "B")) {
             standRun (currentInput);
         }else if (questionAnswered.length === 3 && (character == "G")) {
@@ -75,6 +87,13 @@ var checkForYear = function (currentInput) {
         display(response.year1985to2014.reply1);
         yearAnswered.push(parseInt(currentInput));
         console.log(yearAnswered);
+    } else if (year>1955 && year<= 1984) {
+        display(response.year1955to1984.reply1);
+        yearAnswered.push(parseInt(currentInput));
+        console.log(yearAnswered);
+    } else if (year <=1995) {
+        display(response.year1955.reply1);
+        yearAnswered.push(parseInt(currentInput));
     }
 }
 
@@ -115,4 +134,30 @@ var InOut = function (currentInput) {
 var name1985to2014 = function (currentInput) {
     var newName = currentInput;
     display ("Welcome to the future " + newName);
+}
+//create function for yes, no or set her up
+var yesNoSet = function (currentInput) {
+    if (currentInput == "Y") {
+        console.log(currentInput);
+        display(response.year1955to1984.yes);
+    } else if (currentInput == "N") {
+        console.log(currentInput);
+        display(response.year1955to1984.no);
+    } else if (currentInput == "S") {
+        console.log(currentInput);
+        display(response.year1955to1984.setHer);
+    }
+}
+//create function to. check for H, M, or T response
+var horseMoonTrain = function (currentInput) {
+    if (currentInput == "H") {
+        console.log(currentInput);
+        display(response.year1955.horses);
+    } else if (currentInput == "M") {
+        console.log(currentInput);
+        display(response.year1955.moonshine);
+    } else if (currentInput == "T") {
+        console.log(currentInput);
+        display(response.year1955.train);
+    }
 }
