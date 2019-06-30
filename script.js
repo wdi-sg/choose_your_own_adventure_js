47b71e3c35ec9a8c1c17429b905caafc31acf5fe
gitwenlei <ngwenlei@gmail.com>
2019-06-30 16:14:08 +0800

    Revert to last working script.js

--- script.js
+++ script.js
@@ -1,88 +1,101 @@
-/* Goal
- *  Program should display text&paths and show next text & paths based on selected path.
- *
- * Approach
- *  Each story has 'id', 'text' 'paths'
- *  Each path has 'target' which should point to specific story id and 'text' as display value.
- */
-console.log("Welcome to Back to the Future");
+// prompt for playerName - gameQuestion "What is your name?"
+// clear field after input entered
+// player enter name . Taka userInput and store into playerName.
+// prompt "Nice to meet you " + userInput. + " What year would you like to go to?"
+// player enter year . fun checkYear
 
-// START OF STORIES ARRAY//////////////////////
-var stories = [
-        // PART 1 (2015) - STORIES & PATHS
-        {
-          id: '2015',
-          text: "I see you're a fan of Back to the Future 2. You confronted with your nemesis",
-          paths: [
-            {target: 'b', text: "Would you rather deal with Biff"},
-            {target: 'g', text: "Would you rather deal with Griff?"}
-          ]
-        },
 
-        {
-          id: 'b',
-          text: "Hmm, interesting. Biff is angry and has a cane.",
-          paths: [
-            {target: 's', text: "Do you stand and fight?"},
-            {target: 'r', text: "Or run away like a coward?"}
-          ]
-        },
+console.log("hello script js");
 
-        {
-          id: 'g',
-          text: "Griff is asking you to join him for a secret rendezvous. What do you say?",
-          paths: [
-            {target: 'i', text: "Are you in?"},
-            {target: 'o', text: "Or are your out"}
-          ]
-        },
+var nextStage = 0;
+var msg = 0;
 
-        {
-          id: 'i',
-          text: "Bad call. Griff and his cronies rob the Hill Valley bank and frame you for it. No more time travel for you. THE END.",
-        },
+var yearChoice = ['2015', '1985', '1955'];
+var yearMsg = ["I see you're a fan of Back to the Future 2. Would you rather deal with Biff, or Griff? **(B/G)**", "Doc has already destroyed the Time Machine at this point. I guess you'll have to wait around until 2015. What name would you like to go by until then?", "I see you're a fan of Back to the Future 1. Your future Mom has just asked you to the Enchantment Under the Sea dance. What do you do?**(Y/N/S)**"];
 
-        {
-          id: 'o',
-          text: "Good call. You deck Griff in the jaw and run away. He gives chase on his hoverboard and ends up in a pile of manure.THE END.",
-        },
+var charChoice = ['B', 'G'];
+var charMsg = ["Hmm, interesting. Biff is angry and has a cane. Do you Stand and fight, or Run away like a coward? **(S/R)**", "Griff is asking you if you are in, or out. What do you say?**(I/O)**"];
 
-]// END OF STORIESS ARRAY//////////////////////
+var fightFlightChoice = ['S', 'R'];
+var fightFlightMsg = ["Good choice. Biff is old and feeble at this point. You push him over and he falls in a pile of manure.", "You get away, but your future son Marty Jr. is heckled for the rest of his days for his dad's cowardice."];
 
-var currentInput;
-var currentText;
+var inOutChoice = ['I', 'O'];
+var inOutMsg = ["Bad call. Griff and his cronies rob the Hill Valley bank and frame you for it. No more time travel for you.", "Good call. You deck Griff in the jaw and run away. He gives chase on his hoverboard and ends up in a pile of manure."];
+
 
 var inputHappened = function(currentInput){
-    playStory(stories, currentInput);
-    displayStory(currentText);
-    displayPath(currentInput)
+    var tempInput = currentInput.toUpperCase();
+    // CHECK STAGE PLAYER IS IN
+    // RUN TIMETRAVEL STAGE ONLY IF CURRENT STAGE IS 0
+    switch(nextStage){
+        case 0:
+            display("Please choose year: 2015, 1985, or 1955");
+            break;
+        case 1:
+            year(tempInput);
+            display(msg);
+            break;
+        case 2:
+            biffOrGriff(tempInput);
+            display(msg);
+            break;
+        case 3:
+            stayRun(tempInput);
+            display(msg);
+            break;
+        case 4:
+            inOrOut(tempInput);
+            display(msg);
+            break;
+        default:
+            display("please enter choice");
+    }
 };
 
-// START OF DISPLAYING STORIES
- // for (var i = 0; i < stories.length; i++) {
- //    if (stories[i].id == '2015') {
- //        console.log (stories[i].text); // text of path
- //    }
- // }
-var showPath = function () {
-    // var currentPath = [];
-    // show the paths available based on earlier choice
-    var myMsg = stories[0].paths[0].text;
-    console.log(myMsg);
-    //return paths.toString();
-}
 
-
- var playStory = function (stories, currentInput) {
-    for (var i = 0; i < stories.length; i++) {
-        // CHECK IF USER INPUT IS EQUAL TO STORY ID
-        if (stories[i].id === currentInput) {
-            currentText = stories[i].text;
-            showPath(currentInput);
-            return currentText;
-        } else {
-            currentText = "Please enter choice";
-            return currentText;
+var year = function (currentInput) {
+    for (var i = 0; i < yearChoice.length; i++ ) {
+        if (currentInput == yearChoice[i]) {
+            msg = yearMsg[i];
+            nextStage = 1;
+            return msg;
         }
     }
- };
+    // CLEAR INPUT BOX
+    document.querySelector('#input').value="";
+};
+
+var biffOrGriff = function (currentInput) {
+    for (var i = 0; i < charChoice.length; i++ ) {
+        if (currentInput == charChoice[i]) {
+            var msg = charMsg[i];
+            nextStage = 2;
+            return msg;
+        }
+    }
+    // CLEAR INPUT BOX
+    document.querySelector('#input').value="";
+};
+
+
+var stayRun = function (currentInput) {
+    for (var i = 0; i < fightFlightChoice.length; i++ ) {
+        if (currentInput == fightFlightChoice[i]) {
+            var msg = fightFlightMsg[i];
+            return msg;
+        }
+    }
+    // CLEAR INPUT BOX
+    document.querySelector('#input').value="";
+};
+
+var inOrOut = function (currentInput) {
+    for (var i = 0; i < inOutChoice.length; i++ ) {
+        if (currentInput == inOutChoice[i]) {
+            var msg = inOutMsg[i];
+            return msg;
+        }
+    }
+    // CLEAR INPUT BOX
+    document.querySelector('#input').value="";
+};
