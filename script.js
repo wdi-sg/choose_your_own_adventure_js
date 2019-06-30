bd259b5e920b2eb246ff6fd445bd05f74b742e61
gitwenlei <ngwenlei@gmail.com>
2019-06-30 12:18:08 +0800

    Added inOrOut function. Error with progressing with next stage.

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ var charMsg = ["Hmm, interesting. Biff is angry and has a cane. Do you Stand and
 var fightFlightChoice = ['S', 'R'];
 var fightFlightMsg = ["Good choice. Biff is old and feeble at this point. You push him over and he falls in a pile of manure.", "You get away, but your future son Marty Jr. is heckled for the rest of his days for his dad's cowardice."];
 
+var inOutChoice = ['I', 'O'];
+var inOutMsg = ["Bad call. Griff and his cronies rob the Hill Valley bank and frame you for it. No more time travel for you.", "Good call. You deck Griff in the jaw and run away. He gives chase on his hoverboard and ends up in a pile of manure."];
+
 
 var inputHappened = function(currentInput){
     var tempInput = currentInput.toUpperCase();
@@ -26,61 +29,82 @@ var inputHappened = function(currentInput){
     switch(currentStage){
         case 0:
             timeTravel(tempInput);
-            console.log(currentStage);
+            console.log("i'm in timeTravel");
             break;
         case 1:
             chooseChar(tempInput);
-            console.log(currentStage);
+            console.log("i'm in chooseChar");
             break;
         case 2:
             stayRun(tempInput);
-            console.log(currentStage);
+            console.log("i'm in stayRun");
             break;
+        case 3:
+            inOrOut(tempInput);
+            console.log("i'm in inOrOut");
+            break;
+        default:
+            display("please enter choice");
     }
 };
 
 
 var timeTravel = function (currentInput) {
-    currentStage = 1;
-
     for (var i = 0; i < yearChoice.length; i++ ) {
         // PLAYER CHOOSE YEAR
         if (currentInput == yearChoice[i]) {
             // DISPLAY CHOSEN YEAR MSG
-            display(yearMsg[i]);
+            var msg = yearMsg[i];
         }
     }
     // CLEAR INPUT BOX
     document.querySelector('#input').value="";
+    display(msg);
+    currentStage = 1;
 };
 
 var chooseChar = function (currentInput) {
-    currentStage = 2;
-
     for (var i = 0; i < charChoice.length; i++ ) {
         // PLAYER CHOOSE CHARACTER
         if (currentInput == charChoice[i]) {
             // DISPLAY CHOSEN YEAR MSG
-            display(charMsg[i]);
+            var msg = charMsg[i];
         }
     }
     // CLEAR INPUT BOX
     document.querySelector('#input').value="";
+    display(msg);
+    currentStage = 2;
 };
 
 
 var stayRun = function (currentInput) {
-    currentStage = 3;
-
     for (var i = 0; i < fightFlightChoice.length; i++ ) {
         // PLAYER CHOOSE CHARACTER
         if (currentInput == fightFlightChoice[i]) {
             // DISPLAY CHOSEN YEAR MSG
-            display(fightFlightMsg[i]);
+            var msg = fightFlightMsg[i];
+
         }
     }
     // CLEAR INPUT BOX
     document.querySelector('#input').value="";
+    display(msg);
+    currentStage = 3;
+};
+
+var inOrOut = function (currentInput) {
+    for (var i = 0; i < inOutChoice.length; i++ ) {
+        // PLAYER CHOOSE CHARACTER
+        if (currentInput == inOutChoice[i]) {
+            // DISPLAY CHOSEN YEAR MSG
+            var msg = inOutMsg[i];
+        }
+    }
+    // CLEAR INPUT BOX
+    document.querySelector('#input').value="";
+    display(msg);
+    currentStage = 3;
 };
 
 
