b95e8a243218dab27149474f033f77a36873841f
sharkyu3 <lowhuiyu@hotmail.com>
2019-06-30 18:47:25 +0800

    completed shelter, idol and chill paths

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ console.log ("Tally is " + survivorTally);
 var gamePath = 0;
 console.log ("Game path is " + gamePath);
 
+//var to track if player has immunity
+var immunity = false;
+console.log ("Player has immunity " + immunity);
+
 //welcome message to start game rolling
 display("Hi " + name + "! You have just arrived on the beach with your new tribe. What would you do first?" + "\n" + "1. Make shelter" + "\n" + "2. Make alliances" + "\n" + "3. Search for an idol" + "\n" + "4. Chill" + "\n" + "(type: shelter/alliances/idol/chill to select option)");
 
@@ -55,16 +59,28 @@ var wood = {
     adjustTally: + +2,
 };
 
+var build = {
+    displayMsg1: "How will you contribute to the shelter build?" + "\n" + "1. Chop the wood to the correct length" + "\n" + "2. Weave leaves for the roof" + "\n" + "(type: chop/weave to select option)",
+    adjustTally: + +1,
+};
+
+var water = {
+    displayMsg1: "You search around the watering hole but find nothing. You hear people approaching and they are whispering. Do you want to:" + "\n" + "1. Stay and try to listen to the conversation" + "\n" + "2. Run away and head back to camp" + "\n" + "(type: stay/run to select option)",
+};
+
 var hardwork = {
-    displayMsg1: "You manage to gather lots of useful material and your tribe mates see you as a hardworking individual! Great job!",
+    displayMsg1: "Your tribe mates see you as a hardworking individual! Great job!",
     adjustTally: + +1,
 };
 
 var idolClue = {
-    displayMsg1: "**WELL DONE " + name + "! You have found a clue to a hidden immunity idol!**",
-    adjustTally: + +5,
+    displayMsg1: "**WELL DONE " + name + "! You have found a clue to a hidden immunity idol!**" + "\n" + "It reads: 'The idol is hidden in plain sight under the tribe flag. However, it will only be there for the next 1 hour.'" + "\n" + "Do you want to make a dash for it and try to get the idol OR forgo the opportunity as you don't want to be an open target?" + "\n" + "(type: dash/forgo to select option)",
+    adjustTally: + +2,
 };
-//"You have found a clue to a hidden immunity idol!" + "\n" + "|@@@@|     |####|" + "\n" + "|@@@@|     |####|"
+
+var medEvac = {
+    displayMsg1: "Oh no! You were chopping wood when the machete slipped from your hand and gave you a nasty gash on your forehead!" + "\n" + "Unfortunately, the medical team has made an assessment that further treatment is required and you will be medically evacuated from the game. Get well soon " + name + "!",
+};
 
 var eliminated = {
     displayMsg1: "Jeff Probst: " + name + ", your tribe has spoken.",
@@ -106,12 +122,17 @@ var inputHappened = function(currentInput) {
     }else if (currentInput === "build" && gamePath === "shelterPath") {
         display(build.displayMsg1);
         clearInput("");
-        survivorTally = survivorTally + wood.adjustTally;
+        survivorTally = survivorTally + build.adjustTally;
 
     }else if (currentInput === "trees" && gamePath === "idolPath") {
         display(idolClue.displayMsg1);
         clearInput("");
         survivorTally = survivorTally + idolClue.adjustTally;
+        gamePath = "immunityPath";
+
+    }else if (currentInput === "water" && gamePath === "idolPath") {
+        display(water.displayMsg1);
+        clearInput("");
 
     //to restart function if player wants to start again
     }else if (currentInput === "game" && gamePath === "chillPath") {
@@ -120,20 +141,66 @@ var inputHappened = function(currentInput) {
         return;
 
     }else if (currentInput === "quit" && gamePath === "chillPath") {
-        display("You continue to slack off till your tribe mates can take it no longer. " + "\n" + eliminated.displayMsg1);
+        display("You continue to slack off till your tribe mates can take it no longer. " + "\n" + eliminated.displayMsg1 + "\n" + "(type: bye)");
         clearInput("");
         survivorTally = 0;
+        gamePath = "gameOver";
 
 // to access THIRD level actions
     }else if (currentInput === "y" && gamePath === "shelterPath") {
         display(idolClue.displayMsg1);
         clearInput("");
         survivorTally = survivorTally + idolClue.adjustTally;
+        gamePath = "immunityPath";
 
     }else if (currentInput === "n" && gamePath === "shelterPath") {
         display(hardwork.displayMsg1);
         clearInput("");
         survivorTally = survivorTally + hardwork.adjustTally;
+        gamePath = "mainPath";
+
+    }else if (currentInput === "chop" && gamePath === "shelterPath") {
+        display(medEvac.displayMsg1 + "\n" + "(type: bye)");
+        clearInput("");
+        survivorTally = 0;
+        gamePath = "gameOver";
+
+    }else if (currentInput === "weave" && gamePath === "shelterPath") {
+        display(hardwork.displayMsg1 + "For now, let's get to a challenge!" + "\n" + "(type: go)");
+        clearInput("");
+        survivorTally = survivorTally + hardwork.adjustTally;
+        gamePath = "mainPath";
+
+    }else if (currentInput === "stay" && gamePath === "idolPath") {
+        display("You overheard 3 of your tribe mates forming an alliance and complaining about some of the other members of the tribe. You sneak off and head back to camp. For now, let's get to a challenge!" + "\n" + "(type: go)");
+        clearInput("");
+        survivorTally = survivorTally + +2;
+        gamePath = "mainPath";
+
+    }else if (currentInput === "run" && gamePath === "idolPath") {
+        display("You ran back to camp and bumped into your tribe mates who were collecting wood for the shelter and fire. They noticed that you seemed flustered and suspect you of searching for the idol and you now have a target on your back. For now, let's get to a challenge!" + "\n" + "(type: go)");
+        clearInput("");
+        survivorTally = survivorTally + -1;
+        gamePath = "mainPath";
+
+    // to access FOURTH level actions
+    }else if (currentInput === "dash" && gamePath === "immunityPath") {
+        display("You have found a HIDDEN IMMUNITY IDOL!" + "For now, let's get to a challenge!" + "\n" + "(type: go)");
+        clearInput("");
+        survivorTally = survivorTally + +5;
+        gamePath = "mainPath";
+        immunity = true;
+
+    }else if (currentInput === "forgo" && gamePath === "immunityPath") {
+        display("Next time maybe! For now, let's get to a challenge!" + "\n" + "(type: go)");
+        clearInput("");
+        survivorTally = survivorTally + -1;
+        gamePath = "mainPath";
+
+    //GAME OVER PATH
+    }else if (currentInput === "bye" && gamePath === "gameOver") {
+        display("GAME OVER!!!");
+        clearInput("");
     }
 
 
