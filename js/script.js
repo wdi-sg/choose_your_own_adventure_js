bedac4438f918b053330c9727a879cbd52a1daa8
wilfredloh <lohwilfred19@gmail.com>
2019-06-30 14:02:55 +0800

    replaced checkLayers function to be able to add multiple layers and scenarios

--- js/script.js
+++ js/script.js
@@ -6,21 +6,24 @@ let scenario = 0;
 let userName = '';
 let userBar = 0;
 let userItems = '-';
-let extraCounter = '';
+let startCounter = '';
 // -------------------------  AWAITING USER INPUT --------
 let inputHappened = (anInput) => {
     let input = anInput.toUpperCase();
 
-    if (extraCounter === 'username') {
+    if (startCounter === 'username') {
         userName = anInput;
-        extraCounter = '';
+        startCounter = 'start';
         startGame();
     } else if (input === 'S') {
         startGame();
     } else if (input === 'R') {
         resetGame();
-    } else if (input === 'A' || 'B' || 'C' ){
+    }
+    if (startCounter === 'start'){
+        if (input === 'A' || 'B' || 'C' ){
         checkLayers(input);
+        }
     }
     console.log('currentLayer:' + currentLayer)
     console.log('scenario:' + scenario)
@@ -30,165 +33,156 @@ let inputHappened = (anInput) => {
 }
 
 // -------------------------  START & RESET GAME -----
-let getUsername = () => {
-    display('What is your name?');
-    extraCounter = 'username';
-}
 let startGame = () => {
     if (currentLayer === 0 && userName) {
-        stories.storyLayer0();
+        stories.zeroL.oneS.A();
     } else if (currentLayer === 0){
         getUsername();
     }
 }
-let goBack = () => {
-    currentLayer -= 2;
+let getUsername = () => {
+    display('What is your name?');
+    startCounter = 'username';
 }
 let resetGame = () => {
     currentLayer = 0;
     scenario = 0;
     userName = '';
+    startCounter = '';
     display("Game reset. \n Type 's' to play again.");
 }
 // ----------------  CHECK WHAT LAYER & SCENARIO USER IS IN --
-let layerArray = [0,1,2,3,4,5,6,7];
-let scenarioLayers = [0,1,2,3,4,5,6,7,8,9,10];
-let inputLayers = [0,1,2,3];
+let layerArr = [0,1,2,3,4,5,6,7];
+let layerArrAfter = ['zeroL','oneL','twoL','threeL','fourL','fiveL'];
+let layerAfter = '';
 
-// let newFunc = () => {
-//     for (let i = 0; i<layerArray.length; i++) {
-//         if (currentLayer === layerArray[i]){
-//             if (scenario === 1){
+let scenarioArr = [0,1,2,3,4,5,6,7,8,9,10];
+let scenarioArrAfter = ['zeroS','oneS','twoS','threeS','fourS','fiveS','sixS','sevenS','eightS'];
+let scenarioAfter = '';
 
-//             }
-//         }
-//     }
-// }
+let breakToggle = false;
 
 let checkLayers = (input) => {
-    if (currentLayer === 3) {
-        if (scenario === 1) {
-            if (input === 'A') {
-                stories.storyLayer3A1();
-            } else if (input === 'B') {
-                stories.storyLayer3B1();
-            }
-        } else if (scenario === 3) {
-            if (input === 'A') {
-                stories.storyLayer3A3();
-            } else if (input === 'B') {
-                stories.storyLayer3B3();
-            }
-        }
-        else if (scenario === 5) {
-            if (input === 'A') {
-                stories.storyLayer3A5();
-            } else if (input === 'B') {
-                stories.storyLayer3B5();
-            }
-        }
-    }
-    if (currentLayer === 2) {
-        if (scenario === 1) {
-            if (input === 'A') {
-                stories.storyLayer2A1();
-            } else if (input === 'B') {
-                stories.storyLayer2B1();
-            } else if (input === 'C') {
-                goBack();
-                stories.storyLayer0();
-            }
-        } else if (scenario === 2) {
-            if (input === 'A') {
-                stories.storyLayer2A2();
-            } else if (input === 'B') {
-                stories.storyLayer2B2();
-            } else if (input === 'C') {
-                stories.storyLayer2C2();
+    for (let i = 0; i<layerArr.length; i++) {
+        if (currentLayer === layerArr[i]){
+            layerAfter = layerArrAfter[i];
+            for (let j = 0; j<scenarioArr.length; j++) {
+                if (breakToggle === true) {
+                    break;
+                }
+                if (scenario === scenarioArr[j]){
+                    scenarioAfter = scenarioArrAfter[j];
+                    let story = stories[layerAfter][scenarioAfter];
+                    if (input === 'A') {
+                        story.A();
+                    } else if (input === 'B') {
+                        story.B();
+                    } else if (input === 'C') {
+                        story.C();
+                    }
+                }
             }
         }
     }
-    if (currentLayer === 1) {
-        if (input === 'A') {
-            stories.storyLayer1A1();
-        } else if (input === 'B') {
-            stories.storyLayer1B1();
-        }
-    }
+    breakToggle = false;
 }
 // ---------------------  ACTIVATE STORY BASED ON LAYER & SCENARIO --
 let stories = {
-//  ------------------------------ LAYER 0
-    storyLayer0 () {
-        display(`Hi ${userName}! It's dinner time! \n What should we eat for dinner? \n A. 🍣 \n B. 🍕`);
-        nextLayer(1);
+    zeroL: { /////////------------ LAYER 0 --------------///////////
+        oneS: { // -------SCENARIO----------
+            A () { // ------INPUT -----
+                display(`Hi ${userName}! It's dinner time! \n What should we eat for dinner? \n A. 🍣 \n B. 🍕`);
+                nextLayer(1);
+            }
+        }
     },
-// -------------------------- LAYER 1
-    storyLayer1A1 () {
-        display(`Great choice! Where should we eat our sushi?\n \n A. Sushi express \n B. Genki sushi \n C. Go back`);
-        userBar += 1;
-        nextLayer(1);
+    oneL: { /////////------------ LAYER 1 --------------///////////
+        oneS: { // ----SCENARIO ----
+            A () { // ------INPUT -----
+                display(`Great choice! Where should we eat our sushi?\n \n A. Sushi express \n B. Genki sushi \n C. Go back`);
+                userBar += 1;
+                nextLayer(1);
+            },
+            B () {
+                display('pepperoni, hawaiian, or screw pizza!');
+                nextLayer(2);
+            }
+        }
     },
-    storyLayer1B1 () {
-        display('pepperoni, hawaiian, or screw pizza!');
-        nextLayer(2);
+    twoL: { /////////------------ LAYER 2 --------------///////////
+        oneS: {
+            A () {
+                display('10 plates or 20 plates?');
+                nextLayer(1);
+            },
+            B () {
+                display('lobster ramen it is!');
+                nextLayer();
+            },
+            C () {
+                goBack();
+                stories.zeroL.oneS.A();
+            }
+        },
+        twoS: {
+            A () {
+                display('pepperoni with or without mushroom?');
+                nextLayer(2);
+            },
+            B () {
+                display('hawaiian out of stock, sorry!');
+                nextLayer();
+            },
+            C () {
+                display('you have decided to have dessert instead. bubble tea or waffles?');
+                nextLayer(3);
+            }
+        }
     },
-// ------------------------- LAYER 2
-    storyLayer2A1 () {
-        display('10 plates or 20 plates?');
-        nextLayer(1);
-    },
-    storyLayer2B1 () {
-        display('lobster ramen it is!')
-        nextLayer(2)
-    },
-    storyLayer2A2 () {
-        display('pepperoni with or without mushroom?');
-        nextLayer(3);
-    },
-    storyLayer2B2 () {
-        display('hawaiian out of stock, sorry!');
-        nextLayer(4);
-    },
-    storyLayer2C2 () {
-        display('you have decided to have dessert instead. bubble tea or waffles?');
-        nextLayer(5);
-    },
-// ------------------------- LAYER 3
-    storyLayer3A1 () {
-        display('total for 10 plates is $91,000. Thanks for eating!');
-        nextLayer();
-    },
-    storyLayer3B1 () {
-        display ("It's your lucky day! Sushi's on the house!")
-        nextLayer()
-    },
-    storyLayer3A3 () {
-        display('pepperoni with mushroom. yum!');
-        nextLayer();
-    },
-    storyLayer3B3 () {
-        display('pepperoni without mushroom... no vege is bad!!');
-        nextLayer();
-    },
-    storyLayer3A5 () {
-        display('all the bubble tea shops are closed!');
-        nextLayer();
-    },
-    storyLayer3B5 () {
-        display('waffles are cheap! only $1.70!');
-        nextLayer();
+    threeL: { /////////------------ LAYER 3 --------------///////////
+        oneS: {
+            A () {
+                display('total for 10 plates is $91,000. Thanks for eating!');
+                nextLayer();
+            },
+            B () {
+                display ("It's your lucky day! Sushi's on the house!");
+                nextLayer();
+            }
+        },
+        twoS: {
+            A () {
+                display('pepperoni with mushroom. yum!');
+                nextLayer();
+            },
+            B () {
+                display('pepperoni without mushroom... no vege is bad!!');
+                nextLayer();
+            }
+        },
+        threeS: {
+            A () {
+                display('all the bubble tea shops are closed!');
+                nextLayer();
+            },
+            B () {
+                display('waffles are cheap! only $1.70!');
+                nextLayer();
+            }
+        }
     }
-//  ------------------------- END LAYER
-}
-// --------------------- ACTIVATE NEXT LAYER & SELECT SCENE --
-
+}//  ------------------------- END LAYER
+// --------------------- SELECT SCENE ------
 let nextLayer = (number) => {
-    for (let i = 0; i < scenarioLayers.length; i++) {
-        if (number === scenarioLayers[i]) {
-            scenario = i;
-            console.log('value of:' + i );
+    for (let i = 0; i < scenarioArr.length; i++) {
+        if (number === scenarioArr[i]) {
+            scenario = scenarioArr[i];
+            console.log('value of scenarioArr[i]:' + scenarioArr[i] );
         }
     }
     currentLayer += 1;
+    breakToggle = true;
+}
+let goBack = () => {
+    currentLayer -= 2;
 }
