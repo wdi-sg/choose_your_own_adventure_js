5a00b05695f1e0b60c7c507391ffeed4f6f7f445
gitwenlei <ngwenlei@gmail.com>
2019-07-01 00:21:18 +0800

    Formatted questions with \n

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ var score = 0;
 var msg;
 
 var yearChoice = ['2015', '1985', '1955'];
-var yearMsg = ["I see you're a fan of Back to the Future 2. Would you rather deal with Biff, or Griff? **(B/G)**", "Doc has already destroyed the Time Machine at this point. I guess you'll have to wait around until 2015. What name would you like to go by until then?", "I see you're a fan of Back to the Future 1. Your future Mom has just asked you to the Enchantment Under the Sea dance. What do you do?**(Y/N/S)**"];
+var yearMsg = ["I see you're a fan of Back to the Future 2. Would you rather deal with Biff, or Griff? \n **(B/G)**", "Doc has already destroyed the Time Machine at this point. I guess you'll have to wait around until 2015. \n What name would you like to go by until then?", "I see you're a fan of Back to the Future 1. Your future Mom has just asked you to the Enchantment Under the Sea dance. What do you do? \n **(Y/N/S)**"];
 
 var charChoice = ['B', 'G'];
-var charMsg = ["Hmm, interesting. Biff is angry and has a cane. Do you Stand and fight, or Run away like a coward? **(S/R)**", "Griff is asking you if you are in, or out. What do you say?**(I/O)**"];
+var charMsg = ["Hmm, interesting. Biff is angry and has a cane. Do you Stand and fight, or Run away like a coward? \n **(S/R)**", "Griff is asking you if you are in, or out. What do you say? \n **(I/O)**"];
 
 var fightFlightChoice = ['S', 'R'];
-var fightFlightMsg = ["Good choice. Biff is old and feeble at this point. You push him over and he falls in a pile of manure.", "You get away, but your future son Marty Jr. is heckled for the rest of his days for his dad's cowardice."];
+var fightFlightMsg = ["Good choice. \n Biff is old and feeble at this point. You push him over and he falls in a pile of manure.", "You get away, but your future son Marty Jr. is heckled for the rest of his days for his dad's cowardice."];
 
 var inOutChoice = ['I', 'O'];
-var inOutMsg = ["Bad call. Griff and his cronies rob the Hill Valley bank and frame you for it. No more time travel for you.", "Good call. You deck Griff in the jaw and run away. He gives chase on his hoverboard and ends up in a pile of manure."];
+var inOutMsg = ["Bad call. \n Griff and his cronies rob the Hill Valley bank and frame you for it. No more time travel for you.", "Good call. \n You deck Griff in the jaw and run away. He gives chase on his hoverboard and ends up in a pile of manure."];
 
 var underTheSeaChoice = ['Y', 'N', 'S'];
-var underTheSeaMsg = ["Creepy. I hope you have some backup plan in place to get out of this. Until then, you're stuck in 1955", "Honorable. But this also means that your future Dad will never meet your Mom, and therefore you cannot exist.", "Interesting. You set up an elaborate plan for your future Dad to surprise your Mom by beating you up. Despite going horribly awry, the plan ultimately works. You may go back to your own time."];
+var underTheSeaMsg = ["Creepy. \n I hope you have some backup plan in place to get out of this. Until then, you're stuck in 1955", "Honorable. \n But this also means that your future Dad will never meet your Mom, and therefore you cannot exist.", "Interesting. \n You set up an elaborate plan for your future Dad to surprise your Mom by beating you up. Despite going horribly awry, the plan ultimately works. You may go back to your own time."];
 
 
 displayStory(initMsg);
@@ -37,7 +37,7 @@ var inputHappened = function(currentInput){
             console.log("case 0!");
             promptName(tempInput);
             displayStory("Welcome " + tempInput);
-            displayStory("Hello " + tempInput + ", which year would you like to time travel to? 2015, 1985 or 1955");
+            displayStory("Hello " + tempInput + ", which year would you like to time travel to? \n 2015, 1985 or 1955");
             console.log("going to case: ", nextStage);
             console.log("hello: ", playerName);
             break;
