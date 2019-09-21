f982c42210890ceea65edeca022bc4af4cd10ca9
SYNG <simyenng@gmail.com>
2019-09-21 18:22:20 +0800

    Implemented game progress check Add gameCounter

--- script.js
+++ script.js
@@ -5,34 +5,44 @@ Choose Your Own Adventure
 
 Ask player which House to be sorted into, after which player needs to find the House common room within Hogswarts Castle.
 
-
-
-
 */
 
 //for easy update of display message
 var input = document.getElementById("input");
 
+//to track progress of game
+var gameCounter = 0;
 
 //to store player name
 var player;
 
-
-//start game
+//start game by requesting player's name
 alert("Welcome to Hogswarts. What's your name?");
 input.placeholder = "Enter name";
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
 
-//store player name
-  player = currentInput.charAt(0).toUpperCase() +  currentInput.slice(1);
-  console.log( "Player: " + player );
-  input.value = "Enter House";
-  return `Hi, ${player}. What House are you in?`;
+    //check if is new game
+    if (gameCounter === 0) {
+
+    //store player name
+    //capitalize first character of entered name
+      player = currentInput.charAt(0).toUpperCase() +  currentInput.slice(1);
+      console.log( "Player: " + player );
+
+    //increment game progress
+      gameCounter ++;
+      console.log( "Game counter: " + gameCounter);
+
+      input.value = "Enter House";
+      return `Hi, ${player}. What House are you in?`;
+
+    }
 
 
   // var output = "WOW SOMETHING HAPPENED";
   // input.value = "clear";
   // return output;
+
 };
