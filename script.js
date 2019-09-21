29a438b81ddd13b9bfa21d7ecc0d1bfcb0d286f9
David M <davidmosk@gmail.com>
2019-09-21 13:20:50 +0800

    added date check NaN and ask started checking date inputted

--- script.js
+++ script.js
@@ -5,14 +5,13 @@
 
 console.log("hello script js");
 
-// start by initializing the state and playername
-var currentState = "start";
-var playerName = null;
 
+//create an object that can be have more data added as needed
+// so i don't have to keep defining variables
 var playerData = {
   state: "start",
   name: "",
-  travelYear: "",
+  travelYear: ""
 }
 
 var inputHappened = function(currentInput){
@@ -26,16 +25,39 @@ switch (playerData.state) {
     return askTravelYear(playerData.name);
     break;
 
-  // case "travelYear":
-  //   playerName = currentInput;
-  //   askTravelYear(playerName);
-  //   currentState = "travelYear";
-  //   break;
-
-}
-}
+  case "travelYear":
+    playerData.travelYear = parseInt(currentInput);
+    if (isNaN(playerData.travelYear)) {
+      playerData.state = "travelYear";
+      return "Please enter a date (YYYY)";
+    // playerData.state = "travelYear";
+    }
+    else {
+    return askFilmDetails(playerData.travelYear);
+    break;}
+    }
+  }
 
 var askTravelYear = function (playerName) {
-  var output = "Hi " + playerName + ", what year would you like to travel to?";
+  var output = "Hi " + playerName + ", what year would you like to go to? (YYYY)";
+  return output;
+}
+
+var askFilmDetails = function (travelYear) {
+  // add the travel year to the object
+  playerData.travelYear = travelYear;
+// do some date checks
+  if (playerData.travelYear >= 2015){
+  var output = playerData.name + ", I see you're a fan of Back to the Future 2. \n Would you rather deal with Biff or Griff? (B/G)";
+   playerData.state = "checkBifforGiff";
+   }
+   else  if (playerData.travelYear >= 1985 && playerData.travelYear <=2014){
+  var output = playerData.name + ", Doc has already destroyed the Time Machine at this point. I guess you'll have to wait around until 2015. What name would you like to go by until then?";
+   playerData.state = "changePastName";
+   }
+// add the new state for the question asked
+    else {
+    var output = "Hi " + playerData.name + ", wI see you're a fan of " + playerData.travelYear;
+    }
   return output;
 }
