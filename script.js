1b78eff18204ea82bc3d0627cab7267e6a7c1ee6
kevinngth <kevin.ng.th@gmail.com>
2019-09-21 22:04:49 +0800

    up to further 1

--- script.js
+++ script.js
@@ -35,6 +35,9 @@ let coinFlip = function() {
 // player object
 let player = {
     name: "",
+    getName: function(){
+        return this.name;
+    }
 };
 
 // putting all the steps into this object
@@ -43,12 +46,6 @@ let gameBoard = {
     step2: function() {
         return `Doug: "Listen up ${player.name}. You're messing with the wrong guys. If you want to see your friend again, you get me my package back."\n\nget moving / reply`;
     },
-    // if user types in a non-valid input
-    invalid: `You mumbled under your breadth. The hangover feels like a balloon under your cranium, slowly being inflated, pressure mounting.`,
-    // player died
-    badEnding: `There's a loud ringing in your ears. Your knees buckled and your vision was swimming. A quick flash of darkness envelops the senses as you feel your body shut itself down. \n\nThanks for playing!`,
-    // player won
-    goodEnding: "You shot Doug. Stu runs to the car, starts it up and you hop in. He jams the gas pedal before you can even close the door. Stu and you get out of there as fast as possible.\n\nThanks for playing!",
     // story chunks
     "get moving": `You struggle to get up, using the light from the phone to get to the door. On the other side of the door, you find yourself in a hotel room. "What was I doing in a closet?", you thought to yourself.\n\ncheck phone / look around`,
     // story chunks
@@ -56,12 +53,22 @@ let gameBoard = {
     // story chunks
     "check phone": `You look through the phone to get a clue about last night. The last thing you remembered was drinking at the club with Stu and he offered you some pills for preventing hangover...\n\nopen gallery / open messages`,
     // story chunks
-    "look around": `The room is in a mess. Used condoms lying around, clothes everywhere, pills on the ground. On closer inspection, you realised that this may be the package Doug was talking about. You turn the room upside down looking for the package. Then, you thought of one place, the place you came out of. You look to the closet, staring straight back at you are two things: A gun and the opened package. You take the\n\ngun / package`
-// prelude to ending, with 2 possibilities
+    "look around": `The room is in a mess. Used condoms lying around, clothes everywhere, pills on the ground. On closer inspection, you realised that this may be the package Doug was talking about. You turn the room upside down looking for the package. Then, you thought of one place, the place you came out of. You look to the closet, staring straight back at you are two things: A gun and the opened package. You take the\n\ngun / package`,
+    // story chunks, needs to work with preEnd
+    "package": [`"It's only right to return what was wrongfully taken. You're responsible that way, `,`", you encourage yourself. `,`You see Doug reaching for his gun. You\n\ntry to fight / run away`],
+    // prelude, result based on coinFlip
+    "try to fight": `You charged at Doug, fighting to get the gun off his hands. `,
+    // prelude to badEnding
+    "run away": `You died.`,
+    // prelude to goodEnding
+    "gun": [`"He's probably going to kill us both if I turn up with just half of the pills. Might as well beat him to the chase", you thought. `,`You pretend to take the package, but you reach for the gun. `],
+    // explanation before ending
+    preEnd: `You made a call to Doug and arranged a meeting. Doug shows up with Stu, tied up and helpless. Stu: "Sorry I lied. Those weren't vitamins. They were drugs! I got them from this guy at the club!" Doug: "You weren't supposed to take them, moron. You were supposed to give them to others. They're roofies, retards!" You: "Untie him first, I'll hand you the package." Doug proceeds to untie Stu. Stu shakes off the ropes and gets up. `,
+    // player died
+    badEnding: `Bang! There's a loud ringing in your ears. Your knees buckled and you hit the ground. Your vision is swimming. A quick flash of darkness envelops the senses as you feel your body shut itself down. \n\nThanks for playing!`,
+    // player won
+    goodEnding: "You shot Doug. Stu runs to the car, starts it up and you hop in. He jams the gas pedal before you can even close the door. Stu and you get out of there as fast as possible.\n\nThanks for playing!",
 };
-
-// prelude to goodEnding
-let gunStory = `"He's probably going to kill us both if I turn up with just half of the pills. Might as well beat him to the chase", you thought. You made a call to Doug and arranged a meeting. Doug shows up with Stu, tied up and helpless. Stu: "Sorry I lied. Those weren't vitamins. They were drugs! I got them from this guy at the club!" Doug: "You weren't supposed to take them, moron. You were supposed to give them to others. They're roofies, retards!" You: "Untie him first, I'll hand you the package." Doug proceeds to untie Stu. Stu shakes off the ropes and gets up. You pretend to take the package, but you reach for the gun. `
 
 // starter code
 var inputHappened = function(currentInput){
@@ -69,28 +76,43 @@ var inputHappened = function(currentInput){
     console.log(runTime);
     previousInputs.push(currentInput);
     console.log(previousInputs);
+    let result = "skipped past logic circuit";
 // test code
-    if (player.name !== "") {
+    if (player.getName() !== "") {
 // prevent user from erroneous inputs
-        if (gameBoard[currentInput] === undefined) {
-            runTime--;
-            previousInputs.pop()
-            let savePoint = gameBoard[previousInputs[runTime]];
-            return savePoint;
-        } else if (currentInput === "gun") {
-            return gunStory + gameBoard.goodEnding;
-        } else {
-            console.log(gameBoard[currentInput]);
-            return gameBoard[currentInput];
+        switch (gameBoard[currentInput]) {
+            case undefined:
+                runTime--;
+                previousInputs.pop();
+                result = gameBoard[previousInputs[runTime]];
+                console.log("savePoint ACTIVATED");
+            break;
+            case gameBoard["gun"]:
+                result = gameBoard["gun"][0] + gameBoard["preEnd"] + gameBoard["gun"][1] + gameBoard.goodEnding;
+            break;
+            case gameBoard["run away"]:
+                result = gameBoard["badEnding"];
+            break;
+            case gameBoard["package"]:
+                result = gameBoard["package"][0] + player.getName() + gameBoard["package"][1] + gameBoard["preEnd"] + gameBoard["package"][2];;
+            break;
+            case gameBoard["try to fight"]:
+                if (coinFlip()) {
+                    result = gameBoard["try to fight"] + gameBoard["goodEnding"];
+                } else {
+                    result = gameBoard["try to fight"] + gameBoard["badEnding"];
+                };
+            break;
+            default:
+                result = gameBoard[currentInput];
         };
 // step 2
     } else {
         player.name = currentInput;
-        console.log(player.name);
-        return gameBoard.step2();
+        result = gameBoard.step2();
     };
-    console.log("skipped past logic circuit");
-    return "broken";
+    console.log(result);
+    return result;
 };
 
 
@@ -98,43 +120,29 @@ var inputHappened = function(currentInput){
 
 
 
-/* ///// resource pot ////////
+/*
+//////// notes ////////
+
+Possible improvements:
+
 Find people and get clues
+Go places and get clues
+Find items and get clues
+Dark room can be anywhere: Wardrobe in a hotel room/Storeroom of a club(bottle opener)/Restaurant kitchen(knife).
 
-Go places and get clue
+Text somebody: (array.pop)if you've texted them before, they won't reply.
 
-Find items and get clue
+Remembering phone number: if correct number will call the correct person, if wrong will call another person and jeopardize your safety.
 
-Dark room can be anywhere. Wardrobe in a hotel room. Storage room of a club in the day (able to pick up bottle opener). Kitchen of a restaurant (able to pick up knife). Confession
+array of places, accessed by random array index
+let places = ["hotel", "restaurant", "club", "back alley", "hotel suite", etc]
 
-
-Choose to open door and go out or do nothing: if you do nothing, your phone vibrates and you get to text somebody: if you've already texted them before, they won't reply. Use conditionals to text texted or not.
-
-Phone number option : if correct will call the correct person, if wrong will call another person and jeopardize your safety
-
-Able to go to a few places: back alley, hotel suite,
-
-Able to fight so thugs
-Get knocked out
+Able to fight so thugs, have HP
+Array of weapons of varying damage: player{name: "", weapon: {knife: {damage: 5}}, health: 10}
 
 Pop item from array after accessing array, return "there's nothing here..." if undefined.
 
- - name: user input
- - weapon: choice from array
- - class
- - health
-
-Use for loops to print out the
-
-Weapons array
-array of places, accessed by random array index
-let places = ["hotel", "restaurant", "club"];
-
-Class array
-
-Enemies array
-
-One ending: found out who has your bag of drugs
+Complex ending: find out who has your bag of drugs
 
 ASCII image
 */
