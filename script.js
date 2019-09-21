8944260f64e02a8b71795c62b2f1b11d3ab1bb84
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-09-22 00:21:37 +0800

    Updated script.js with pseudocode

--- script.js
+++ script.js
@@ -2,4 +2,42 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
 
+
+    /* 1. Minesweeper Concept
+
+    This part can also be automated by system first.
+    // Prompt player 1 to input name
+    // Choose where to place bomb/items
+        // Row 1, 2, 3
+        // Column 1, 2, 3
+    // Assign random number from 1-9 to each of the box
+    // Show to player 1 in which number the bomb/item he has placed is
+    // Switch to player 2
+    */
+
+    // Prompt player 2 for name
+    // Prompt to select a number from 1 - 9 to see if hit bomb
+        // If not, return "You are safe. Please select another."
+        // If hit bomb, game over
+    // Game ends when player safely opened 8 other boxes without hitting bomb or when hit bomb
+    // Every safe opening of a box has a score of 1.
+    // Save all scores and compare the highest.
+
+
+    /* 2. Super Mario Concept*/
+
+    // Prompt player for name
+    // Prompt player to choose character: Mario or Lugi
+    // Alert player to click start
+    // When player click start, randomize a number to how many steps the character moves forward
+    // At the 10th step, there is a gap
+    // If player reaches the gap, game over
+    // At the 15th step, there are coins for grab
+        // Player can choose to continue or take the coins
+    // At the 18th step, there is enemy
+        // E.g. if just nice at the step of the enemy, game over
+    // Flag at the 100th step.
+        // If player reaches the 100th step safely, can choose to continue to next level or end the game.
+            // Whichever option chosen, bring to end game and cal score
+
 };
