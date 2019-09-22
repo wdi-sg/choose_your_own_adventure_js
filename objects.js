f0df7b3f393c231adf162762a840819b79f4a25a
Ryan-Yeo <ryanrj@hotmail.sg>
2019-09-22 17:24:11 +0800

    added encounter scene and fight scene

--- objects.js
+++ objects.js
@@ -25,71 +25,136 @@ var character = {
 
 //Fight 4 demons for now, maybe can add more later
 var demon = [{
-                "fire demon": {
+                "demon": {
                                 stats: {
-                                            hp: 20,
-                                            power: 50,
+                                            hp: 60,
+                                            power: 30,
                                             speed: 30,
                                      }
                              }
             },
             {
-                "water demon": {
+                "demon": {
                                 stats: {
                                             hp: 30,
                                             power: 30,
-                                            speed: 40
+                                            speed: 60
                                      }
                              }
             },
             {
-                "earth demon": {
+                "demon health": {
                                 stats: {
                                             hp: 60,
-                                            power: 30,
-                                            speed: 10,
+                                            power: 40,
+                                            speed: 30,
                                      }
                              }
             },
             {
-                "wind demon": {
+                "demon boss": {
                                 stats: {
-                                            hp: 20,
-                                            power: 40,
-                                            speed: 40
+                                            hp: 200,
+                                            power: 100,
+                                            speed: 100
                                      }
                              }
             }]
 
 
 //4 paths to choose from, there after each destination is determined by random, will merge paths by using exact same object keys and values.
-var path =[
-        {
-            fire_path: [
-                            ["Fire Path 1", "Fire Path 2", "Fire Path 3"],
-                            ["Human Path", "Human Path", "Human Path"],
-                            ["Human Path", "God Path", "Beast Path"]
-                     ]
-        },
-        {
-            water_path: [
-                            ["Water Path 1", "Water Path 2", "Water Path 3"],
-                            ["Human Path", "Human Path", "Human Path"],
-                            ["Human Path", "God Path", "Beast Path"]
-                     ]
-        },
-        {
-            earth_path: [
-                            ["Water Path 1", "Water Path 2", "Water Path 3"],
-                            ["Human Path", "Human Path", "Human Path"],
-                            ["Human Path", "God Path", "Beast Path"]
-                     ]
-        },
-        {
-            wind_path: [
-                            ["Water Path 1", "Water Path 2", "Water Path 3"],
-                            ["Human Path", "Human Path", "Human Path"],
-                            ["Human Path", "God Path", "Beast Path"]
-                     ]
-        },
-]
+var path = {
+            "Fire Path": [["Element Path", "Element Path", "Element Path"],
+                            ["Element Path", "Element Path", "Human Path"],
+                            ["Human Path", "God Path", "Beast Path"]],
+            "Water Path": [["Element Path", "Element Path", "Element Path"],
+                            ["Element Path", "Element Path", "Human Path"],
+                            ["Human Path", "God Path", "Beast Path"]],
+            "Earth Path": [["Element Path", "Element Path", "Element Path"],
+                            ["Element Path", "Element Path", "Human Path"],
+                            ["Human Path", "God Path", "Beast Path"]],
+            "Wind Path": [["Element Path", "Element Path", "Element Path"],
+                            ["Element Path", "Element Path", "Human Path"],
+                            ["Human Path", "God Path", "Beast Path"]],
+        }
+
+//this is where user encounters the demons according to the path traveled.
+var encounter = function(){
+    if(input === "proceed"){
+        pathTaken[1] = path[pathTaken[0]][ranNum][ranNum];
+        if(pathTaken[1] === "Element Path") {
+            enemy = demon[ranNumDemon]["demon"];
+            enemyName = "Elemental Demon";
+            enemyDie = "get nothing"
+            enemyHP = enemy.stats.hp;
+            enemyPower = enemy.stats.power;
+            enemySpeed = enemy.stats.speed;
+        } else if(pathTaken[1] === "Beast Path") {
+            enemy = demon[2]["demon health"];
+            enemyName = "Beast Demon";
+            enemyDie = "get more power"
+            enemyHP = enemy.stats.hp;
+            enemyPower = enemy.stats.power;
+            enemySpeed = enemy.stats.speed;
+        } else if(pathTaken[1] === "Human Path") {
+            enemy = demon[ranNumDemon]["demon"];
+            enemyName = "Lesser Demon";
+            enemyDie = "get more health"
+            enemyHP = enemy.stats.hp;
+            enemyPower = enemy.stats.power;
+            enemySpeed = enemy.stats.speed;
+        } else if(pathTaken[1] === "God Path") {
+            enemy = demon[3]["demon boss"];
+            enemyName = "Demon Lord";
+            enemyDie = "finish the game"
+            enemyHP = enemy.stats.hp;
+            enemyPower = enemy.stats.power;
+            enemySpeed = enemy.stats.speed;
+        };
+        result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nWatch out, " + name + "! You've just encountered a " + enemyName + "! \n\nIt's stats are displayed below: \n\nDemon HP: " + enemyHP + "\nDemon Power: " + enemyPower + "\nDemon Speed: " + enemySpeed + "\n\nIf you manage to defeat it, you will be able to " + enemyDie + ". \n\nWill you run or will you fight, " + name + "??? \n\nPlease type 'RUN' to move on to the next path, or type 'FIGHT' to start fighting the " + enemyName + ".";
+    } else {
+        step.pop();
+        result = "Please type 'Proceed' to continue";
+    }
+};
+
+
+
+//this is where the fight scene occurs
+var fightScene = function(){
+    if(input === "fight"){
+            if(characterSpeed > enemySpeed){
+                var whoMovesFirst = name;
+                var whoMovesSecond = enemyName;
+            } else {
+                var whoMovesFirst = enemyName;
+                var whoMovesSecond = name;
+            };
+            if (whoMovesFirst === name){
+                enemyHP = parseInt(enemyHP) - parseInt(characterPower);
+                if (enemyHP <= 0){
+                    enemyHP = 0;
+                }
+            } else {
+                characterHP = parseInt(characterHP - enemyPower);
+                if (characterHP <= 0){
+                    characterHP = 0;
+                }
+            };
+            if (enemyHP === 0){
+                result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nAs your speed is " + characterSpeed + " and the " + enemyName + " speed is " + enemySpeed + " The one who strikes first is " + whoMovesFirst + "!! \n\nCongratulations! You have defeated the " + enemyName + "!!";
+            } else if (characterHP === 0){
+                result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\ns your speed is " + characterSpeed + " and the " + enemyName + " speed is " + enemySpeed + " The one who strikes first is " + whoMovesFirst + "!! \n\nIt looks like " + name + " the " + characterChosen + " has been defeated";
+            };
+        } else if (input === "run" && run === "") {
+            run = "User have ran"
+            result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou managed to ran away from the demon, you live to fight another day now " + name + ". Or do you???? \n\nPlease type 'Proceed' to continue ";
+        } else if (input === "run" && run !== ""){
+            step.pop();
+            result = "It seems that " + name + " the great " + characterChosen + " have already ran away before and is unable to do so again, please type 'FIGHT' to face the enemy!! ";
+        } else {
+                step.pop();
+                result = "Please type in 'RUN' or 'FIGHT' ";
+        }
+}
+};
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("hello script js");
 alert("Welcome Adventurer! \n\nPlease type in your name for your in game character in the input box given (e.g. White Wolf) \n\nYou cannot change your name later so please choose wisely");
 
 // The step variable is to check the number of inputs so far. Name is to store the name the user have chosen. characterChosen is to store the character class chosen. PathTaken is to store the number of paths taken so far.
+//All these variables outside of the function allows me to better control what i want the user to type and what i want to display, by using array length as a conditional and targeting array elements
 var step = []
 var name = "";
 var characterChosen = "";
@@ -10,6 +11,17 @@ var characterHP = "";
 var characterPower = "";
 var characterSpeed = "";
 var pathTaken = [];
+var run = "";
+
+//Variables pertaining to the enemy which i want to track and edit accordingly
+var ranNum = Math.floor(Math.random() * 3);
+var ranNumDemon = Math.floor(Math.random() * 2);
+var enemy = "";
+var enemyName = "";
+var enemyDie = ""
+var enemyHP = "";
+var enemyPower = "";
+var enemySpeed = "";
 
 var inputHappened = function(currentInput){
 
@@ -33,24 +45,24 @@ if (step.length > 1){
         characterHP = JSON.stringify(character.warrior.stats.hp);
         characterPower = JSON.stringify(character.warrior.stats.power);
         characterSpeed = JSON.stringify(character.warrior.stats.speed);
-        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)"
+        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)";
     } else if (input === "mage"){
         characterChosen = "Mage"
         characterHP = JSON.stringify(character.mage.stats.hp);
         characterPower = JSON.stringify(character.mage.stats.power);
         characterSpeed = JSON.stringify(character.mage.stats.speed);
-        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)"
+        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)";
     } else if (input === "ranger"){
         characterChosen = "Ranger"
         characterHP = JSON.stringify(character.ranger.stats.hp);
         characterPower = JSON.stringify(character.ranger.stats.power);
         characterSpeed = JSON.stringify(character.ranger.stats.speed);
-        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)"
+        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)";
     } else{
         step.pop();
         result = "Please choose the correct input! \n\n(Warrior / Mage / Ranger)";
-    }
-}
+    };
+};
 
 
 //if there are more than 2 elements in the array, check if the THIRD element is the paths to be taken, if is then proceed, if it's something else then delete their input and prompt them to input correctly. Change third element to LOWERCASE.
@@ -58,38 +70,54 @@ if (step.length > 2){
     //changing all inputs into lower case
     input = step[2].toLowerCase();
     if(input === "fire path"){
-        pathTaken = "Fire Path"
-        result = "The Fire Path is one of searing heat and melting flesh, many demons exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue"
+        pathTaken[0] = "Fire Path";
+        result = "The Fire Path is one of searing heat and melting flesh, many demons exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue";
     } else if (input === "water path"){
-        pathTaken = "Water Path"
-        result = "The Water Path is one of crashing waves and crushing depths, many sea monsters exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue"
+        pathTaken[0] = "Water Path";
+        result = "The Water Path is one of crashing waves and crushing depths, many sea monsters exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue";
     } else if (input === "earth path"){
-        pathTaken = "Earth Path"
-        result = "The Earth Path is one of meteoric avalanches and ambushing cliffs, many rock trolls exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue"
+        pathTaken[0] = "Earth Path";
+        result = "The Earth Path is one of meteoric avalanches and ambushing cliffs, many rock trolls exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue";
     } else if (input === "wind path"){
-        pathTaken = "Wind Paths"
-        result = "The Wind Path is one of tearing tornadoes and dizzying heights, many sky beasts exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue"
+        pathTaken[0] = "Wind Path";
+        result = "The Wind Path is one of tearing tornadoes and dizzying heights, many sky beasts exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue";
     } else {
         step.pop();
-        result = "Please choose the correct input! \n\n(Fire Path / Water Path/ Earth Path/ Wind Path)"
+        result = "Please choose the correct input! \n\n(Fire Path / Water Path/ Earth Path/ Wind Path)";
     };
 };
 
 
-//this step is to give some instructions and display the stats. NOT DONE WITH INSTRUCTIONS YET!!!
+//this step is to give some instructions and display the stats.
 if(step.length > 3){
     //changing all inputs into lower case
     input = step[3].toLowerCase();
     if(input === "chiong ah"){
-        result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nFrom here on things get tricky, paths diverge and paths converge, you never know where you will end up next or what you will face. \n\nIf you want to run away from the enemy you encounter you may do so, but only ONCE."
+        result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nThe stats display above will be available from here on, they will help you keep track of your character status. \nYour HP stat is especially important as if it reaches 0, you will have lost this game and you have to start over from the beginning. Additionally, things get tricky from now onwards, paths diverge and paths converge, you never know where you will end up next or what you will face. \n\nIf you want to run away from the enemy you encounter you may do so, but only ONCE, so choose wisely " + name + ". \n\nNow let us begin!!! The " + pathTaken + " waits for no one! \n\nPlease type 'Proceed' to, you know, proceed. "
     } else {
         step.pop();
-        result = "Yooooooo c'mon man you know what you need to do. \nType 'CHIONG AH' to continue "
+        result = "Yooooooo c'mon man you know what you need to do. \nType 'CHIONG AH' to continue";;
     }
-}
+};
+
+
+//this is where user encounters the demons according to the path traveled.
+if (step.length > 4){
+    //changing all inputs into lower case
+    input = step[4].toLowerCase();
+    encounter();
+};
+
+
+//this is where the fight scene occurs
+if (step.length > 5){
+    //changing all inputs into lower case
+    input = step[5].toLowerCase();
+    fightScene();
 
 
 
+};
 
 
 
@@ -103,6 +131,10 @@ console.log("Character Speed is:        " + characterSpeed);
 console.log("No. of steps so far is:    " + step.length);
 console.log("Steps so far are:          " + step);
 console.log("Paths taken so far are:    "+ pathTaken);
+console.log("Paths left are:            "+ path[pathTaken[0]])
+console.log("Have the user ran so far?  " + run);
+console.log("Current enemy is           " + enemyName);
+console.log("Current enemy HP is        " + enemyHP);
 
 
 return result;
