670acf94474512a84e3e9a87b096183535419ec4
Nathanael Tan <nathanael.tan92@gmail.com>
2019-09-22 16:00:32 +0800

    Added Score to display final score at end of tree

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var gameStart = true;
+var score = 0;
 
 var inputHappened = function(input) {};
 
@@ -9,7 +10,7 @@ while (gameStart === true) {
     `Nice to meet you, ${name}. What year would you like to go to? (YYYY)`
   );
 
-  if (Number(yearSelect) >= 2015) {
+  if (parseInt(yearSelect) >= 2015) {
     var BorG = prompt(
       "I see you're a fan of Back to the Future 2. Would you rather deal with Biff, or Griff? (B/G)"
     );
@@ -19,13 +20,22 @@ while (gameStart === true) {
       );
 
       if (stayOrRun.toUpperCase() === "S") {
+        score += 1
         alert(
-          "Good choice. Biff is old and feeble at this point. You push him over and he falls in a pile of manure."
+          `Good choice. Biff is old and feeble at this point. You push him over and he falls in a pile of manure.
+          
+          Final Score : ${score}`
         );
+      
       } else if (stayOrRun.toUpperCase() === "R") {
+        score -= 1
         alert(
-          "You get away, but your future son Marty Jr. is heckled for the rest of his days for his dad's cowardice."
+         ` You get away, but your future son Marty Jr. is heckled for the rest of his days for his dad's cowardice.
+         
+         Final Score: ${score}`
+            
         );
+        
       }
     } else if (BorG.toUpperCase() === "G") {
       var inOrOut = prompt(
@@ -33,12 +43,17 @@ while (gameStart === true) {
       );
 
       if (inOrOut.toUpperCase() === "I") {
+        score -=1
         alert(
-          "Bad call. Griff and his cronies rob the Hill Valley bank and frame you for it. No more time travel for you."
+          `Bad call. Griff and his cronies rob the Hill Valley bank and frame you for it. No more time travel for you.
+          Final Score: ${score}
+          `
         );
       } else if (inOrOut.toUpperCase() === "O") {
+        score += 1
         alert(
-          "Good call. You deck Griff in the jaw and run away. He gives chase on his hoverboard and ends up in a pile of manure."
+          `Good call. You deck Griff in the jaw and run away. He gives chase on his hoverboard and ends up in a pile of manure.
+          Final Score: ${score}`
         );
       }
     }
@@ -50,7 +65,7 @@ while (gameStart === true) {
     if (newName === "Calvin Klein") {
       alert("Welcome to the future, Calvin Klein.");
     } else {
-      alert(`${newName} is a cool name.`);
+      alert(`${newName} ...really?`);
     }
   } else if (Number(yearSelect) >= 1955 && Number(yearSelect) <= 1984) {
     var danceOption = prompt(
@@ -58,19 +73,26 @@ while (gameStart === true) {
     );
 
     if (danceOption.toUpperCase() === "Y") {
-      alert(
-        "Creepy. I hope you have some backup plan in place to get out of this. Until then, you're stuck in 1955."
+      score -= 1
+        alert(
+        `Creepy. I hope you have some backup plan in place to get out of this. Until then, you're stuck in 1955.
+        Final Score: ${score}`
       );
     } else if (danceOption.toUpperCase() === "N") {
-      alert(
-        "Honorable. But this also means that your future Dad will never meet your Mom, and therefore you cannot exist."
+      score -= 1 
+        alert(
+        `Honorable. But this also means that your future Dad will never meet your Mom, and therefore you cannot exist.
+        
+        Final Score: ${score}`
       );
     } else if (danceOption.toUpperCase() === "S") {
-      alert(
-        "Interesting. You set up an elaborate plan for your future Dad to surprise your Mom by beating you up. Despite going horribly awry, the plan ultimately works. You may go back to your own time."
+      score += 1;
+        alert(
+        `Interesting. You set up an elaborate plan for your future Dad to surprise your Mom by beating you up. Despite going horribly awry, the plan ultimately works. You may go back to your own time.
+        Final Score: ${score}`
       );
     } else {
-      prompt("Please enter a valid response.");
+      prompt("That is not a valid response, please try again");
      
     }
   } else if (Number(yearSelect) < 1955) {
@@ -79,43 +101,48 @@ while (gameStart === true) {
     );
 
     if (timeMachine.toUpperCase() === "H") {
-      alert(
-        "Good idea, but no. The time machine needs to get to 88mph. 12 horsepower ain't gonna cut it."
+      score -= 1
+        alert(
+        `Good idea, but no. The time machine needs to get to 88mph. 12 horsepower ain't gonna cut it.
+        Final Score: ${score}`
       );
     } else if (timeMachine.toUpperCase() === "M") {
-      alert(
-        "You'd be better off drinking the moonshine. Do not pass Go, do not collect $200. Stuck in 1855."
+     score -= 1
+        alert(
+        `You'd be better off drinking the moonshine. Do not pass Go, do not collect $200. Stuck in 1855.
+        Final Score: ${score}`
       );
     } else if (timeMachine.toUpperCase() === "T") {
-      var clara = prompt(
-        "Good call! This plan seems to be working. But wait! Clara wants to go Back to the Future with you at the last moment. What do you do? (T/L)"
+      score += 1
+        var clara = prompt(
+        `Good call! This plan seems to be working. But wait! Clara wants to go Back to the Future with you at the last moment. What do you do? (T/L)
+       Score: ${score}`
       );
 
       if (clara.toUpperCase() === "T") {
+        score += 1
         alert(
-          "Interesting choice. Unfortunately the Doc can't grab Clara and get back to the car in time. He ends up staying in 1855 with her."
+          `Interesting choice. Unfortunately the Doc can't grab Clara and get back to the car in time. He ends up staying in 1855 with her.
+          Final Score: ${score}`
         );
       } else if (clara.toUpperCase() === "L") {
+        score -= 1
         alert(
-          "Smart choice. Unfortunately the Doc was deeply in love with Clara, and when he gets back to 1985 he becomes very depressed."
+          `Smart choice. Unfortunately the Doc was deeply in love with Clara, and when he gets back to 1985 he becomes very depressed.
+          Final Score: ${score}`
         );
       } else {
-        prompt("Please enter a valid response.");
-        // go back to clara;
+        prompt("That is not a valid response, please try again");
+       
       }
     } else {
-      prompt("Please enter a valid response.");
-      // go back to timeMachine
+      prompt("That is not a valid response, please try again");
+    
     }
   } else {
     alert("Please enter a valid year.");
-    // go back to game start;
+ 
   }
 }
 
-var restart = prompt("Do you want to restart game? Y/N ");
-if (restart.toUpperCase() === "Y") {
-  gameStart = true;
-} else {
-  gameStart = false;
-}
+
