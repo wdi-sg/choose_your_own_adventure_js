5ce85eafba7e108ca6d3e628bda52a86fcb6f879
dannyozh <danielongzhihao@gmail.com>
2019-09-22 23:41:36 +0800

    managed to create two routes. going to bed..

--- script.js
+++ script.js
@@ -5,16 +5,26 @@ console.log("hello script js");
 //global variables
 var stage = 0;
 var playerName = prompt ("Welcome to Guitar Hero." + " Enter your name in the box.");
-var date = prompt ("We're going to travel back in time to learn the ways of Guitar gods." + " Enter in one of three years, 1969, 1979, or 1989, and press enter again");
+var date = prompt ("We're going to travel back in time to learn the ways of Guitar gods." + " Enter in one of two years, 1969 or 1979, and press enter again");
 
 var Jimi = {
 Age: 27,
-Wisdom: "Sometimes you want to give up the guitar, you'll hate the guitar. But if you stick with it, you're gonna be rewarded."
+Info: "Hendrix's performance at Woodstock '69 is still considered to be one of the most coveted in rock history"
 }
 
 var Robbie = {
 Age: 76,
-Wisdom: "I play guitar quite a bit, because I'm always in search of something. I don't play to jam, but because I'm fishing. I'm looking for something, that I hope you can never find. If I do find it, I'm afraid I won't have a need to do this any more."
+Info: "In 1969, Robbie Robertson formed The Band, one of the celebrated Canadian-American Roots Rock group."
+}
+
+var Dylan = {
+Age: 78,
+Info: "In 1979, Bob Dylan released Slow Train Coming, his first record since converting to Christianity"
+}
+
+var Page = {
+Age: 75,
+Info: "In 1979, Jimmy Page, along with Led Zeppelin, performed made a successful concert return to Knebworth, England."
 }
 
 function nextStage () {
@@ -23,6 +33,10 @@ function nextStage () {
 
 function previousStage() {
     stage --;
+}
+
+function start() {
+    stage = 1;
 }
 
 
@@ -41,19 +55,29 @@ alert ("enter your chosen date again to start quest");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  if (stage === 1 && currentInput.includes("1969")) {
+  if (stage === 1 && currentInput.includes(parseInt("1969"))) {
     nextStage();
     output = "Choose between Jimi (J) or Robbie (R)";
     return output;
 } else if (stage === 2 && currentInput.includes("J")) {
-            output = "Hendrix passed away at" + " " + Jimi.Age + "." + " He said" + " " + Jimi.Wisdom + " To learn from Robbie, press R. Or enter another year (1979 or 1989)";
+            output = "Hendrix passed away at" + " " + Jimi.Age + "." + " " + " To learn about Robbie, press R. Or type in Go to return to the start.";
             return output;
 } else if (stage === 2 && currentInput.includes("R")) {
-            output = "Robbie is currently" + " " + Robbie.Age + "." + " He said" + " " + Robbie.Wisdom + " To learn from Jimi press J. Or enter another year (1979 or 1989)";
+            output = "Robbie is currently" + " " + Robbie.Age + "." + " " + Robbie.Info + " To learn about Jimi press J. Or type in Go to return to the start.";
             return output;
-} else if (stage === 1 && currentInput.includes("1979")) {
-    output = "john says hi";
+} else if (stage === 2 && currentInput.includes("Go")) {
+            output = "You are back at the start. Enter in 1969 or 1979";
+            start ();
+            return output;
+} else if (stage === 1 && currentInput.includes(parseInt("1979"))) {
     nextStage();
-    return output
+    output = "Choose between Jimmy Page (P) and Bob Dylan (D).";
+    return output;
+} else if (stage === 2 && currentInput.includes("P")) {
+    output = "At" + Page.Age + " Seminal guitarist Jimmy Page is still going strong." + " " + "One of his more memorable performance came" + Page.Info + " To learn about Bob Dylan, type in D. Or type in Go to return to the start."
+    return output;
+} else if (stage === 2 && currentInput.includes("D")) {
+    output = "Bob Dylan is currently" + " " + Dylan.Age + " " + Dylan.Info + "." + " To learn about Jimmy Page, type in P. Or type in Go to return to the start."
+    return output;
 }
 };
