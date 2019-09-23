88f7480dd8fee1d49bdae36aaf0f0c272fd3392b
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-09-24 01:01:57 +0800

    Added index.html and refactor script.js to include HTML element using DOM

--- script.js
+++ script.js
@@ -1,81 +1,52 @@
-    /* 1. Minesweeper Concept
-
-    This part can also be automated by system first.
-    // Prompt player 1 to input name
-    // Choose where to place bomb/items
-        // Row 1, 2, 3
-        // Column 1, 2, 3
-    // Assign random number from 1-9 to each of the box
-    // Show to player 1 in which number the bomb/item he has placed is
-    // Switch to player 2
-    */
-
-
-    // Prompt to select a number from 1 - 9 to see if hit bomb
-        // If not, return "You are safe. Please select another."
-        // If hit bomb, game over
-    // Game ends when player safely opened 8 other boxes without hitting bomb or when hit bomb
-    // Every safe opening of a box has a score of 1.
-    // Save all scores and compare the highest.
-
-
-    /* 2. Super Mario Concept*/
-
-    // Prompt player for name
-    // Prompt player to choose character: Mario or Lugi
-    // Alert player to click start
-    // When player click start, randomize a number to how many steps the character moves forward
-    // At the 10th step, there is a gap
-    // If player reaches the gap, game over
-    // At the 15th step, there are coins for grab
-        // Player can choose to continue or take the coins
-    // At the 18th step, there is enemy
-        // E.g. if just nice at the step of the enemy, game over
-    // Flag at the 100th step.
-        // If player reaches the 100th step safely, can choose to continue to next level or end the game.
-            // Whichever option chosen, bring to end game and cal score
-
-console.log("Welcome to Minesweeper v2! Please enter 'Start Game' to proceed.");
-
 // Create an empty array of mystery boxes
 var mysteryBox = []; // This mysteryBox will be shown to user to pick from numbers 1 - 9
+
+// To define the item is in which box number of the mystery box
 var boxItem = null;
 
 // Create an array to store player's selected choices
 var playerSelectedChoices = [];
+
+// Create div element
+var displayGameStepPrompt  = document.createElement("div");
+// Append div to document body
+document.body.appendChild(displayGameStepPrompt);
+
+// Create an array of questions
+var gameStepPrompt = [
+"Please enter your name.",
+"Please input any number from 1 - 9."
+];
+
+var player = {
+    name: "",
+    attemptNo: null,
+    playerSelectedChoices: [],
+    gameScore: null
+};
 
 var inputHappened = function(currentInput){
 
     // Type in "Start Game" (Imagine user clicks on "Start Game" button)
     if(currentInput === "Start Game") {
 
-        // Prompt user for name
-        var playerName = prompt("Please enter your name.");
-        console.log("Hello " + playerName);
+        displayGameStepPrompt.innerHTML = gameStepPrompt[0];
+        return "";
 
-        // System assign random number to the array of mystery boxes
-        numberGenerator(mysteryBox);
-        console.log("Array in mysteryBox: " + mysteryBox);
+    } else if(currentInput !== "Start Game"){
 
-        // Get the box item location
-        boxItem = mysteryBox[2];
-        console.log("The item is at number " + boxItem);
+        // Run setup mystery box
+        setupMysteryBox();
 
-        // Player will be prompted to be input choice number as long as he/she does not hit the bomb
-        while(parseInt(playerChoiceInput) !== boxItem){
+        displayGameStepPrompt.innerHTML = gameStepPrompt[1];
+        player.name = currentInput;
+        return "Hello " + player.name + "\n" + "The item is in one of the mystery boxes below: " + mysteryBox;
 
-            var playerChoiceInput = prompt("Please select a number from 1 - 9.");
+    } else if(displayGameStepPrompt.innerHTML = gameStepPrompt[1]) {
 
-            playerChoice(parseInt(playerChoiceInput));
-            playerSelectedChoices.push(playerChoiceInput);
+        playerChoice(parseInt(currentInput));
+        playerSelectedChoices.push(currentInput);
 
-            console.log("Your choices are: " + playerSelectedChoices);
-        }
-
-    } else {
-
-        // Prompt user to input exact text to start the game
-        return "Please enter 'Start Game' to proceed.";
     }
 
 
@@ -103,15 +74,38 @@ var numberGenerator = function(arr) {
     numberGenerator(arr);
 };
 
+var setupMysteryBox = function() {
+
+    // Assign item into box number
+    numberGenerator(mysteryBox);
+    console.log("Array in mysteryBox: " + mysteryBox);
+
+    // Get the box item location
+    boxItem = mysteryBox[2];
+    console.log("The item is at number " + boxItem);
+};
+
 // This function will check if player's input choice matches the specified boxItem value.
 var playerChoice = function(mysteryBoxNumber) {
 
         if(parseInt(mysteryBoxNumber) !== boxItem) {
-            console.log("You have entered: " + mysteryBoxNumber);
-            console.log("You are safe!");
+            displayGameStepPrompt.innerHTML = gameStepPrompt[1];
+            return "You have entered: " + mysteryBoxNumber + "\n" + "You are safe!";
 
         } else {
-            console.log("You hit the bomb! Game Over!");
+            displayGameStepPrompt.innerHTML = gameStepPrompt[0]
+            return "You hit the bomb! Game Over!";
         }
+
+};
+
+var display = function( data ){
+  var displayElement = document.querySelector('#output');
+
+  // get rid of the entire contents
+  displayElement.innerHTML = "";
+
+  // put the data into the div
+  output.innerText = data;
 
 };
