e5b6f5a8415489f245b6025098480bc79af24412
datguyrhy <54259724+datguyrhy@users.noreply.github.com>
2019-09-23 23:38:41 +0800

    Add files via upload
    - added reset button to go back to beginning
    -added images to each location
    - added some styles to button

--- script.js
+++ script.js
@@ -2,73 +2,94 @@ console.log("hello script js");
 
 /* ---- pink display box selector----*/
 var display = function( data ){
-var textOutput = document.querySelector('#output');
-textOutput.innerText = data;
+var displayElement = document.querySelector('#output');
+displayElement.innerHTML = "";
+output.innerText = data;
 };
 
-
+/* ---resets the box to beginning ----*/
+document.querySelector('#reset').addEventListener('click',function(event){
+  var startMap = document.getElementById('map-display');
+  startMap.setAttribute('src', '../images/lotr-map.jpg');
+  var intro = "\n Welcome to Middle-earth" +"\n Where would u like to go?" + "\n 1.theShire" + "\n 2. gondor" + "\n 3.rivendell" ;
+  display( intro );
+  console.log("why");
+});
+/*----- name input cofirmation button ----*/
+// added images that will change with location using DOM
 document.querySelector('#nameConfirm').addEventListener('click', function(event){
   //selected name input box value
   var nameInput = document.querySelector('#nameInput').value;
   console.log(nameInput);
-  var intro = "Hello " + nameInput + "\n Welcome to Middle-earth" +"\n Where would u like to go?" + "\n 1.shire" + "\n 2. gondor" + "\n 3.rohan" ;
+  var map = document.createElement('IMG');
+  map.setAttribute('src', '../images/lotr-map.jpg');
+  map.setAttribute('id', 'map-display');
+  document.body.appendChild(map);
+  var intro = "Hello " + nameInput + "\n Welcome to Middle-earth" +"\n Where would u like to go?" + "\n 1.theShire" + "\n 2. gondor" + "\n 3.rivendell" ;
   display( intro );
 });
 
 
-var shire = {
+var theShire = {
   name: "shire",
   north: "tavern",
   south: "bilbo's home",
   east: "event yard",
-  West: "Mirkwood"
 };
 var gondor = {
   name: "gondor",
   north: "Market place",
-  south: "Narrow Alley Way",
-  east: "you peer into the distance",
-  west: " you are stopped by a guard"
+  south: "Gates of Gondor",
+  east: "Entrance to Helmsdeep",
+  west: " you are stopped by a guard."
 };
-var rohan = {
-  name: "rohan",
-  north: "you see the entrance to the castle",
-  south: "there is nothing of interest",
+var rivendell = {
+  name: "rivendell",
+  north: "you see the entrance",
+  south: "Dark looking woods",
   east: "you peer into the distance",
   west: " you are stopped by a guard"
 };
 
-var place = [shire,gondor, rohan];
+var place = [theShire,gondor, rivendell];
 
 // location access
 
-document.querySelector('#input').addEventListener('change', function(event){
+var main = document.querySelector('#input').addEventListener('change', function(event){
   var currentInput = event.target.value;
   var location = place[0].name;
 
-  if( currentInput === "shire"){
-
+  if( currentInput === "theShire"){
+    var shireMap = document.getElementById('map-display');
+    shireMap.setAttribute('src', '../images/the-shire.jpg');
     location = place[0].name;
     display( location + "\n" + "1." + place[0].north + "\n" + "2." + place[0].south
-    + "\n" +"3." + place[0].east + "\n"+ "4." + place[0].west);
+    + "\n" +"3." + place[0].east);
   }
 
   else if( currentInput === "gondor"){
+    var gondorMap = document.getElementById('map-display');
+    gondorMap.setAttribute('src', '../images/gondor.jpg');
     location = place[1].name;
     display( location + "\n" + "1." + place[1].north + "\n" + "2." + place[1].south
     + "\n" +"3." + place[1].east + "\n"+ "4." + place[1].west);
     }
 
-  else if( currentInput === "rohan"){
+  else if( currentInput === "rivendell"){
+      var rivMap = document.getElementById('map-display');
+      rivMap.setAttribute('src', '../images/rivendell.png');
       location = place[2].name;
       display( location + "\n" + "1." + place[2].north + "\n" + "2." + place[2].south
       + "\n" + "3." + place[2].east + "\n"+ "4." + place[2].west);
       }
+
+  else(display("that is not a valid input!"));
 });
 
-
-
-// if (currentInput == "shire north") function shireGo(shire, direction){
-//   var output =  shire.north;
-//   return output;
-// }
+// tried to create img element in displaybox to store image. cant get it to work
+// var mapGen = function(){
+//   var cre8Map = document.createElement("img");
+//   cre8Map.setAttribute('src', '../images/lotr-map.jpg');
+//   cre8Map.setAttribute('id', 'map-display');
+//   document.getElementById('output').appendChild("crea8Map");
+// };
