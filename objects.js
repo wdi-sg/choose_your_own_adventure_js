658f00b3895c7585d531de7b95945e60afa9a250
Ryan-Yeo <ryanrj@hotmail.sg>
2019-09-24 08:49:56 +0800

    added dom for gifs, change CSS

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ var character = {
              },
     mage: {
                 stats: {
-                            hp: 40,
+                            hp: 60,
                             power: 90,
                             speed: 50
                         }
@@ -27,8 +27,8 @@ var character = {
 var demon = [{
                 "demon": {
                                 stats: {
-                                            hp: 60,
-                                            power: 30,
+                                            hp: 30,
+                                            power: 60,
                                             speed: 30,
                                      }
                              }
@@ -36,8 +36,8 @@ var demon = [{
             {
                 "demon": {
                                 stats: {
-                                            hp: 30,
-                                            power: 30,
+                                            hp: 40,
+                                            power: 40,
                                             speed: 30
                                      }
                              }
@@ -46,7 +46,7 @@ var demon = [{
                 "demon health": {
                                 stats: {
                                             hp: 60,
-                                            power: 40,
+                                            power: 30,
                                             speed: 30,
                                      }
                              }
@@ -56,7 +56,7 @@ var demon = [{
                                 stats: {
                                             hp: 200,
                                             power: 200,
-                                            speed: 140,
+                                            speed: 200,
                                      }
                              }
             }]
@@ -65,17 +65,17 @@ var demon = [{
 //4 paths to choose from, there after each destination is determined by random, will merge paths by using exact same object keys and values.
 var path = {
             "Fire Path": [["Element Path", "Element Path", "Element Path"],
-                            ["Element Path", "Human Path", "Human Path"],
-                            ["Human Path", "Beast Path", "Beast Path"]],
-            "Water Path": [["Element Path", "Human Path", "Element Path"],
-                            ["Human Path", "Element Path", "Human Path"],
-                            ["Human Path", "Beast Path", "Beast Path"]],
-            "Earth Path": [["Element Path", "Beast Path", "Element Path"],
-                            ["Beast Path", "Element Path", "Human Path"],
-                            ["Human Path", "God Path", "Beast Path"]],
-            "Wind Path": [["Element Path", "Element Path", "Beast Path"],
-                            ["Element Path", "God Path", "Element Path"],
+                            ["Human Path", "Human Path", "Human Path"],
+                            ["Beast Path", "Beast Path", "Beast Path"]],
+            "Water Path": [["Element Path", "Element Path", "Element Path"],
+                            ["Beast Path", "Human Path", "Human Path"],
                             ["God Path", "Beast Path", "Beast Path"]],
+            "Earth Path": [["Element Path", "Element Path", "Element Path"],
+                            ["Beast Path", "Beast Path", "Human Path"],
+                            ["God Path", "God Path", "Beast Path"]],
+            "Wind Path": [["Element Path", "Element Path", "Beast Path"],
+                            ["Human Path", "Beast Path", "Human Path"],
+                            ["God Path", "God Path", "Beast Path"]],
         }
 
 //this is where user encounters the demons according to the path traveled.
@@ -92,6 +92,14 @@ var encounter = function(){
         enemyHP = enemy.stats.hp;
         enemyPower = enemy.stats.power;
         enemySpeed = enemy.stats.speed;
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://i.pinimg.com/originals/3e/62/37/3e6237ad701e6567edf1a1380d4675d8.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else if(currentPath === "Beast Path") {
         enemy = demon[2]["demon health"];
         enemyName = "Beast Demon";
@@ -99,6 +107,14 @@ var encounter = function(){
         enemyHP = enemy.stats.hp;
         enemyPower = enemy.stats.power;
         enemySpeed = enemy.stats.speed;
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://i.pinimg.com/originals/dd/36/6e/dd366ea2a91e5725faa553f3dfb77ed7.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else if(currentPath === "Human Path") {
         enemy = demon[ranNumDemon]["demon"];
         enemyName = "Lesser Demon";
@@ -106,6 +122,14 @@ var encounter = function(){
         enemyHP = enemy.stats.hp;
         enemyPower = enemy.stats.power;
         enemySpeed = enemy.stats.speed;
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://i.gifer.com/origin/86/864a44c4de2a4eeed2d0dcb4214b9fde_w200.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else if(currentPath === "God Path") {
         enemy = demon[3]["demon boss"];
         enemyName = "Demon Lord";
@@ -113,6 +137,14 @@ var encounter = function(){
         enemyHP = enemy.stats.hp;
         enemyPower = enemy.stats.power;
         enemySpeed = enemy.stats.speed;
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://i.gifer.com/origin/78/78498ca21ddf76028c8a62c7e52c5af1_w200.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     };
     result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nWatch out, " + name + "! You've just encountered a " + enemyName + "! \n\nIt's stats are displayed below: \n\nDemon HP: " + enemyHP + "\nDemon Power: " + enemyPower + "\nDemon Speed: " + enemySpeed + "\n\nIf you manage to defeat it, you will be able to " + enemyDie + ". \n\nPlease type 'FIGHT' to start fighting the " + enemyName + ".";
 
@@ -148,7 +180,15 @@ if(input === "fight"){
                 characterHP = 0;
                 };
                 step.pop();
-                result = "Oh no, the "+ enemyName + " attacked you first! \n\nYou are still alive " + name + " the " + characterChosen + " please type in 'FIGHT' to continue fighting the " + enemyName + "!!"
+                result = "Oh no, the "+ enemyName + " attacked you first! \n\nBut you are still alive " + name + " the " + characterChosen + "! \n\nPlease type in 'FIGHT' to continue fighting the " + enemyName + "!!"
+                //Removing Dom manipulation from before
+                imageBox.removeChild(GIFs);
+                //Adding DOM manipulation
+                GIFs = document.createElement("img");
+                GIFs.setAttribute("src", "https://media0.giphy.com/media/3ohfFDNA6mGzIL9xaE/giphy.gif?cid=790b76112d7a64a3df6f7880389b62cd3af50f266327e997&rid=giphy.gif")
+                imageBox = document.querySelector("#gifs")
+                imageBox.appendChild(GIFs);
+                //////////////////////////
             }
             if (enemyHP <= 0){
                 enemyHP = 0;
@@ -158,16 +198,42 @@ if(input === "fight"){
             if (characterHP <= 0){
                 characterHP = 0;
                 };
+                step.pop();
+                result = "Oh no, the "+ enemyName + " attacked you first! \n\nBut you are still alive " + name + " the " + characterChosen + "! \n\nPlease type in 'FIGHT' to continue fighting the " + enemyName + "!!"
+                //Removing Dom manipulation from before
+                imageBox.removeChild(GIFs);
+                //Adding DOM manipulation
+                GIFs = document.createElement("img");
+                GIFs.setAttribute("src", "https://media0.giphy.com/media/3ohfFDNA6mGzIL9xaE/giphy.gif?cid=790b76112d7a64a3df6f7880389b62cd3af50f266327e997&rid=giphy.gif")
+                imageBox = document.querySelector("#gifs")
+                imageBox.appendChild(GIFs);
+                //////////////////////////
         }
 
 
         if (enemyHP === 0){
             result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nAs your speed is " + characterSpeed + " and the " + enemyName + " speed is " + enemySpeed + ", the one who strikes first is " + whoMovesFirst + "!!\n\nThe enemy's health is now " + enemyHP + " while yours is " + characterHP + "!!  \n\nCongratulations! You have defeated the " + enemyName + "!! \n\nPlease type 'Proceed' to continue ";
+                //Removing Dom manipulation from before
+                imageBox.removeChild(GIFs);
+                //Adding DOM manipulation
+                GIFs = document.createElement("img");
+                GIFs.setAttribute("src", "https://media0.giphy.com/media/uTuLngvL9p0Xe/giphy.gif?cid=790b76113b3272cfcbc959a9135e7fa4fc53701a57cca188&rid=giphy.gif")
+                imageBox = document.querySelector("#gifs")
+                imageBox.appendChild(GIFs);
+                //////////////////////////
         };
 
         if (characterHP === 0){
             setTimeout(location.reload.bind(location), 10000);
             result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nAs your speed is " + characterSpeed + " and the " + enemyName + " speed is " + enemySpeed + ", the one who strikes first is " + whoMovesFirst + "!!\n\nThe enemy's health is now " + enemyHP + " while yours is " + characterHP + "!! \n\nIt looks like " + name + " the great " + characterChosen + " has been defeated. \n\nDo not falter " + name + "! The Demon Lord needs to be slain and only you have the power to accomplish this task! \n\nThe page will reload in 10 seconds";
+                //Removing Dom manipulation from before
+                imageBox.removeChild(GIFs);
+                //Adding DOM manipulation
+                GIFs = document.createElement("img");
+                GIFs.setAttribute("src", "https://media1.giphy.com/media/HvRZOW92Jg2aY/giphy.gif?cid=790b7611bdfb88aea0f49f3cb3d8d2e8a111deb17b1680b2&rid=giphy.gif")
+                imageBox = document.querySelector("#gifs")
+                imageBox.appendChild(GIFs);
+                //////////////////////////
         };
 
 
--- script.js
+++ script.js
@@ -14,6 +14,8 @@ var pathTaken = [];
 var run = "";
 var ranNum = "";
 var ranNumDemon = "";
+var GIFs;
+var imageBox;
 
 
 //Variables pertaining to the enemy which i want to track and edit accordingly
@@ -39,6 +41,13 @@ if (step.length >= 0 && name === ""){
     name = currentInput;
     step[0] = name;
     result = "Good day " + name + "! Your adventure awaits you! \n\nPerilous paths will be taken, and only the strong, or the lucky, will survive. Will you be the one? \n\nGood luck " + name + ", may fortune and the force be with you \n\nPlease type in your Adventurer Class (Warrior / Mage / Ranger)";
+
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media1.giphy.com/media/OF0yOAufcWLfi/giphy.gif?cid=790b7611b74731e506d52166848f936f252df3ba357ea0f3&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
 }
 
 
@@ -51,19 +60,43 @@ if (step.length === 2){
         characterHP = JSON.stringify(character.warrior.stats.hp);
         characterPower = JSON.stringify(character.warrior.stats.power);
         characterSpeed = JSON.stringify(character.warrior.stats.speed);
-        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)";
+        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n\n(Fire Path / Water Path / Earth Path / Wind Path)";
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media0.giphy.com/media/deMPDmQw59lJRu3A83/giphy.gif?cid=790b7611850cbdf14286c2b6a3b37cf5dbadf1edcabe4664&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else if (input === "mage"){
         characterChosen = "Mage"
         characterHP = JSON.stringify(character.mage.stats.hp);
         characterPower = JSON.stringify(character.mage.stats.power);
         characterSpeed = JSON.stringify(character.mage.stats.speed);
         result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)";
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media2.giphy.com/media/Av0zcKH3i2BkaY1GXW/giphy.gif?cid=790b7611983f1b44f80f9135ea483ee2d99e95bfd5eb212a&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else if (input === "ranger"){
         characterChosen = "Ranger"
         characterHP = JSON.stringify(character.ranger.stats.hp);
         characterPower = JSON.stringify(character.ranger.stats.power);
         characterSpeed = JSON.stringify(character.ranger.stats.speed);
-        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n(Fire Path / Water Path / Earth Path / Wind Path)";
+        result = "You have chosen to be a " + characterChosen + ", your stats are as follows: \n\n HP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nYou may now choose the path you would like to take \n\n(Fire Path / Water Path / Earth Path / Wind Path)";
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media0.giphy.com/media/psqqazmAKeeMU/giphy.gif?cid=790b76114701a4b45a1b9712e24bafd776e16c1b5deb097b&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else{
         step.pop();
         result = "Please choose the correct input! \n\n(Warrior / Mage / Ranger)";
@@ -78,15 +111,47 @@ if (step.length === 3){
     if(input === "fire path"){
         pathTaken[0] = "Fire Path";
         result = "The Fire Path is one of searing heat and melting flesh, many demons exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue";
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media0.giphy.com/media/fiyQQLci4d1w6Njojo/giphy.gif?cid=790b76113b80344dbfed7e18190093f5b05b47efc9ad0e85&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else if (input === "water path"){
         pathTaken[0] = "Water Path";
         result = "The Water Path is one of crashing waves and crushing depths, many demons exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue";
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media0.giphy.com/media/ET5QsE8329VM4/giphy.gif?cid=790b76112f9043ad22a6fbeaee4dfd49422ac7008caabf23&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else if (input === "earth path"){
         pathTaken[0] = "Earth Path";
         result = "The Earth Path is one of meteoric avalanches and ambushing cliffs, many demons exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue";
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://i.makeagif.com/media/4-09-2015/BnO2P_.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else if (input === "wind path"){
         pathTaken[0] = "Wind Path";
         result = "The Wind Path is one of tearing tornadoes and dizzying heights, many demons exist here to test your strength " + name + ". \n\nAs a " + characterChosen + ", this will be a tremendous test indeed, however that will not deter you now will it, " + name + "? \n\n Let us march on forward! \n\nPlease type 'CHIONG AH' to continue";
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media1.giphy.com/media/HhhajSmRfikXC/giphy.gif?cid=790b7611fd7e306928e31c6da18fca35e297ff0b086adb99&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else {
         step.pop();
         result = "Please choose the correct input! \n\n(Fire Path / Water Path/ Earth Path/ Wind Path)";
@@ -100,6 +165,14 @@ if(step.length === 4){
     input = step[3].toLowerCase();
     if(input === "chiong ah"){
         result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nThe stats display above will be available from here on, they will help you keep track of your character status. \nYour HP stat is especially important as if it reaches 0, it is game over and you have to start over from the beginning. Additionally, things get tricky from now onwards, paths diverge and paths converge, you never know where you will end up next or what you will face. \n\nNow let us begin!!! The " + pathTaken + " waits for no one! \n\nPlease type 'Proceed' to, you know, proceed. "
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media1.giphy.com/media/3oxHQpJKupQXsmU1JS/giphy.gif?cid=790b76113cf187ab53365789fe03c5237441c248b70f4d6a&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else {
         step.pop();
         result = "Yooooooo c'mon man you know what you need to do. \n\nType 'CHIONG AH' to continue";;
@@ -190,6 +263,14 @@ if (step.length === 15){
         enemyPower = enemy.stats.power;
         enemySpeed = enemy.stats.speed;
         result = name + " the " + characterChosen +"\nHP: " + characterHP + "\nPower: " + characterPower +"\nSpeed: " + characterSpeed + "\n\nWatch out, " + name + "! You've just encountered a " + enemyName + "! \n\nIt's stats are displayed below: \n\nDemon HP: " + enemyHP + "\nDemon Power: " + enemyPower + "\nDemon Speed: " + enemySpeed + "\n\nIf you manage to defeat it, you will be able to " + enemyDie + ". \n\nPlease type 'FIGHT' to start fighting the " + enemyName + ".";
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://i.gifer.com/origin/78/78498ca21ddf76028c8a62c7e52c5af1_w200.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
     } else {
         step.pop();
         result = "Please type 'Proceed' to continue";
@@ -213,12 +294,12 @@ if (enemyName === "Beast Demon" && enemyHP === 0){
 }
 
 if (enemyName === "Lesser Demon" && enemyHP === 0){
-    characterHP = parseInt(characterHP) + 50;
+    characterHP = parseInt(characterHP) + 100;
     enemyHP = 20;
 }
 
 if (enemyName === "Elemental Demon" && enemyHP === 0){
-    characterSpeed = parseInt(characterSpeed) + 50;
+    characterSpeed = parseInt(characterSpeed) + 100;
     enemyHP = 20;
 }
 
@@ -227,6 +308,14 @@ if (enemyName === "Demon Lord" && enemyHP === 0){
         location.reload();
     };
     result = "Congratulations " + name + " the " + characterChosen + "!! You have defeated the Demon Lord!! \n\nYou are indeed the hero we all need but do not deserve. However, there are more perilous paths yet to be taken. \n\nWill you be the one to conquer them all " + name + " the " + characterChosen + "? Let us find out! \n\nPlease type in 'Reload' to play the game again."
+        //Removing Dom manipulation from before
+        imageBox.removeChild(GIFs);
+        //Adding DOM manipulation
+        GIFs = document.createElement("img");
+        GIFs.setAttribute("src", "https://media2.giphy.com/media/YGJBp5EgyVP9K/giphy.gif?cid=790b7611224f0f55fce4fe90090dc1d4c58f82a82eb4b335&rid=giphy.gif")
+        imageBox = document.querySelector("#gifs")
+        imageBox.appendChild(GIFs);
+        //////////////////////////
 }
 
 
