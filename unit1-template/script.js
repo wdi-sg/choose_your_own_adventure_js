cc66f8b9a69ef98c1217eb39cc876038e7f40370
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2019-12-14 20:41:40 +0800

    Created objects and able to log in console.

--- unit1-template/script.js
+++ unit1-template/script.js
@@ -1,18 +1,38 @@
 console.log("hello script js");
 
-
-var enterName = function(name){
-  alert(`Hello, ${name}. Let's get the game started!`);
-};
-
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  var output = ;
+  var output = `Hello, ${currentInput}. Let's get the game started!`;
   return output;
-};
+}
+//debugger;
 
-var display = function( data ){
-  var displayElement = document.querySelector('#output');
+//store the different paths and sub paths (questions)
+var paths = {
+    begin: `You come across a huge cabin in a cold night with three doors. Which one do you enter? Door 1,2 or 3`,
+
+    one:{
+    scene: `The cabin seems to be empty. Would you rather check upstairs or downstairs? Type in your choice.`,
+    upstairs: `The creaky wooden floors woke up the crazy owner. Do you want to hide or run?`,
+    downstairs:`You see a cozy nook with a fireplace. Do you want to sleep or continue exploring?`
+    }
+
+};
+console.log(paths.begin);
+
+//store the different final outcomes
+var outcome ={
+    hideOutcome:`Oh no! He sees you and locks you in a room.`, //path 1
+    runOutcome: `Escape owner and run to safety.`, //path 1
+    sleepOutcome: `You were woken up by a bear who got in the cabin.`, //path 1
+    exploreOutcome: `There is nothing special here. Leave` //path 1
+};
+console.log(outcome.sleepOutcome);
+
+// function Game() {
+//     document.getElementById("story").innerHTML = '${currentInput}, you come across a huge cabin in a cold night with 3 doors. Which one do you enter- Door 1, 2 or 3? Type in the number of your choice.`;
+// }
+
   //Begin game: Enter name
 
 // Any path the user goes down must ask them at least three questions
@@ -49,4 +69,3 @@ var display = function( data ){
                 //if down - There are wild wolves inside! Sorry .End game (outcome)
                 //else if not - No adventure for today. End game(outcome)
             //else - Oops! you got caught in a booby trap while attempting to leave. End game(outcome)
-
