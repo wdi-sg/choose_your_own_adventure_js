f7ddbaa7677001e89e7a445c90ea4cb5de5a4fb4
upieez <samuelhww@gmail.com>
2019-12-15 23:07:46 +0800

    change code and made function for layers

--- part2/script.js
+++ part2/script.js
@@ -1,119 +1,95 @@
 console.log("Choose your own adventure!");
 
-// Pseudo-code
-/*
-
-// What I need to do is to not complicate things and focus on the first small few steps
-
-step 1 : i need to have my user input his/her name. counter will be 0
-step 2 : after use enters,  displays the message that he needs to choose the options
-step 3 : counter will be 1 now. I need to make user enter a/b/c and enter the scenario
-step 4 : after entering a scenario, user needs to choose the options on where to go
-step 5 : user will get the desired response
-
-// Scenario stages explanation
-Scenario have to go from layers to layers to layers.
-Scenario 0
-user will have to input the name
-Scenario 1
-user choosing where to bring the date.
-if user chooses movie goes to scenario 2
-Scenario 2
-user will have two options.
-if user chooses horror
-Scenario 3
-the outcome of the date will happened i.e scare
-*/
-
 // Global variable
 
 var name = "";
 
 // create the object to store the layer and responses
 
-var storyStages = [
-{
-    stage0: {
-        stageA: `Hello and welcome ${name}! Choose option A, B, C`
-    }
-},
-{
-    stage1: {
-        stageA: "movies",
-        stageB: "dinner",
-        stageC: "market"
-    }
-},
-{
-    stage2: {
-        stageA: {
-            optionA: "horror!",
-            optionB: "comedy!",
-            optionC: "action!"
-        },
-        stageB: {
-            optionA: "chinese!",
-            optionB: "western!",
-            optionC: "japanese!"
-        },
-        stageC: {
-            optionA: "games!",
-            optionB: "theme park!"
-        }
-    }
-},
-{
-    stage3: {
-        stageA: {
-            optionA: "Like it!",
-            optionB: "Average show!",
-            optionC: "Love it!"
-        },
-        stageB: {
-            optionA: "Love it!",
-            optionB: "Hate it!",
-            optionC: "Average food!"
-        },
-        stageC: {
-            optionA: "Love games!",
-            optionB: "It's fine!"
-        }
-    }
-}];
-
-// create the function to loop through the layers(stages) and responses
-
-var storySelect = ["stage0", "stage1", "stage2", "stage3"];
-var storyStage = ["stageA", "stageB", "stageC"];
-var storyOption = ["optionA", "optionB", "optionC"];
-var layer = 0;
-
-var story = function(input){
-    name = input;
-    console.log(name);
-    for (var i = 0; i < storyStages.length; i++){
-        var mainStorer = storyStages[i];
-        for (var j = 0; j < storySelect.length; j++){
-            var stageStorer = mainStorer[storySelect[j]];
-            for (var k = 0; k < storyStage.length; k++){
-                var finalStory = stageStorer[storyStage[k]];
-                if(layer === 0){
-                    layer++;
-                    return finalStory;
-                    break;
-                }
-            }
-            break;
-        }
-        break;
+var storyA = {
+    layer1: "You have chosen movies! What would you like to watch? A. Horror B. Comedy C. Action",
+    layer2: {
+        A: "Horror works! Date will go out with you again",
+        B: "Comedy movie was a terrible idea!",
+        C: "Action is great! Will go out with you for the next date"
     }
 };
 
+var storyB = {
+    layer1: "You have chosen dinner! Where would you like to do? A. Japanese B. Chinese C. Fusion",
+    layer2: {
+        A: "Sushi was a great idea! Will go out with you again",
+        B: "Chinese was bad! Will not go out with you again",
+        C: "Fusion was awesome! Will go out with you again"
+    }
+};
+
+var storyC = {
+    layer1: "You have chosen night market! What would you like to do? A. Play arcade games B. Go on a roller coaster",
+    layer2: {
+        A: "You won a prize! Will go out with you again",
+        B: "Scary! Will not go out with you again"
+    }
+};
+
+// create the function to loop through the layers(stages) and responses
+
+var storyLayer = ["layer1", "layer2", "layer3"];
+var storyOption = ["A", "B", "C"];
+var layer = 0;
+var option = "";
+
+var storySelection = function(input){
+     if (layer === 2){
+        if (input === "A"){
+            option = input;
+            return storyA[storyLayer[0]];
+        } else if (input === "B"){
+            option = input;
+            return storyB[storyLayer[0]];
+        } else if (input === "C"){
+            option = input;
+            return storyC[storyLayer[0]];
+        }
+    } else if (layer === 3){
+        if(option === "A"){
+            if (input === "A"){
+            return storyA[storyLayer[1]][storyOption[0]]
+            } else if (input === "B"){
+                return storyA[storyLayer[1]][storyOption[1]]
+            } else if (input === "C"){
+                return storyA[storyLayer[1]][storyOption[2]]
+            }
+        } else if(option === "B"){
+            if (input === "A"){
+                return storyB[storyLayer[1]][storyOption[0]]
+            } else if (input === "B"){
+                return storyB[storyLayer[1]][storyOption[1]]
+            } else if (input === "C"){
+                return storyB[storyLayer[1]][storyOption[2]]
+            }
+        } else if(option === "C"){
+            if (input === "A"){
+                return storyC[storyLayer[1]][storyOption[0]]
+            } else if (input === "B"){
+                return storyC[storyLayer[1]][storyOption[1]]
+            }
+        }
+    }
+}
+
 var inputHappened = function(currentInput){
-
-    console.log(layer);
-    return story(currentInput);
-
+    name = currentInput;
+    if (layer === 0){
+        layer++;
+        return "Hi! " + name + " choose your option: A. Movies B. Dinner C. Night Market"
+    } else if (layer === 1){
+        layer++;
+        return storySelection(currentInput);
+    } else if (layer === 2){
+        layer++
+        return storySelection(currentInput);
+    }
 };
 
 var display = function( data ){
