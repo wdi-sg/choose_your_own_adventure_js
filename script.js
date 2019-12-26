214eddf56b63b55d92e1571e5d7565716416e7cc
joycemap <57893220+joycemap@users.noreply.github.com>
2019-12-27 00:43:22 +0800

    Improved Game Play

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ var output;
 var stage = 0; // set the game play level: choose doctor is stage 0; choose planet is stage 1, monster stage 2, tool stage 3
 var inputHappened = function (currentInput) {
   var doctorChoice = [11, 12, 13];
-  var planetChoice = ["Skaros", "Mondas", "Earth"];
-  var monsterChoice = ["Daleks", "Cybermen", "Mutant Spiders"];
-  var toolChoice = ["Sonic Screwdriver", "Boombox", "Gun"];
+  var planetChoice = ["S", "M", "E"];
+  var monsterChoice = ["D", "C", "MS"];
+  var toolChoice = ["S", "B", "G"];
 
 switch (stage) {
   case 0:
-    switch (currentInput) {
+    switch (parseInt(currentInput)) {
 
       case doctorChoice[0]:
-        output = "Want some fish fingers and custard? Pick a planet - Skaros, Mondas, or Earth?";
+        output = "Want some fish fingers and custard? Pick a planet - Skaros(S), Mondas(M), or Earth(E)?";
         stage += 1;
         break;
       case doctorChoice[1]:
-        output = "You picked me for my eyebrows. Now pick a planet - Skaros, Mondas, or Earth?";
+        output = "You picked me for my eyebrows. Now pick a planet - Skaros(S), Mondas(M), or Earth(E)?";
         stage += 1;  
         break;
       case doctorChoice[2]:
-        output = "Reverse the polarity! Pick a planet - Skaros, Mondas, or Earth?";
+        output = "Reverse the polarity! Pick a planet - Skaros(S), Mondas(M), or Earth(E)?";
         stage += 1;
         break;
       }
@@ -33,15 +33,15 @@ switch (stage) {
   case 1:
     switch (currentInput) {
       case planetChoice[0]:
-        output = "You are an ambitious one! There will be danger. They come in so many forms. You get to pick. Daleks. Cybermen, or Mutant Spiders?";
+        output = "You are an ambitious one! There will be danger. They come in so many forms. You get to pick. Daleks(D), Cybermen(C), or Mutant Spiders(MS)?";
         stage += 1;
         break;
       case planetChoice[1]: 
       stage += 1;  
-      output = "You are brave! There will be danger. They come in so many forms. You get to pick. Daleks. Cybermen, or Mutant Spiders?";
+      output = "You are brave! There will be danger. They come in so many forms. You get to pick. Daleks(D), Cybermen(C), or Mutant Spiders(MS)?";
       break;  
       case planetChoice[2]: 
-      output = "Earth is defended! Let's fight some Monsters. There are so many, you get to pick. Daleks. Cybermen, or Mutant Spiders?";
+      output = "Earth is defended! Let's fight some Monsters. There are so many, you get to pick. Daleks(D), Cybermen(C), or Mutant Spiders(MS)?";
       stage += 1;
       break;
       }
@@ -50,19 +50,19 @@ switch (stage) {
     switch (currentInput) {
       
       case monsterChoice[0]: 
-        output = "We'll need some tools! Sonic Screwdriver, a boombox or a gun?";
+        output = "We'll need some tools! Sonic Screwdriver(S), Boombox(B) or Gun(G)?";
         stage += 1;
         break;
       case monsterChoice[1]: 
-      output = "They delete people like you! Pick a Tool: Sonic Screwdriver, a boombox or a gun?";
+      output = "They delete people like you! Pick a Tool: Sonic Screwdriver(S), Boombox(B) or Gun(G)?";
       stage += 1;
       break;  
       case monsterChoice[2]: 
-        output = "Big and hairy! Which tool is best: Sonic Screwdriver, a boombox or a gun?";
+        output = "Big and hairy! Which tool is best: Sonic Screwdriver(S), Boombox(B) or Gun(G)?";
         stage += 1;
         break;
       }
-      break;    
+     break;    
 
   case 3:
     switch (currentInput) {
@@ -71,7 +71,7 @@ switch (stage) {
         output = "Yes! You saved the day!";
         break;
       case toolChoice[1]: 
-        output = "Interesting Choice but it\'s not strong enough!";
+        output = "Interesting choice but it's not strong enough!";
         break;  
       case toolChoice[2]: 
         output = "You know I don't use guns! Get off my TARDIS!";
