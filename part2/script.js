d4ac19eea2c3385a6eb023d7aef9ed6d08c979d7
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-22 03:06:05 +0800

    Further Functionality 6 - Implemented combat, with enemy hp, user hp, battle loop, and weapon bonuses

--- part2/script.js
+++ part2/script.js
@@ -1,9 +1,23 @@
 console.log("hello script js");
 
 var scoreCount = 0;
-var failedAttempts = 20;
+var hp = 20;
 var currentRoom = "at home";
 var groceryStore;
+var monsters = {
+    "granny": 3,
+    "Yakuza Boss": 3,
+    "Angry Student": 3,
+    "Dancing Man": 3,
+    "Member Of Parliament": 3,
+    "Ninja Warrior": 3,
+    "Baby Yoda": 3,
+    "Papa Mando": 3,
+    "Thomas Shelby": 3
+}
+var weaponBonus = 0;
+var currentMonster;
+var fightTurn = "user";
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
@@ -12,7 +26,7 @@ var inputHappened = function(currentInput){
   if (currentRoom === "at home"){
     username = currentInput;
     currentRoom = "outside";
-    return formatText(`Hey ${username}, it's panic buying season! The objective is to buy 10 items. You can only fail 20 times!Which grocery store do you want to go to?Cold Storage / FairPrice / Sheng Siong`)
+    return formatText(`Hey ${username}, it's panic buying season! The objective is to defeat the panic-buyers! You get one point per defeated buyer. If you get 5 points, you win! Which grocery store do you want to go to?Cold Storage / FairPrice / Sheng Siong`)
   }
 
 //Choosing a Grocery Store
@@ -30,75 +44,51 @@ var inputHappened = function(currentInput){
   if (currentRoom === "cold storage" || currentRoom === "fairprice" || currentRoom === "sheng siong"){
     if (currentInput.includes("1") || currentInput.includes("2") || currentInput.includes("3")){
         currentRoom = currentRoom + " aisle " + currentInput;
-        return formatText(`You are now in ${currentRoom}. You can try to buy something, or leave the aisle. (Buy / Leave)`);
+        currentMonster = getRandomMonster();
+        return formatText(`You are now in ${currentRoom}. But wait! You encounter a ${currentMonster}. You can try to fight him, or leave the aisle. (Fight / Leave)`);
     } else {
         return `Please type 1 or 2 or 3, to access an aisle`
     }
   }
 
-//Buy or Leave
+//Fight or Leave
   if (currentRoom.includes("aisle")){
-    if (currentInput.toLowerCase().includes("buy")){
-        return getItem();
+    if (currentInput.toLowerCase().includes("fight") && fightTurn === "user"){
+        fightTurn = "monster";
+        return formatText(hitMonster());
+    } else if (currentInput.toLowerCase().includes("fight") && fightTurn === "monster"){
+        fightTurn = "user";
+        return formatText(monsterAttack());
     } else if(currentInput.toLowerCase().includes("leave")){
         currentRoom = groceryStore;
         return formatText(`You are now back in ${currentRoom}'s main area! Pick an aisle to get stuff! (1/2/3)`);
     } else{
-        return formatText(`Sorry, we didn't get that. Type "buy" or "leave" to buy something, or leave the aisle`);
+        return formatText(`Sorry, we didn't get that. Type "fight" or "leave" to fight him, or leave the aisle`);
     }
   }
 
-  // if (currentRoom === "cold storage aisle 1"){
-  //   return aisleFunction();
-  // }
-
-  // if (currentRoom === "cold storage aisle 2"){
-  // }
-
-  // if (currentRoom === "cold storage aisle 3"){
-  // }
-
-  // if (currentRoom === "fairprice aisle 1"){
-  // }
-
-  // if (currentRoom === "fairprice aisle 2"){
-  // }
-
-  // if (currentRoom === "fairprice aisle 3"){
-  // }
-
-  // if (currentRoom === "sheng siong aisle 1"){
-  // }
-
-  // if (currentRoom === "sheng siong aisle 2"){
-  // }
-
-  // if (currentRoom === "sheng siong aisle 3"){
-  // }
-
-
-
   return "error";
+
 };
 
 //determines if successful at buying item
-var getItem = function(){
-    if (Math.random() > 0.5){
-        scoreCount++;
-        if (scoreCount === 10) {
-            return formatText(`Congratulations! You win! Take home the prize of another ${randomItem()}`)
-        } else {
-            return formatText(`You managed to buy ${randomItem()}!Your current score is ${scoreCount}. You have ${failedAttempts} failed attempts left.Try this aisle again? or another aisle? (Buy / Leave)`);
-        }
-    } else {
-        failedAttempts--;
-        if (failedAttempts === 0){
-            return formatText(`You lose :( . Refresh the page to try this game again.`)
-        } else{
-            return formatText(`They are all sold out! You have ${failedAttempts} failed attempts left. Try this aisle again? or another aisle? (Buy / Leave)`);
-        }
-    }
-}
+// var getItem = function(){
+//     if (Math.random() > 0.5){
+//         scoreCount++;
+//         if (scoreCount === 10) {
+//             return formatText(`Congratulations! You win! Take home the prize of another ${randomItem()}`)
+//         } else {
+//             return formatText(`You managed to buy ${randomItem()}!Your current score is ${scoreCount}. You have ${failedAttempts} failed attempts left.Try this aisle again? or another aisle? (Buy / Leave)`);
+//         }
+//     } else {
+//         failedAttempts--;
+//         if (failedAttempts === 0){
+//             return formatText(`You lose :( . Refresh the page to try this game again.`)
+//         } else{
+//             return formatText(`They are all sold out! You have ${failedAttempts} failed attempts left. Try this aisle again? or another aisle? (Buy / Leave)`);
+//         }
+//     }
+// }
 
 //gets random item, if successful in buying it
 var randomItem = function(){
@@ -108,7 +98,7 @@ var randomItem = function(){
     } else if (number > 7) {
         return "hand sanitizer"
     } else {
-        return "canned food"
+        return "flamethrower"
     }
 }
 
@@ -117,7 +107,8 @@ var clearInput = function(){
     document.getElementById('input').value = "";
 }
 
-var formatText = function(text){
+//Format Text Function
+var formatText = function( text ){
     var formattedText ="";
     for (let char of text){
         if (char === "!" || char === "." || char === "?"){
@@ -128,6 +119,7 @@ var formatText = function(text){
     return formattedText;
 }
 
+//Display Function
 var display = function( data ){
   var displayElement = document.querySelector('#output');
 
@@ -136,6 +128,60 @@ var display = function( data ){
 
   // put the data into the div
   output.innerText = data;
-};
+}
 
+//Check if any monster still alive
+// var allMonstersDead = function(){
+//     for (let monster in monsters){
+//         if (monsters[monster] <= 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+//Fighting Monster and Buying Item
+var hitMonster = function(){
+    var num = Math.random();
+    if (num < 0.5){
+        monsters[currentMonster] -= (weaponBonus + 1);
+        if (monsters[currentMonster] <= 0 && scoreCount === 5){
+            return `You have defeated 5 enemies and won the game! Congratulations! Celebrate with some ${randomItem}`
+        } else if (monsters[currentMonster] <= 0) {
+            weaponBonus += 1;
+            delete monsters[currentMonster];
+            scoreCount++;
+            return `You scored a final hit! You have defeated ${currentMonster}. Hooray!You have now procured a ${randomItem()}, which will +1 to your attacks.Your current attack power is ${weaponBonus}.\n Type leave to leave the aisle now`
+
+        } else {
+            return `You scored a hit! Your health is now ${hp}. The ${currentMonster}'s hp is now ${monsters[currentMonster]}. Type 'fight' to keep going!`;
+        }
+    } else {
+            return `You missed. Your health is now ${hp}. The ${currentMonster}'s hp is now ${monsters[currentMonster]}. Type 'fight' to keep going!`;
+        }
+}
+
+
+var monsterAttack = function(){
+    var num = Math.random();
+    if (num < 0.5){
+        hp--;
+        if (hp === 0){
+            output = "You have died :(. Try again?"
+        } else {
+        return `The ${currentMonster} hit you! Your health is now ${hp}. The ${currentMonster}'s hp is now ${monsters[currentMonster]}. Type 'fight' to keep going!`;
+        }
+    } else {
+        return `The ${currentMonster} missed! Your health is now ${hp}. The ${currentMonster}'s hp is now ${monsters[currentMonster]}. Type 'fight' to keep going!`;
+    }
+}
+
+var getRandomMonster = function(){
+    var keys = Object.keys(monsters);
+    return keys[Math.floor(Math.random() * keys.length)];
+
+}
+
+
+//Initial Display
 display("What's your name?")
