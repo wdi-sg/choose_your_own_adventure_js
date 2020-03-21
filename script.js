e2b8171c39a8885045a295ee4d6003c46ea880ea
travisenquiry <travisenquiry@gmail.com>
2020-03-21 12:41:11 +0800

    added character name logic

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 console.log("hello script js");
+var name;
+var path;
+var steps = 0;
+
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "SOMETHING HAPPENED";
+
+  console.log(steps);
+  switch(steps){
+    case 0:
+      steps++;
+      return "Welcome adventurer, please enter your name";
+      break;
+
+    case 1:
+      if(currentInput.match(/^[a-z]+$/i)){
+        name = currentInput;
+        steps++;
+        return name;
+        break;
+      }else{
+        return "Special characters are not allowed in name. Please reenter a new name."
+        break;
+      }
+  }
+
 };
 
+      var display = function( data ){
+        var displayElement = document.querySelector('#output');
 
+        // get rid of the entire contents
+        displayElement.innerHTML = "";
+
+        // put the data into the div
+        output.innerText = data;
+      };
