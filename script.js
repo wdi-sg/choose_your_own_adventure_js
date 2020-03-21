7798186017df1771981d85f658840e0dee60a289
kokchuantan <kokchuan1996@gmail.com>
2020-03-21 19:02:48 +0800

    added base logic

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 console.log("hello script js");
 
+var userInput;
+var randomNum;
+
+
+document.querySelector('#output').innerText ='Enter a number between 1 to 100 to begin.';
+
+var random10 = Math.floor(Math.random() * 10);
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "SOMETHING HAPPENED";
+  userInput = parseInt(currentInput);
+  console.log( userInput );
+  if(userInput != 0){
+    if(userInput <= 100 && userInput >= 1){
+      return "SOMETHING HAPPENED";
+    }
+    else{
+      return 'Invalid input, please try again.'
+    }
+  }
+  else if(userInput == 0){
+    console.log('reset');
+    return 'restart'
+  }
 };
-
-
