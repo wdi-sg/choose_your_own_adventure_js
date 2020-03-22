af867f375725e244beba7cc70a792fdf103d7c7f
lekhweemeng <lek.hweemeng@live.com>
2020-03-23 00:06:27 +0800

    Added in logic for non acceptable values

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var inputHappened = function(currentInput){
     var output = "Good Evening " + userName[0] + ". We come to your class advancement. Please choose to advance into one of the below classes : \r Warrior, \r Magician.";
     return output;
     }
-        if (userName.length === 1 && classArray.length === 0){
+        if (userName.length === 1 && classArray.length === 0 && (currentInput === "Warrior" || currentInput === "Magician")){
             classArray.push(currentInput);
             score = score + 1;
             var output = "Ahhhh, you have chosen the path of a Mighty " + classArray[0] + ". To aid in your quest of Great Strength, please choose one of the below, which, do you feel the closest to: \r Light, \r Flame, \r Earth, \r Death." + "\n Points of destruction: " + score;
@@ -33,7 +33,11 @@ var inputHappened = function(currentInput){
                     var output = "Welcome! Our Great Paladin! From the Hall of Light, please protect the people and ward off the evil." + "\n Points of destruction: " + score;
                     return output;
                 }
-            else if (userName.length === 1 && classArray[0] === "Warrior" && currentInput === "Flame"){
+                else if (userName.length === 1 && classArray[0] === "Warrior" && subClassArray[0] === "Light"){
+                var output = "You haven Not Selected the correct response. Please enter only the below: \r Sword, \r Mace.";
+                return output;
+                }
+            if (userName.length === 1 && classArray[0] === "Warrior" && currentInput === "Flame"){
                     subClassArray.push(currentInput);
                     score = score + 5;
                     var output = "Welcome! The Destroyer! Please choose one of the below token and proceed to the hall of destruction and begin your Journey: \r Lantern. \r Oil." + "\n Points of destruction: " + score;
@@ -49,7 +53,11 @@ var inputHappened = function(currentInput){
                     var output = "Welcome! Our Great Destroyer! Please raze the evil off the land." + "\n Points of destruction: " + score;
                     return output;
                }
-            else if (userName.length === 1 && classArray[0] === "Warrior" && currentInput === "Earth"){
+                else if (userName.length === 1 && classArray[0] === "Warrior" && subClassArray[0] === "Flame"){
+                var output = "You haven Not Selected the correct response. Please enter only the below: \r Flame, \r Oil.";
+                return output;
+                }
+            if (userName.length === 1 && classArray[0] === "Warrior" && currentInput === "Earth"){
                     subClassArray.push(currentInput);
                     score = score + 1;
                     var output = "Welcome! Our Mighty Tank! Please choose one of the below token and proceed to the hall of Shields and begin your Journey: \r Shield, \r Plate Armor." + "\n Points of destruction: " + score;
@@ -65,7 +73,11 @@ var inputHappened = function(currentInput){
                     var output = "Welcome! Our Great Tank! Please protect the sanity of the land." + "\n Points of destruction: " + score;
                     return output;
                 }
-            else if (userName.length === 1 && classArray[0] === "Warrior" && currentInput === "Death"){
+                else if (userName.length === 1 && classArray[0] === "Warrior" && subClassArray[0] === "Earth"){
+                var output = "You haven Not Selected the correct response. Please enter only the below: \r Shield, \r Plate Armor.";
+                return output;
+                }
+            if (userName.length === 1 && classArray[0] === "Warrior" && currentInput === "Death"){
                     subClassArray.push(currentInput);
                     score = score + 3;
                     var output = "Welcome! The commander of darkness! Please choose one of the below token and proceed to the graveyard and begin your Journey: \r Tombstone, \r Shovel." + "\n Points of destruction: " + score;
@@ -81,7 +93,11 @@ var inputHappened = function(currentInput){
                     var output = "Welcome! Our Death Knight! Please rise the troops and protect our lands." + "\n Points of destruction: " + score;
                     return output;
                 }
-            else if (userName.length === 1 && classArray[0] === "Magician" && currentInput === "Light"){
+                else if (userName.length === 1 && classArray[0] === "Warrior" && subClassArray[0] === "Death"){
+                var output = "You haven Not Selected the correct response. Please enter only the below: \r Tombstone, \r Shovel.";
+                return output;
+                }
+            if (userName.length === 1 && classArray[0] === "Magician" && currentInput === "Light"){
                     subClassArray.push(currentInput);
                     score = score + 2;
                     var output = "Welcome! The giver of life, Cleric! Please choose one of the below token and proceed to the Church and begin your Journey: \r Staff, \r Holy Water." + "\n Points of destruction: " + score;
@@ -97,7 +113,11 @@ var inputHappened = function(currentInput){
                     var output = "Welcome! Our Great Cleric! Purify and protect our lands." + "\n Points of destruction: " + score;
                     return output;
                 }
-            else if (userName.length === 1 && classArray[0] === "Magician" && currentInput === "Flame"){
+                else if (userName.length === 1 && classArray[0] === "Magician" && subClassArray[0] === "Light"){
+                var output = "You haven Not Selected the correct response. Please enter only the below: \r Staff, \r Holy Water.";
+                return output;
+                }
+            if (userName.length === 1 && classArray[0] === "Magician" && currentInput === "Flame"){
                     subClassArray.push(currentInput);
                     score = score + 10;
                     var output = "Welcome! The yielder of the Flame of Purgatory! Please choose one of the below token and proceed to the Fire Place and begin your Journey: \r Spell Book, \r Cloak." + "\n Points of destruction: " + score;
@@ -113,7 +133,11 @@ var inputHappened = function(currentInput){
                     var output = "Welcome! Our Flame Mage! Raise the fire of protection and deter the evils." + "\n Points of destruction: " + score;
                     return output;
                }
-            else if (userName.length === 1 && classArray[0] === "Magician" && currentInput === "Earth"){
+                else if (userName.length === 1 && classArray[0] === "Magician" && subClassArray[0] === "Flame"){
+                var output = "You haven Not Selected the correct response. Please enter only the below: \r Spell Book, \r Cloak.";
+                return output;
+                }
+            if (userName.length === 1 && classArray[0] === "Magician" && currentInput === "Earth"){
                     subClassArray.push(currentInput);
                     score = score + 1;
                     var output = "Welcome! The sweet-talker of Nature, Durid! Please choose one of the below token and proceed to the Forest and begin your Journey: \r Tree Branch, \r Animal Skull."  + "\n Points of destruction: " + score;
@@ -129,7 +153,11 @@ var inputHappened = function(currentInput){
                     var output = "Welcome! Our Durid! Raise the pack and deter the evils." + "\n Points of destruction: " + score;
                     return output;
                 }
-            else if (userName.length === 1 && classArray[0] === "Magician" && currentInput === "Death"){
+                else if (userName.length === 1 && classArray[0] === "Magician" && subClassArray[0] === "Earth"){
+                var output = "You haven Not Selected the correct response. Please enter only the below: \r Tree Branch, \r Animal Skull.";
+                return output;
+                }
+            if (userName.length === 1 && classArray[0] === "Magician" && currentInput === "Death"){
                     subClassArray.push(currentInput);
                     score = score + 3;
                     var output = "Welcome! Commander of the dead, Necromancer! Please choose one of the below token and proceed to the Forest and begin your Journey: \r Casket, \r Bandage." + "\n Points of destruction: " + score;
@@ -139,9 +167,22 @@ var inputHappened = function(currentInput){
                     score = score + 15;
                     var output = "Welcome! Necromancer! Please protect the land with the army of skeletons." + "\n Points of destruction: " + score;
                     return output;
-                } else if (userName.length === 1 && classArray[0] === "Magician" && subClassArray[0] === "Death" && currentInput === "Bandage"){
+                }
+                else if (userName.length === 1 && classArray[0] === "Magician" && subClassArray[0] === "Death" && currentInput === "Bandage"){
                     score = score + 20;
                     var output = "Welcome! Necromancer! Please protect the land with the army of Mummies." + "\n Points of destruction: " + score;
                     return output;
-            }
+                }
+                else if (userName.length === 1 && classArray[0] === "Magician" && subClassArray[0] === "Death"){
+                var output = "You haven Not Selected the correct response. Please enter only the below: \r Casket, \r Bandage.";
+                return output;
+                }
+        else if (userName.length === 1 && classArray.length === 1){
+            var output = "You haven Not Selected the correct response. Please enter only the below: \r Light, \r Flame, \r Earth, \r Death.";
+            return output;
+        }
+    else if (userName.length === 1 && classArray.length === 0){
+          var output = "You have entered a wrong response. Please restart and enter: \r Warrior, \r Magician";
+          return output;
+    }
 };
