f91b09293680fea654cd72f6fe1584a0cf842b50
Kenan <xinkenan.11@hotmail.com>
2020-03-23 10:35:26 +0800

    initial commit

---
+++ plots.js
@@ -0,0 +1,112 @@
+const genRandomNum = (min, max) => (min-1) + Math.ceil(Math.random() * (max-min + 1));
+
+class Plots {
+
+  constructor(plots) {
+    this.plots = plots;
+    this.init();
+  }
+
+  init() {
+    this.shufflePlots();
+    this.setChoices();
+  }
+
+  // for each plot, randomly push 2 other plots as choices
+  setChoices() {
+    this.plots.forEach(plot => {
+      for (let i = 0; i < 2; i++) {
+        let randomId = genRandomNum(0, this.plots.length-1);
+        plot.addChoice(randomId);
+      }
+    })
+  }
+
+  getRandomPlot() {
+    let randomNum = genRandomNum(0, this.plots.length-1);
+    return this.getPlotById(randomNum);
+  }
+
+  getPlotById (id) {
+    return this.plots.find(plot => plot.id === id);
+  }
+
+  // randomly shuffle plots order
+  shufflePlots() {
+    for (let i = this.plots.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = this.plots[i];
+      this.plots[i] = this.plots[j];
+      this.plots[j] = temp;
+    }
+  }
+}
+
+class Plot {
+  constructor(id, choice, story, type) {
+    this.id = id;
+    this.choice = choice; // the choice to be displayed to user
+    this.story = story; // the story to show
+    this.type = type; // there are be move, suicide (terminate game)
+    this.choices = new Map();
+    this.fixedChoice = null;
+    this.numChoices = 0;
+  }
+
+  addChoice (plotId) {
+    this.choices.set(this.numChoices++, plotId);
+  }
+
+  addFixedChoice(choice) {
+    this.fixedChoice = new Map(choice);
+  }
+
+  // return all choices in array
+  getAllChoices() {
+    return [this.fixedChoice, ...this.choices];
+  }
+
+}
+
+let plot1 = new Plot(0, "Teleport to Miller's Lake.", "You are now at Miller's Lake. </br>" +
+  "Miller's Lake is a small lake in Redania, far north of Oxenfurt and around Brunwich village." +
+  "<br>" +
+  "Along its shore is Brunwich to the north, the Old Chapel and Upper Mill to the east, and an abandoned site to the west. <br>" +
+  "There's also 2 small islands in the middle of the lake, with one of them having an ancient altar on it. <br>" +
+  "Interestingly, in the waters on the north side one can find ruins of a building. <br>" +
+  "It's unclear how it collapsed though. <br>" +
+  "Curiously, there seem to be a set of used outfit laying on a rock by the lake.","move");
+plot1.addFixedChoice([["choice" , 'Take the outfit and wear it immediately.'],
+  ["response", "You have been infected with the cov19 virus. " +
+  "<br> Tips: Avoid touching belongings of other people unless you have to and always wash your hands for at least" +
+  "20 seconds."],["type","suicide"]]);
+
+let plot2 = new Plot(1,  "Visit the Old Chapel", "You are now at the Old Chapel. <br> It was once a place that worshiped the Eternal Fire until a mother, " +
+  "suffering from postpartum depression, drowned all her daughters here before taking her own life. " +
+  "Her spirit, the Cursed Mother, haunts the crypt, thus making the church unusable. <br> There is a piece of ancient parchment on the altar inside the chapel. ","move");
+plot2.addFixedChoice([["choice", "Investigate the parchment."], ["response", "Upon examining the parchment, you learnt that there might be a dragon ball located near an abandoned site near Brunwich."], ["type", "view"]]);
+
+let plot3 = new Plot(2, "Visit Brunwich.", "You woke up in Brunwich. <br>This picturesque little village's beauty makes it a frequent subject of works by bards and poets. The best-known piece whose action is set in Brunwich is \"The Betrothing,\" a farce penned by Stanislas Islas." +
+  " The reputation thus gained has made it a popular destination for bohemians from Oxenfurt, " +
+  "who escape here during the hottest months to pass the time with contests for the most beautiful feathered cap and hunts for a golden horn held at night in the nearby woods.","move");
+plot3.addFixedChoice([["choice", "Move to Abandoned site"], ["Response", "This unnamed abandoned site is located in the Brunwich region, on the western shore of Miller's Lake. "],["type", "move"]] );
+
+let plot4 = new Plot(3, "Teleport to Gaunter's world", "You are now at Gaunter's world. <br>Gaunter's world is a twisted reality world created by Gaunter O'Dimm.<br>" +
+  "<br>" +
+  "The world includes several cemeteries, a cave filled with treasure, a rock with a sword protruding from it, and a house on a hill. It also has various points of interest, though these are meant to stall anyone who challenges O'Dimm.",'move');
+plot4.addFixedChoice([["choice", "Try to take out the sword."], ["response", "You tried your hardest but the sword would not move."],["type", "view"]]);
+
+let plot5 = new Plot(4, "Pay a visit at Borsody Auction House", "You are now looking at Borsody Auction House. <br> The auction house was originally started in 1203 and used to be just a shack located outside Novigrad's walls and where they traded in horses and sheep." +
+  " 42 years later in 1245, the business had grown in fame and size to the point they were able to move not just into Novigrad, but right off the city's main road in the nicest district, occupying an impressive 3-story building.<br>" +
+  "<br>" +
+  "Long past their days of trading cattle, they now dealt exclusively in jewels, artwork, antiques, and collector items. However, these auctions were only held every 3 months and always on a Friday, making people show up in droves.<br>" +
+  "It looks like an auction is going on.");
+plot5.addFixedChoice([["choice", "Join the auction"], ["response", "To your utter astonishment, the item being actioned is one of the Dragon Balls! <br> You bought the dragon ball without hesitation."],["type", "reward"] ]);
+
+
+
+allPlots = [plot1, plot2, plot3, plot4, plot5];
+
+
+
+
--- script.js
+++ script.js
@@ -1,8 +1,114 @@
-console.log("hello script js");
+// any path must ask at least 2 questions
+// there must be at least 6 total destinations
+// at least one of the questions there must be more than two possible user responses
+let userName = "";
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "SOMETHING HAPPENED";
+const coverStory = "This is year 1423, the lands the witcher has found its peace again, " +
+  " after Geralt of Rivia, Ciri Yennefer of Vengerberg together melted away the wild hunt's pollution." +
+  " Just when families were happily enjoying their McSpicy double while watching Star Wars on Netflix, a " +
+  "new crisis has appeared...\n" +
+  "An unknown spaceship has landed on earth and brought alone an hostile species called Cov17. " +
+  "The only way to defeat them, is to collect 5 dragon balls scattered across the lands." +
+  "You, a witcher, is hired by the government to collect 5 dragon balls and defeat the evil cov19.<hr>" +
+  "<em>To begin your quest, sign the contract by entering your name.</em>";
+
+const setIntro = (userName) => {
+  return `<div style="background-color: antiquewhite; color:#343229; display: inline-block; padding:30px" >
+      <h0 style="text-align: center">Work Release</h1>
+      <div style="text-align: left">
+        <p>Temple of Meltitele</p>
+        <p>Ellander, Temeria</p>
+        <p>Date: May 11 1432</p>
+        <p>This document is to certify that they are to accompany <b>${userName}</b>, Ravix of Popcorn, Butcher of McSpicy Double,
+        on a most dangerous contract to track down the 7 Dragons Balls.</p>
+      </div>
+      <div style="float:left">
+        <p>Signed:</p>
+        <p><em>${userName}</em></p>
+        <p>Bucher of McSpicy Double</p>
+      </div>
+      <div style="float: right">
+        <p>Signed:</p>
+        <p><em>Mendenek</em></p>
+        <p>Preistess of Melitele</p>
+      </div>
+    </div>`;
+};
+
+
+// a random number will be generated from lower and upper
+// window is located in the middle, if the random number fits in the window return true
+// the larger the window, the higher the chance of hitting window
+const clearInput = () => document.getElementById("input").value = "";
+const setPrompt = text => document.getElementById("input").setAttribute("placeholder", text);
+const chanceMaker = (lowerLimit, upperLimit, windowSize) => {
+  let randomNum = genRandomNum(lowerLimit, upperLimit);
+  let middle = upperLimit/2;
+  let leftWindowLimit = middle - windowSize;
+  let rightWindowLimit = middle + windowSize;
+  return randomNum > leftWindowLimit && randomNum < rightWindowLimit;
+};
+
+let nextPlot = null;
+let nextStory = null;
+let currPlot = null;
+let currStory = null;
+let currChoices = [];
+let currChoicesStr = null;
+let nextChoicesStr = null;
+let nextChoices = null;
+let gameHasStarted = false;
+let round = 0;
+let plots = null;
+let ballsCollected = 0;
+document.getElementById("output").innerHTML = coverStory;
+const setHtml = (html) => document.getElementById("out").innerHTML = html;
+const getChoicesStr = (plots, choices) => {
+  let choiceStr = "";
+  for (let i = 0; i < choices.length; i++) {
+    // first one is always fixed choice
+    if (i === 0) {
+      choiceStr += `${i}. ${choices[i].get('choice')}<br> `
+    }else {
+      choiceStr+= `${i}. ${plots.getPlotById(choices[i][1]).choice}<br>`;
+    }
+  }
+  return choiceStr;
+};
+const inputHappened = function (currentInput) {
+  if (!gameHasStarted) {
+    userName = currentInput;
+    gameHasStarted = true;
+    round++;
+    plots = new Plots(allPlots);
+    plots.shufflePlots();
+  }
+  if (gameHasStarted && round === 1) {
+    setPrompt("Press enter any key to continue ...");
+    round++;
+    clearInput();
+    nextPlot = plots.getRandomPlot();
+    nextChoices = nextPlot.getAllChoices();
+    return setIntro(userName);
+  }
+
+  clearInput();
+
+  currPlot = nextPlot;
+  currChoices = nextChoices;
+  nextPlot = plots.getPlotById( currChoices[  parseInt(currentInput)][1] );
+  nextChoices = nextPlot.getAllChoices();
+  // // set next story to be
+  // if (parseInt(currentInput) === 0) {
+  //   // currPlot remains the same
+  //   // next plot depends on urser input
+  //   currStory = currPlot.story;
+  //   currChoices = currPlot.getAllChoices();
+  //   nextStory = `${currStory} <br> ${currChoices[0].get('response')}`;
+  // }
+  round++;
+  return `${currStory} <br><hr>\n ${currChoicesStr}`;
+
 };
 
 
