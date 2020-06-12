cb51e9579a6a1bfdf4b876fc4d58d4243eb28e8c
metildachee <metildachee@gmail.com>
2020-06-12 21:05:11 +0800

    restart button working

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-console.log("hello script js");
+let state = {};
+var header = document.getElementById("header");
+var buttonGroup = document.getElementById("btn-group");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "SOMETHING HAPPENED";
-};
+function startGame() {
+  state = {};
+  displayQuestion(1);
+  displayButton(1);
+}
 
+function selectOption(id) {
+  if (id == -1) {
+    return startGame();
+  }
+  displayQuestion(id);
+  displayButton(id);
+}
+
+startGame();
+
+// HELP FUNCTIONS //
+function getTextNode(id) {
+  for (let i = 0; i < textNodes.length; i++) {
+    if (textNodes[i].id === id) {
+      return textNodes[i];
+    }
+  }
+}
+
+function displayQuestion(id) {
+  header.innerHTML = getTextNode(id).text;
+}
+
+function displayButton(id) {
+  let child = buttonGroup.lastElementChild;
+  while (child) {
+      buttonGroup.removeChild(child);
+      child = buttonGroup.lastElementChild;
+  }
+
+  for (let i = 0; i < getTextNode(id).option.length; i++) {
+    const button = document.createElement("button");
+    button.innerHTML = getTextNode(id).option[i].buttonText;
+
+    button.onclick = function() {
+      selectOption(getTextNode(id).option[i].nextNode);
+      console.log(getTextNode(id).option[i].nextNode);
+    }
+
+    buttonGroup.append(button);
+  }
+}
 
