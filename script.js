1d05b89d7815f0cf319a58ddf8f677d7ac763ced
metildachee <metildachee@gmail.com>
2020-06-13 22:32:18 +0800

    health and item tracking added, unable to end game when life is up

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 var username = "";
 var state = {};
+var lives = 5; // current lives
+var maxLives = 5; // max lives
 var header = document.getElementById("header");
 var buttonGroup = document.getElementById("btn-group");
+let health = [0x1F970, 0x1F60D, 0x1F63B, 0x1F496, 0x1F498];
+var healthBar = document.getElementById("health");
+
+document.getElementById("inventory").style.visibility = "hidden";
 
 document.getElementById("lets-play-button").addEventListener('click', function(event) {
     username = document.querySelector("#input").value;
-    if (validateInput(username) == "") {
+    if (validateInput(username) == "") { // validates name
         header.innerHTML = "Oops! Invalid input, please try again ~";
         document.querySelector("#input").value = "";
     }
     else {
         username = validateInput(username);
-        buttonGroup.lastElementChild.remove();
+        destroyAllButtons();
         document.querySelector("#input").remove();
+        document.getElementById("inventory").style.visibility = "visible";
         startGame();
     }
 });
 
 function startGame() {
+  console.log("The game restarts");
   state = { wand: false, keys: false, book: false };
+  displayFullHealth();
   displayQuestion(1);
   displayButton(1);
 }
@@ -27,9 +36,12 @@ function selectOption(id) {
   if (id == -1) { // reset
     return startGame();
   }
+  // displayItems();
   displayQuestion(id);
   displayButton(id);
 }
+
+// TEXTNODE FUNCTIONS //
 
 function getTextNode(id) {
   for (let i = 0; i < textNodes.length; i++) {
@@ -39,17 +51,17 @@ function getTextNode(id) {
   }
 }
 
-function displayQuestion(id) {
-  header.innerHTML = username + ", " + getTextNode(id).text;
+// BUTTONS FUNCTION //
+function destroyAllButtons() {
+    let child = buttonGroup.lastElementChild;
+    while (child) {
+        buttonGroup.removeChild(child);
+        child = buttonGroup.lastElementChild;
+    }
 }
 
 function displayButton(id) {
-  let child = buttonGroup.lastElementChild; // Destroy all previous buttons
-  while (child) {
-      buttonGroup.removeChild(child);
-      child = buttonGroup.lastElementChild;
-  }
-
+   destroyAllButtons();
   let randomNumber = getRandomNumber(getTextNode(id).option.length);
   for (let i = 0; i < randomNumber; i++) {
     if (checkState(id, i)) {
@@ -57,9 +69,12 @@ function displayButton(id) {
         button.innerHTML = getTextNode(id).option[i].buttonText;
 
         button.onclick = function() {
+
           selectOption(getTextNode(id).option[i].nextNode);
+          displayHealth(id, i);
           state = Object.assign(state, getTextNode(id).option[i].state);
           console.log(state);
+          displayItems();
         }
         buttonGroup.append(button);
     }
@@ -85,6 +100,12 @@ function checkState(id, index) {
     }
 }
 
+// TEXT FUNCTIONS //
+function displayQuestion(id) {
+  header.innerHTML = username + ", " + getTextNode(id).text;
+}
+
+// MISC FUNCTIONS //
 function getRandomNumber(arrayLength) {
     if (arrayLength == 1 || arrayLength == 2) {
         return arrayLength;
@@ -105,4 +126,60 @@ function validateInput(input) {
         }
     }
     return inputArray.join("");
+}
+
+function attackMode() {
+    // ask the user what type of weapon they want to use when the attack just begins
+    // the mage will attack you every 10 seconds
+    // she might miss or she might strike
+
+
+}
+// book is used to heal
+// wand is used to cast spells/ attack
+// need to have a health bar
+
+// takes in the current live and updates the lives
+// ITEMS FUNCTION //
+function displayItems() {
+    let itemsEmoji = [0x1F9D0, 0x1F5DD, 0x1F4DC];
+    // wand key book
+    const item = document.getElementById("items");
+    items.innerHTML = "";
+    let entries = Object.entries(state);
+    for (let i = 0; i < Object.keys(state).length; i++) {
+        if (entries[i][1]) { // if item state is true
+            items.innerHTML += String.fromCodePoint(itemsEmoji[i]);
+        }
+    }
+}
+
+// HEALTH FUNCTIONS //
+function displayHealth(id, index) {
+    // check if the current id and index has hurts, if yes, then
+    if (getTextNode(id).option[index].hasOwnProperty("hurts")) {
+        // the current selected option hurts the health
+        healthBar.innerHTML = ""; // clear out all previous health
+        lives--;
+        for (let i = 0; i < lives; i++) {
+            healthBar.innerHTML += String.fromCodePoint(health[i]); // update the life bar
+        } // good health is revealed
+        console.log("No. of lives: " + lives);
+        for (let j = lives; j < maxLives; j++) {
+            console.log("No. of times I run this loop: " + j);
+            healthBar.innerHTML += String.fromCodePoint(0x1F494);
+        } // updates dead hearts (if any)
+        console.log("Maximum live: " + maxLives);
+        console.log("Display end: " + lives);
+    }
+}
+
+function displayFullHealth() {
+    lives = maxLives;
+    console.log("Display full health - We have full health " + lives);
+    healthBar.innerHTML = "";
+    for (let i = 0; i < maxLives; i++) {
+      healthBar.innerHTML += String.fromCodePoint(health[i]);
+      console.log(String.fromCodePoint(health[i]));
+    }
 }
