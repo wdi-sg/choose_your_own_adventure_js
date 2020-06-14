92f133fcc9f15e4d3d9178842993f99d53fc72ab
joeyqlim <joeyqlim@gmail.com>
2020-06-14 17:43:14 +0800

    Added back functionality and background image.

--- script.js
+++ script.js
@@ -37,10 +37,11 @@ function submitAnswer() {
       switch(answer) {
         case "y":
           next(game.two.yes);
-          game.currentScore += 1;
+          game.currentScore += 6;
           break;
         case "n":
           next(game.two.no); // endpoint 1
+          game.currentScore -= 3;
           break;
         default:
           alert('Please enter a valid answer');
@@ -50,66 +51,77 @@ function submitAnswer() {
       switch(answer) {
         case "y":
           next(game.two.yes.yes); 
-          game.currentScore += 1;
+          game.currentScore += 2;
           break;
         case "n":
           next(game.two.yes.no); 
+          game.currentScore += 1;
           break;
         case "eat":
-        next(game.two.no.eat); 
+          next(game.two.no.eat); 
+          game.currentScore += 5;
         break;
       case "sleep":
         next(game.two.no.sleep); 
-        game.currentScore += 1;
+        game.currentScore += 3;
         break;
-        default:
-          alert('Please enter a valid answer');
+      case "back":
+        back(game.two); 
+        break;
+      default:
+        alert('Please enter a valid answer');
       }
       break;
     case 4: // hill, field, ocean or leave? | follow or bye | potato or tomato | hilltop or meadow
     switch(answer) {
       case "a":
-        next(game.two.yes.yes.a); 
+        next(game.two.yes.yes.a); // endpoint 1
         answerBox.innerText = "";
-        game.currentScore += 2;
+        game.currentScore += 10;
         break;
       case "b":
-        next(game.two.yes.yes.b); // endpoint 1
+        next(game.two.yes.yes.b); // endpoint 2
         answerBox.innerText = "";
-        game.currentScore += 1;
+        game.currentScore += 4;
         break;
       case "c":
-        next(game.two.yes.yes.c); // endpoint 2
+        next(game.two.yes.yes.c); // endpoint 3
         answerBox.innerText = "";
-        game.currentScore += 1;
+        game.currentScore += 7;
         break;
       case "d":
         next(game.two.yes.yes.d); 
+        game.currentScore -= 3;
         break;
       case "follow":
-        next(game.two.yes.no.follow); // endpoint 3
+        next(game.two.yes.no.follow); // endpoint 4
         answerBox.innerText = "";
-        game.currentScore += 1;
+        game.currentScore += 3;
         break;
       case "bye":
-        next(game.two.yes.no.bye); // endpoint 4
+        next(game.two.yes.no.bye); // endpoint 5
         answerBox.innerText = "";
+        game.currentScore -= 10;
         break;
       case "potato":
-        next(game.two.no.eat.potato); // endpoint 5
-        // answerBox.innerText = "";
+        next(game.two.no.eat.potato); // endpoint 6
+        answerBox.innerText = "";
+        game.currentScore -= 5;
         break;
       case "tomato":
-        next(game.two.no.eat.tomato); // endpoint 6
+        next(game.two.no.eat.tomato); // endpoint 7
         answerBox.innerText = "";
+        game.currentScore -= 5;
         break;
       case "hilltop":
-        next(game.two.no.sleep.hilltop); // endpoint 7
+        next(game.two.no.sleep.hilltop); // endpoint 8
         answerBox.innerText = "";
+        game.currentScore += 2;
         break;
       case "meadow":
-        next(game.two.no.sleep.meadow); // endpoint 8
+        next(game.two.no.sleep.meadow); // endpoint 9
         answerBox.innerText = "";
+        game.currentScore += 3;
         break;
       default:
         alert('Please enter a valid answer');
@@ -118,11 +130,13 @@ function submitAnswer() {
     case 5: // remember?
       switch(answer) {
         case "y":
-          next(game.two.yes.yes.d.yes); // endpoint 9
+          next(game.two.yes.yes.d.yes); // endpoint 10
           answerBox.innerText = "";
+          game.currentScore += 8;
           break;
         case "n":
           next(game.two.yes.yes.d.no); 
+          game.currentScore -= 8;
           break;
         default:
           alert('Please enter a valid answer');
@@ -131,12 +145,14 @@ function submitAnswer() {
     case 6: // run or sorry?
       switch(answer) {
         case "run":
-          next(game.two.yes.yes.d.no.run); // endpoint 10
+          next(game.two.yes.yes.d.no.run); // endpoint 11
           answerBox.innerText = "";
+          game.currentScore -= 100;
           break;
         case "sorry":
           next(game.two.yes.yes.d.no.sorry); // endpoint 12
           answerBox.innerText = "";
+          game.currentScore += 12;
           break;
         default:
           alert('Please enter a valid answer');
@@ -160,9 +176,9 @@ const game = {
   two: {
     text: `Hi there, yourname. Want to tumble with me (y/n)? \n┏ʕ ◕ ᴥ◕ ʔ┛\n\n`,
     yes: {
-      text: `That's wonderful, yourname. Will you hold my hand (y/n)?\n\n`,
+      text: `That's wonderful, yourname. Will you hold my hand (y/n/back)?\n[p.s. this is your only chance to go back and reset your score!]\n\n`,
       yes: {
-        text: `The little bear takes yourname's hand and leads ${name} to... \n (a) a clover-covered hill \n (b) a field of sunflowers \n (c) the big blue ocean \n (d) leave\n\n`,
+        text: `The little bear takes yourname's hand and leads you to... \n (a) a clover-covered hill \n (b) a field of sunflowers \n (c) the big blue ocean \n (d) leave\n\n`,
         a: {
           text: `So yourname and the bear cub spend the whole day in each other’s arms, tumbling down this clover-covered hill.\n\n`
         },
@@ -191,7 +207,7 @@ const game = {
         }
       },
       no: {
-        text: `The little bear hangs its head and tumbles away sadly. What will you do? (follow/bye)\n\n`,
+        text: `The little bear hangs its head and tumbles away sadly. What will you do? (follow/bye/back)\n\n`,
         follow: {
           text: `yourname follows the little bear, but it's too late. The little bear has disappeared into the Norwegian woods.\n\n`
         },
@@ -202,7 +218,7 @@ const game = {
       }
     },
     no: {
-      text: `"Okay then. What shall we do instead?" (eat/sleep)\n\n`,
+      text: `"Okay then. What shall we do instead?" (eat/sleep/back)\n[p.s. this is your only chance to go back and reset your score!]\n\n`,
       eat: {
         text: `"Yay, I love food! What shall we eat?" (potato/tomato)\n\n`,
         potato: {
@@ -217,10 +233,10 @@ const game = {
         text: `"Yay, sleeping is my second-most favourite activity. Where shall we take a nap?" (hilltop/meadow)
         \n\n`,
         hilltop: {
-          text: `yourname and the little bear reach the hilltop and doze off. ${name} starts to roll down the hill. Luckily, the little bear rescues ${name} and the two of them have the best nap ever, becoming best friends forever. \n ʕ◡ ᴥ ◡ʔ\n\n`
+          text: `You reach the hilltop and doze off together. yourname starts to roll down the hill. Luckily, the little bear jumps to the rescue and the two of them have the best nap ever, becoming best friends forever. \n ʕ◡ ᴥ ◡ʔ\n\n`
         },
         meadow: {
-          text: `yourname and the little bear find a peaceful spot in the meadow and doze off. A lion stealthily approaches, but the little bear is alert, and manages to fight it off. ${name} and the two of them have the best nap ever, becoming best friends forever.
+          text: `yourname and the little bear find a peaceful spot in the meadow and doze off. A lion stealthily approaches, but the little bear is alert, and manages to fight it off. The two of them have the best nap ever, becoming best friends forever.
           \n ʕ◡ ᴥ ◡ʔ\n\n`
         }
       }
@@ -230,7 +246,7 @@ const game = {
 
 // update score
 function updateScore() {
-  scoreBox.innerText = `Your current score is: ${game.currentScore}`
+  scoreBox.innerText = `Your score is: ${game.currentScore}`
 }
 
 // next question function
@@ -238,6 +254,13 @@ function next(s) {
   changeText(s.text);
   game.currentRound++;
   updateScore();
+}
+
+// go back function
+function back(s) {
+  changeText(s.text);
+  game.currentRound--;
+  game.currentScore = 0;
 }
 
 // initialise game question 1
