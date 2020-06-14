619974b6776bb9238fd53a886bfafbdb6032c3b3
joeyqlim <joeyqlim@gmail.com>
2020-06-14 18:00:34 +0800

    Cleaned up some typos and unused variables.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // UI elements for areas on the page
 const question = document.getElementById('question');
 const scoreBox = document.getElementById('score');
-const reset = document.getElementById('reset');
-const resetBtn = document.getElementById('reset-btn');
 const answerBox = document.getElementById('answer');
 const submitBtn = document.getElementById('submit-btn');
 const userInput = document.getElementById('user-input');
@@ -72,7 +70,7 @@ function submitAnswer() {
         alert('Please enter a valid answer');
       }
       break;
-    case 4: // hill, field, ocean or leave? | follow or bye | potato or tomato | hilltop or meadow
+    case 4: // hill, field, ocean or leave? | follow or bye? | potato or tomato | hilltop or meadow?
     switch(answer) {
       case "a":
         next(game.two.yes.yes.a); // endpoint 1
@@ -178,7 +176,7 @@ const game = {
     yes: {
       text: `That's wonderful, yourname. Will you hold my hand (y/n/back)?\n[p.s. this is your only chance to go back and reset your score!]\n\n`,
       yes: {
-        text: `The little bear takes yourname's hand and leads you to... \n (a) a clover-covered hill \n (b) a field of sunflowers \n (c) the big blue ocean \n (d) leave\n\n`,
+        text: `The little bear cub takes yourname's hand and leads you to... \n (a) a clover-covered hill \n (b) a field of sunflowers \n (c) the big blue ocean \n (d) leave\n\n`,
         a: {
           text: `So yourname and the bear cub spend the whole day in each other’s arms, tumbling down this clover-covered hill.\n\n`
         },
@@ -189,30 +187,30 @@ const game = {
           text: `So yourname and the bear cub spend the whole day in each other’s arms, tumbling down a sandy beach and into the big blue ocean.\n\n`
         },
         d: {
-          text: `The little bear looks up at yourname mournfully. “I want you always to remember me. Will you remember that I existed, and that I stood next to you here like this?” (y/n)
+          text: `The little bear cub looks up at yourname mournfully. “I want you always to remember me. Will you remember that I existed, and that I stood next to you here like this?” (y/n)
           \n\n`,
           yes: {
-            text: `Thank you, yourname. The little bear will never forget your kindness.\nʕ๑> ₃ <๑ʔ \n\n`
+            text: `Thank you, yourname. The little bear cub will never forget your kindness.\nʕ๑> ₃ <๑ʔ \n\n`
           },
           no: {
-            text: `The little bear's eyes turn black with rage. It snarls, revealing its razor-sharp teeth. (run/sorry)\nʕ•ˋ _ ˊ•ʔ\n\n`,
+            text: `The little bear cub's eyes turn black with rage. It snarls, revealing its razor-sharp teeth. (run/sorry)\nʕ•ˋ _ ˊ•ʔ\n\n`,
             run: {
-              text: `yourname tries to escape, but is eaten by the angry little bear.\nʕ๑•̀ㅁ•́๑ʔ \n\n`
+              text: `yourname tries to escape, but is eaten by the angry little bear cub.\nʕ๑•̀ㅁ•́๑ʔ \n\n`
             },
             sorry: {
-              text: `The little bear is kind and gracious. yourname is forgiven and allowed to leave.٩\nʕ๑> ₃ <๑ʔ 
+              text: `The little bear cub is kind and gracious. yourname is forgiven and allowed to leave.٩\nʕ๑> ₃ <๑ʔ 
               \n\n`
             }
           }
         }
       },
       no: {
-        text: `The little bear hangs its head and tumbles away sadly. What will you do? (follow/bye/back)\n\n`,
+        text: `The little bear cub hangs its head and tumbles away sadly. What will you do? (follow/bye)\n\n`,
         follow: {
-          text: `yourname follows the little bear, but it's too late. The little bear has disappeared into the Norwegian woods.\n\n`
+          text: `yourname follows the little bear cub, but it's too late. The little bear cub has disappeared into the Norwegian woods.\n\n`
         },
         bye: {
-          text: `You really shouldn't have crushed the little bear's feelings. The earth beneath your feet splits open and yourname tumbles into oblivion.
+          text: `You really shouldn't have crushed the little bear cub's feelings. The earth beneath your feet splits open and yourname tumbles into oblivion.
           \n\n`
         }
       }
@@ -222,10 +220,10 @@ const game = {
       eat: {
         text: `"Yay, I love food! What shall we eat?" (potato/tomato)\n\n`,
         potato: {
-          text: `yourname and the little bear share a red potato. Unfortunately, the little bear turns out to be allergic to red potatoes and dies from its allergic reaction. \n\n`
+          text: `yourname and the little bear cub share a red potato. Unfortunately, the little bear cub turns out to be allergic to red potatoes and dies from its allergic reaction. \n\n`
         },
         tomato: {
-          text: `yourname and the little bear share a green tomato. Unfortunately, you have forgotten that you are allergic to green tomatoes and you die from your allergic reaction. 
+          text: `yourname and the little bear cub share a green tomato. Unfortunately, you have forgotten that you are allergic to green tomatoes and you die from your allergic reaction. 
           \n\n`
         }
       },
@@ -233,10 +231,10 @@ const game = {
         text: `"Yay, sleeping is my second-most favourite activity. Where shall we take a nap?" (hilltop/meadow)
         \n\n`,
         hilltop: {
-          text: `You reach the hilltop and doze off together. yourname starts to roll down the hill. Luckily, the little bear jumps to the rescue and the two of them have the best nap ever, becoming best friends forever. \n ʕ◡ ᴥ ◡ʔ\n\n`
+          text: `You reach the hilltop and doze off together. yourname starts to roll down the hill. Luckily, the little bear cub jumps to the rescue and the two of them have the best nap ever, becoming best friends forever. \n ʕ◡ ᴥ ◡ʔ\n\n`
         },
         meadow: {
-          text: `yourname and the little bear find a peaceful spot in the meadow and doze off. A lion stealthily approaches, but the little bear is alert, and manages to fight it off. The two of them have the best nap ever, becoming best friends forever.
+          text: `yourname and the little bear cub find a peaceful spot in the meadow and doze off. A lion stealthily approaches, but the little bear cub is alert, and manages to fight it off. The two of them have the best nap ever, becoming best friends forever.
           \n ʕ◡ ᴥ ◡ʔ\n\n`
         }
       }
