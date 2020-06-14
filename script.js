55cf208af92c5a959a52631702a61ebe8df49c41
Darrell Yeo <zeniethlily@gmail.com>
2020-06-14 18:14:42 +0800

    full level navigation completed

--- script.js
+++ script.js
@@ -19,27 +19,42 @@ var inputHappened = function(currentInput){ //captures the button that was press
 };
 var processInput = function(input){
     if(input == "Left"){
-        if(currentLevel == 1 || currentLevel == 3){
+        if(currentLevel == 2 || currentLevel == 6){
             player.level.push(currentLevel);
-            currentLevel = 2;
+            currentLevel = 5;
+        } else if(currentLevel == 7){
+            player.level.push(currentLevel);
+            currentLevel = 6;
         } else if(currentLevel == 4){
             player.level.push(currentLevel);
             currentLevel = 3;
+        } else if(currentLevel == 1 || currentLevel == 3 || currentLevel == 5){
+            player.level.push(currentLevel);
+            currentLevel = 2;
         }
     } else if(input == "Forward"){
         if(currentLevel == 1){
             player.level.push(currentLevel);
             currentLevel = 3;
+        } else if(currentLevel == 6){
+            player.level.push(currentLevel);
+            currentLevel = 8;
         }
     } else if(input == "Back"){
         currentLevel = player.level.pop();
     } else if(input == "Right"){
-        if(currentLevel == 1 || currentLevel == 3){
+        if(currentLevel == 4 || currentLevel == 6){
             player.level.push(currentLevel);
-            currentLevel = 4;
+            currentLevel = 7;
+        } else if(currentLevel == 5){
+            player.level.push(currentLevel);
+            currentLevel = 6;
         } else if(currentLevel == 2){
             player.level.push(currentLevel);
             currentLevel = 3;
+        } else if(currentLevel == 1 || currentLevel == 3 || currentLevel == 7){
+            player.level.push(currentLevel);
+            currentLevel = 4
         }
     }
 };
@@ -57,6 +72,14 @@ var loadLevel = function(level){
             return thirdLevel();
         } else if(level == 4){
             return fourthLevel();
+        } else if(level == 5){
+            return fifthLevel();
+        } else if(level == 6){
+            return sixthLevel();
+        } else if(level == 7){
+            return seventhLevel();
+        } else if(level == 8){
+            return finalLevel();
         }
     }
 }
@@ -78,20 +101,56 @@ var firstLevel = function(){
 
 var secondLevel = function(){
     //second level code goes here, loads when user select LEFT path
-    var gameText = "The path opens up into a small cavern, there are two doors. Where will you go?";
+    var gameText = "";
+    if(player.items.includes("Axe")){
+        gameText = "The path opens up into a small cavern, there are two doors. Where will you go?";
+    } else {
+        player.items.push("Axe");
+        gameText = "The path opens up into a small cavern, there are two doors. You spot an axe leaning against the wall, and pick it up. Maybe it will be useful. Where will you go next?";
+    }
     buttonChanger("button2","Back");
+    //document.getElementById("button1").disabled = false;
 
     return gameText;
 }
 
 var thirdLevel = function(){
-    var gameText = "Third level!!";
+    var gameText = "You reach a Tee junction, and you spot 2 doors - to the left and right. Where will you go?";
     buttonChanger("button2","Back");
     return gameText;
 }
 
 var fourthLevel = function(){
-    var gameText = "Fourth LEVEL!!!WTFBBQ"
+    var gameText = "Fourth LEVEL!!!WTFBBQ";
+    buttonChanger("button2","Back");
+    //document.getElementById("button1").disabled = false;
+    return gameText;
+}
+
+var fifthLevel = function(){
+    var gameText = "Fifth Level";
+    buttonChanger("button2","Back");
+    //document.getElementById("button1").disabled = true;
+    return gameText;
+}
+
+var sixthLevel = function(){
+    var gameText = "You are in a square room with a large metal door, flanked by 2 smaller doors - one of which you entered via. You hear growling coming from the large door. Press Forward to enter it, or enter either of the other 2 doors.";
+    // document.getElementById("button1").disabled = false;
+    // document.getElementById("button3").disabled = false;
+    buttonChanger("button2","Forward");
+    return gameText;
+}
+
+var seventhLevel = function(){
+    var gameText = "seventh!";
+    //document.getElementById("button3").disabled = true;
+    buttonChanger("button2","Back");
+    return gameText;
+}
+
+var finalLevel = function(){
+    var gameText = "final boss time. pew pew pew. Press back to run like a chicken!"
     buttonChanger("button2","Back");
     return gameText;
 }
