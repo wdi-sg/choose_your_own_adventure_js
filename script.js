286d6b1e38f5c3a02ba1ab9a6c1e7921acdea7bd
Isabella <65014479+izzycsy@users.noreply.github.com>
2020-06-14 18:03:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ Step 0: Choose your main catto
 Scottish Fold / American Shorthair / Forest Cat 
 Step 1: User chooses 1 option
 Step 2: User confirms option chose
+Step 3: With Catto chosen, list attack variables
 */
 
 var activityStatus = "online";
@@ -23,5 +24,41 @@ var inputHappened = function(input) {
   }
 } 
 // console.log(0);
+//Key in stated options to choose catto
+if(step === 1) {
+  if(input === "SF") {
+    step++;
+    reset();
+    return "Do you want to choose Scottish Fold? \n Enter (Y) or (N).";
+  } else if(input === "AS") {
+      step++;
+      reset();
+      return "Do you want to choose American Shorthair? \n Enter (Y) or (N).";
+  } else if(input === "FC") {
+      step++;
+      reset();
+      return "Do you want to chooseForest Cat? \n Enter (Y) or (N).";
+  } else if(input != "SF" || input != "AS" || input != "FC") {
+      reset();
+      return "Please use SF / FC / FC.";
+  } 
+  //console.log(1);
 
+  //confirm w user of catto option selected
+} else if(step === 2) {
+    if(input === "Y") {
+      activityStatus = "You're all set!";
+      reset();
+      return "Meow! You've chosen Scottish Fold, please take good care of it!";
+    } else if(input === "N") {
+        activityStatus = "You're not set yet...";
+        reset();
+        return "Purr... Do you not want a catto?";
+    } else if(input != "Y" || input != "N") {
+        activityStatus = "You're not set yet...";
+        reset();
+        return "Please key Y / N";
+    }
+}
+//console.log(2);
 
